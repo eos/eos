f4cde60ecbede198baf81d290e244ef68a61fe36
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-02-03 17:11:57 +0100

    Add Kinematics class for name-based lookup

---
+++ src/utils/kinematic.cc
@@ -0,0 +1,60 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#include <src/utils/kinematic.hh>
+#include <src/utils/private_implementation_pattern-impl.hh>
+
+#include <map>
+
+namespace wf
+{
+    template <>
+    struct Implementation<Kinematics>
+    {
+        std::map<std::string, double> variables;
+    };
+
+    Kinematics::Kinematics() :
+        PrivateImplementationPattern<Kinematics>(new Implementation<Kinematics>)
+    {
+    }
+
+    Kinematics::~Kinematics()
+    {
+    }
+
+    double
+    Kinematics::operator[] (const std::string & variable) const
+    {
+        auto i(_imp->variables.find(variable));
+
+        if (_imp->variables.end() == i)
+            throw UnknownKinematicVariableError(variable);
+
+        return i->second;
+    }
+
+    void
+    Kinematics::declare(const std::string & variable)
+    {
+        auto i(_imp->variables.find(variable));
+
+        if (_imp->variables.end() == i)
+            _imp->variables[variable] = 0.0;
+    }
+
+    void
+    Kinematics::set(const std::string & variable, const double & value)
+    {
+        auto i(_imp->variables.find(variable));
+
+        if (_imp->variables.end() == i)
+            throw UnknownKinematicVariableError(variable);
+
+        i->second = value;
+    }
+
+    UnknownKinematicVariableError::UnknownKinematicVariableError(const std::string & variable) throw () :
+        Exception("Unknown kinematic variable: '" + variable + "'")
+    {
+    }
+}
--- src/utils/kinematic.hh
+++ src/utils/kinematic.hh
@@ -3,8 +3,32 @@
 #ifndef WFITTER_GUARD_SRC_UTILS_KINEMATIC_HH
 #define WFITTER_GUARD_SRC_UTILS_KINEMATIC_HH 1
 
+#include <src/utils/exception.hh>
+#include <src/utils/private_implementation_pattern.hh>
+
 namespace wf
 {
+    struct UnknownKinematicVariableError :
+        public Exception
+    {
+        UnknownKinematicVariableError(const std::string & variable) throw ();
+    };
+
+    class Kinematics :
+        public PrivateImplementationPattern<Kinematics>
+    {
+        public:
+            Kinematics();
+
+            ~Kinematics();
+
+            double operator[] (const std::string & variable) const;
+
+            void declare(const std::string & variable);
+
+            void set(const std::string & variable, const double & value);
+    };
+
     inline double lambda(const double & a, const double & b, const double & c)
     {
         return a * a + b * b + c * c - 2.0 * (a * b + a * c + b * c);
