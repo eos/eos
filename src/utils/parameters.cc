aabbb0781e898fbb8b673079e8114febe669ba5e
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-02-03 17:09:09 +0100

    Add Parameters class for name-based lookups

---
+++ src/utils/parameters.cc
@@ -0,0 +1,121 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#include <src/utils/parameters.hh>
+#include <src/utils/private_implementation_pattern-impl.hh>
+
+#include <map>
+#include <vector>
+
+namespace wf
+{
+    namespace impl
+    {
+
+    }
+
+    template <>
+    struct Implementation<Parameters>
+    {
+        std::vector<double> parameters;
+
+        std::map<std::string, unsigned> parameter_map;
+
+        Implementation(const std::initializer_list<Parameters::NameValuePair> & list)
+        {
+            for (auto i(list.begin()), i_end(list.end()) ; i != i_end ; ++i)
+            {
+                parameter_map[i->first] = parameters.size();
+                parameters.push_back(i->second);
+            }
+        }
+    };
+
+    Parameters::Parameters(Implementation<Parameters> * imp) :
+        PrivateImplementationPattern<Parameters>(imp)
+    {
+    }
+
+    Parameters::~Parameters()
+    {
+    }
+
+    Parameter
+    Parameters::operator[] (const std::string & name) const
+    {
+        auto i(_imp->parameter_map.find(name));
+
+        if (_imp->parameter_map.end() == i)
+            throw UnknownParameterError(name);
+
+        return Parameter(_imp, i->second);
+    }
+
+    void
+    Parameters::set(const std::string & name, const double & value)
+    {
+        auto i(_imp->parameter_map.find(name));
+
+        if (_imp->parameter_map.end() == i)
+            throw UnknownParameterError(name);
+
+        _imp->parameters[i->second] = value;
+    }
+
+    Parameters
+    Parameters::FromList(const std::initializer_list<Parameters::NameValuePair> & list)
+    {
+        return Parameters(new Implementation<Parameters>(list));
+    }
+
+    Parameters
+    Parameters::StandardModell()
+    {
+        return Parameters::FromList({
+            // Wilson coefficients at mu = 4.8 GeV to NNLL accuary, cf. ABBBSW2008, p. 6, Table 2.
+            // We use C_{7,8} = C_{7,8}^eff - C'_{7,8}^eff.
+            Parameters::NameValuePair{"c1", -0.257},
+            Parameters::NameValuePair{"c2", +1.009},
+            Parameters::NameValuePair{"c3", +0.005},
+            Parameters::NameValuePair{"c4", -0.078},
+            Parameters::NameValuePair{"c5", +0.000},
+            Parameters::NameValuePair{"c6", +0.001},
+            Parameters::NameValuePair{"c7", -0.298},
+            Parameters::NameValuePair{"c8", -0.164},
+            Parameters::NameValuePair{"c9", +4.211},
+            Parameters::NameValuePair{"c10", -4.103},
+            Parameters::NameValuePair{"mu", 4.8}
+        });
+    }
+
+    Parameter::Parameter(const std::tr1::shared_ptr<Implementation<Parameters>> & imp, unsigned index) :
+        _imp(imp),
+        _index(index)
+    {
+    }
+
+    Parameter::~Parameter()
+    {
+    }
+
+    Parameter::operator double () const
+    {
+        return _imp->parameters[_index];
+    }
+
+    double
+    Parameter::operator() () const
+    {
+        return _imp->parameters[_index];
+    }
+
+    const Parameter &
+    Parameter::operator= (const double & value)
+    {
+        _imp->parameters[_index] = value;
+    }
+
+    UnknownParameterError::UnknownParameterError(const std::string & name) throw () :
+        Exception("Unknown parameter: '" + name + "'")
+    {
+    }
+}
---
+++ src/utils/parameters.hh
@@ -0,0 +1,61 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#ifndef WFITTER_GUARD_SRC_UTILS_PARAMETERS_HH
+#define WFITTER_GUARD_SRC_UTILS_PARAMETERS_HH 1
+
+#include <src/utils/exception.hh>
+#include <src/utils/private_implementation_pattern.hh>
+
+namespace wf
+{
+    struct UnknownParameterError :
+        public Exception
+    {
+        UnknownParameterError(const std::string & variable) throw ();
+    };
+
+    class Parameter;
+
+    class Parameters :
+        public PrivateImplementationPattern<Parameters>
+    {
+        private:
+            Parameters(Implementation<Parameters> *);
+
+        public:
+            typedef std::pair<std::string, double> NameValuePair;
+
+            ~Parameters();
+
+            Parameter operator[] (const std::string & name) const;
+
+            void set(const std::string & name, const double & value);
+
+            static Parameters FromList(const std::initializer_list<NameValuePair> &);
+
+            static Parameters StandardModell();
+    };
+
+    class Parameter
+    {
+        private:
+            std::tr1::shared_ptr<Implementation<Parameters>> _imp;
+
+            unsigned _index;
+
+            Parameter(const std::tr1::shared_ptr<Implementation<Parameters>> & imp, unsigned index);
+
+        public:
+            friend class Parameters;
+
+            ~Parameter();
+
+            operator double () const;
+
+            double operator() () const;
+
+            const Parameter & operator= (const double &);
+    };
+}
+
+#endif
