f47771f459f99085bae233c8bd5ce4053d3ab8b0
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-02-23 13:48:22 +0100

    Add ObservableOptions

--- src/utils/observable.cc
+++ src/utils/observable.cc
@@ -1,6 +1,9 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 #include <src/utils/observable.hh>
+#include <src/utils/private_implementation_pattern-impl.hh>
+
+#include <map>
 
 namespace wf
 {
@@ -12,11 +15,62 @@ namespace wf
     {
     }
 
+    template <>
+    struct Implementation<ObservableOptions>
+    {
+        std::map<std::string, std::string> options;
+    };
+
+    ObservableOptions::ObservableOptions() :
+        PrivateImplementationPattern<ObservableOptions>(new Implementation<ObservableOptions>)
+    {
+    }
+
+    ObservableOptions::~ObservableOptions()
+    {
+    }
+
+    const std::string &
+    ObservableOptions::operator[] (const std::string & key) const
+    {
+        std::cout << "OO::operator[]" << std::endl;
+        auto i(_imp->options.find(key));
+        if (_imp->options.end() == i)
+            throw UnknownOptionError(key);
+
+        return i->second;
+    }
+
+    bool
+    ObservableOptions::has(const std::string & key) const
+    {
+        return _imp->options.end() != _imp->options.find(key);
+    }
+
+    void
+    ObservableOptions::set(const std::string & key, const std::string & value)
+    {
+        auto i(_imp->options.find(key));
+        if (_imp->options.end() != i)
+        {
+            i->second = value;
+        }
+        else
+        {
+            _imp->options[key] = value;
+        }
+    }
+
     ObservableFactory::ObservableFactory()
     {
     }
 
     ObservableFactory::~ObservableFactory()
+    {
+    }
+
+    UnknownOptionError::UnknownOptionError(const std::string & key) throw () :
+        Exception("Unknown option: '" + key + "'")
     {
     }
 }
--- src/utils/observable.hh
+++ src/utils/observable.hh
@@ -3,6 +3,7 @@
 #ifndef WFITTER_GUARD_SRC_UTILS_OBSERVABLE_HH
 #define WFITTER_GUARD_SRC_UTILS_OBSERVABLE_HH 1
 
+#include <src/utils/exception.hh>
 #include <src/utils/kinematic.hh>
 #include <src/utils/parameters.hh>
 
@@ -24,6 +25,27 @@ namespace wf
     };
 
     typedef std::tr1::shared_ptr<Observable> ObservablePtr;
+
+    struct UnknownOptionError :
+        public Exception
+    {
+        UnknownOptionError(const std::string & key) throw ();
+    };
+
+    class ObservableOptions :
+        public PrivateImplementationPattern<ObservableOptions>
+    {
+        public:
+            ObservableOptions();
+
+            ~ObservableOptions();
+
+            const std::string & operator[] (const std::string & key) const;
+
+            bool has(const std::string & key) const;
+
+            void set(const std::string & key, const std::string & value = "");
+    };
 
     class ObservableFactory
     {
