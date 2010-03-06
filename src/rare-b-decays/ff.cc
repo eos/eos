308e48188388027ef80945d268cb16a58804cdd5
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-03-06 12:41:57 +0100

    Add client to print form factor sets

---
+++ src/rare-b-decays/ff.cc
@@ -0,0 +1,75 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#include <src/rare-b-decays/exclusive-b-to-s-dilepton.hh>
+#include <src/rare-b-decays/form_factors.hh>
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace wf;
+
+struct DoUsage
+{
+    std::string what;
+
+    DoUsage(const std::string & what) :
+        what(what)
+    {
+    }
+};
+
+int
+main(int argc, char * argv[])
+{
+    try
+    {
+        if (2 != argc)
+            throw DoUsage("Need at exactly one set of form factors");
+
+        std::tr1::shared_ptr<FormFactors<BToKstar>> set(FormFactorFactory<BToKstar>::create(argv[1]));
+        if (! set)
+            throw DoUsage("Unknown set of form factors: '" + std::string(argv[1]) + "'");
+
+        const unsigned points = 300;
+
+        std::cout << "#s\tV\tA0\tA1\tA2" << std::endl;
+
+        for (unsigned j = 0 ; j <= points ; ++j)
+        {
+            const double s_low = 0;
+            const double s_high = 19.211;
+            double s = s_low + j * (s_high - s_low) / points;
+            double s_hat = s / 5.28 / 5.28;
+
+            std::cout
+                << s << '\t'
+                << set->v(s_hat) << '\t'
+                << set->a_0(s_hat) << '\t'
+                << set->a_1(s_hat) << '\t'
+                << set->a_2(s_hat) << '\t'
+                << std::endl;
+        }
+    }
+    catch (DoUsage & e)
+    {
+        std::cout << e.what << std::endl;
+        std::cout << "Usage: ff FORMFACTORSET" << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (Exception & e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << "STL Exception; " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (...)
+    {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
