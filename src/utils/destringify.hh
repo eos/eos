276a40979b56a1bc612e089cdc72f63980b8bb45
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-03-30 18:04:09 +0200

    [utils] Add destringify

---
+++ src/utils/destringify.hh
@@ -0,0 +1,39 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#ifndef WFITTER_GUARD_SRC_UTILS_DESTRINGIFY_HH
+#define WFITTER_GUARD_SRC_UTILS_DESTRINGIFY_HH 1
+
+#include <sstream>
+
+namespace wf
+{
+    namespace impl
+    {
+        template <typename T_>
+        struct SimpleDestringify
+        {
+            static T_ destringify(const std::string & input)
+            {
+                std::stringstream ss(input);
+                T_ value;
+
+                ss >> value;
+
+                return value;
+            }
+        };
+
+        template <typename T_> struct DoDestringify;
+
+        template <> struct DoDestringify<double> : public SimpleDestringify<double> {};
+    }
+
+    template <typename T_>
+    T_
+    destringify(const std::string & input)
+    {
+        return impl::DoDestringify<T_>::destringify(input);
+    }
+}
+
+#endif
