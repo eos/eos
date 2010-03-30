2983e095c30b18878b739e3757ea716aba6b9c6a
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-03-30 18:17:32 +0200

    [clients] Improve user interface of observables client

--- src/rare-b-decays/observables.cc
+++ src/rare-b-decays/observables.cc
@@ -1,6 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 #include <src/rare-b-decays/factory.hh>
+#include <src/utils/destringify.hh>
 
 #include <cmath>
 #include <cstdlib>
@@ -24,42 +25,72 @@ main(int argc, char * argv[])
 {
     try
     {
-        if (2 > argc)
-            throw DoUsage("Need at least one observable");
-
         Parameters parameters(Parameters::Defaults());
         Kinematics kinematics;
         kinematics.declare("s");
-
+        double s_low(0.0), s_high(19.21);
+        std::list<Parameter> variations;
         std::list<ObservablePtr> observables;
 
-        for (char ** o(argv + 1), ** o_end(argv + argc) ; o != o_end ; ++o)
+        for (char ** a(argv + 1), ** a_end(argv + argc) ; a != a_end ; ++a)
         {
-            ObservableOptions options;
-            std::string observable(*o);
-
-            std::string::size_type pos;
-            while (std::string::npos != (pos = observable.rfind(',')))
+            std::string argument(*a);
+            if ("--parameter" == argument)
             {
-                std::string::size_type sep(observable.find('=', pos + 1));
-                if (std::string::npos == sep)
-                    throw DoUsage("Invalid option: '" + observable.substr(pos + 1) + "'");
-
-                std::string key(observable.substr(pos + 1, sep - pos - 1));
-                std::string value(observable.substr(sep + 1));
-
-                options.set(key, value);
-                observable.erase(pos);
+                std::string name(*(++a));
+                double value(destringify<double>(*(++a)));
+                parameters.set(name, value);
+                std::cerr << "Parameter: " << name << " -> " << value << std::endl;
+                continue;
             }
 
-            ObservablePtr ptr(RareBFactory::make(observable, parameters, options));
+            if ("--range" == argument)
+            {
+                s_low = destringify<double>(*(++a));
+                s_high = destringify<double>(*(++a));
+                std::cerr << "Range: " << s_low << " .. " << s_high << std::endl;
+                continue;
+            }
+
+            if ("--vary" == argument)
+            {
+                std::string name(*(++a));
+                variations.push_back(parameters[name]);
+                std::cerr << "Vary: " << name << std::endl;
+                continue;
+            }
+
+            if ("--observable" != argument)
+                throw DoUsage("Unknown option: '" + argument + "'");
+
+            ObservableOptions options;
+            argument = *(++a);
+
+            std::string::size_type pos;
+            while (std::string::npos != (pos = argument.rfind(',')))
+            {
+                std::string::size_type sep(argument.find('=', pos + 1));
+                if (std::string::npos == sep)
+                    throw DoUsage("Invalid observable option: '" + argument.substr(pos + 1) + "'");
+
+                std::string key(argument.substr(pos + 1, sep - pos - 1));
+                std::string value(argument.substr(sep + 1));
+
+                options.set(key, value);
+                argument.erase(pos);
+            }
+
+            ObservablePtr ptr(RareBFactory::make(argument, parameters, options));
             if (! ptr)
-                throw DoUsage("Unknown observable: '" + observable + "'");
+                throw DoUsage("Unknown observable: '" + argument + "'");
 
             observables.push_back(ptr);
         }
 
-        const unsigned points = 200;
+        if (observables.empty())
+            throw DoUsage("Need at least one observable");
+
+        const unsigned points = 50;
 
         std::cout << "#\ts";
         for (auto o(observables.begin()), o_end(observables.end()) ; o != o_end ; ++o)
@@ -68,29 +99,9 @@ main(int argc, char * argv[])
         }
         std::cout << std::endl;
 
-        std::list<Parameter> variations =
-        {
-            /*
-            parameters["CKM::A"],
-            parameters["CKM::lambda"],*/
-            /*parameters["formfactors::a0_uncertainty"],
-            parameters["formfactors::a1_uncertainty"],
-            parameters["formfactors::a2_uncertainty"],
-            parameters["formfactors::v_uncertainty"],*/
-            /*parameters["mass::b(MSbar)"],
-            parameters["B->K^*::a_1_par"],
-            parameters["B->K^*::a_2_par"],
-            parameters["B->K^*::a_1_perp"],
-            parameters["B->K^*::a_2_perp"],*/
-            /*parameters["mu"],
-            parameters["mass::c"],*/
-            parameters["c7prime"],
-        };
 
         for (unsigned j = 0 ; j <= points ; ++j)
         {
-            const double s_low = 1.0;
-            const double s_high = 6.0;
             double s = s_low + j * (s_high - s_low) / points;
 
             std::cout << s << std::flush;
@@ -98,7 +109,7 @@ main(int argc, char * argv[])
 
             for (auto o(observables.begin()), o_end(observables.end()) ; o != o_end ; ++o)
             {
-                double central = (*o)->evaluate(kinematics), sign = central / std::abs(central);
+                double central = (*o)->evaluate(kinematics);
                 double delta_min = 0.0, delta_max = 0.0;
 
                 for (auto p(variations.begin()), p_end(variations.end()) ; p != p_end ; ++p)
@@ -131,7 +142,7 @@ main(int argc, char * argv[])
                 delta_max = std::sqrt(delta_max);
                 delta_min = std::sqrt(delta_min);
 
-                std::cout << '\t' << central - delta_min << '\t' << central << '\t' << central + delta_max;
+                std::cout << '\t' << delta_min << '\t' << central << '\t' << delta_max;
             }
 
             std::cout << std::endl;
@@ -139,8 +150,8 @@ main(int argc, char * argv[])
     }
     catch (DoUsage & e)
     {
-        std::cout << e.what << std::endl;
-        std::cout << "Usage: observables OBSERVABLE [OBSERVABLE [...]]" << std::endl;
+        std::cerr << e.what << std::endl;
+        std::cerr << "Usage: observables --range SMIN SMAX [--parameter NAME VALUE]* [--vary NAME]* [--observable NAME]+" << std::endl;
         return EXIT_FAILURE;
     }
     catch (Exception & e)
