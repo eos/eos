592ace5387b8b428f4ebdf0720212cfb6a541cec
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-04-13 14:28:52 +0200

    [utils] Use Delta^2 rule by Atkins and Simpson rule for integration

--- src/utils/integrate.cc
+++ src/utils/integrate.cc
@@ -1,71 +1,134 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 #include <src/utils/integrate.hh>
-#include <src/utils/random_number_engine.hh>
 
 #include <limits>
+#include <vector>
 
 namespace wf
 {
+    using std::abs;
+    using std::real;
+    using std::imag;
+
     double integrate(const std::tr1::function<double (const double &)> & f, unsigned n, const double & a, const double & b)
     {
-        static RandomNumberEngine engine;
-
-        double f_mean;
-
-        for (unsigned i(0) ; i < n ; ++i)
-        {
-            double r = double(engine()) / double(std::numeric_limits<unsigned>::max());
-            double x = a + (b - a) * r;
-            double y = f(x);
-
-            f_mean += y;
-        }
-
-        f_mean /= double(n);
-
-        return f_mean * (b - a);
-    }
-#if 0
         if (n & 0x1)
             n += 1;
 
+        if (n < 16)
+            n = 16;
+
         double h = (b - a) / n;
+        std::vector<double> y;
 
-        double result = f(a) + f(b);
-        for (unsigned i = 1 ; i <= n / 2 - 1 ; ++i)
+        for (auto k(0) ; k < n + 1 ; ++k)
         {
-            result += 2.0 * f(a + h * (2.0 * i));
-        }
-        for (unsigned i = 1 ; i <= n / 2 ; ++i)
-        {
-            result += 4.0 * f(a + h * (2.0 * i - 1.0));
+            y.push_back(f(a + k * h));
         }
 
-        result *= h / 3;
+        double Q0 = 0.0, Q1 = 0.0, Q2 = 0.0;
+        for (auto k(0) ; k < n / 8 ; ++k)
+        {
+            Q0 += y[8 * k] + 4.0 * y[8 * k + 4] + y[8 * k + 4];
+        }
+        for (auto k(0) ; k < n / 4 ; ++k)
+        {
+            Q1 += y[4 * k] + 4.0 * y[4 * k + 2] + y[4 * k + 4];
+        }
+        for (auto k(0) ; k < n / 2 ; ++k)
+        {
+            Q2 += y[2 * k] + 4.0 * y[2 * k + 1] + y[2 * k + 2];
+        }
+
+        Q0 = Q0 * h / 3.0 * 4.0;
+        Q1 = Q1 * h / 3.0 * 2.0;
+        Q2 = Q2 * h / 3.0;
+
+        double denom = (Q0 + Q2 - 2.0 * Q1);
+        double num = Q2 - Q1;
+        double correction = num * num / denom;
+        double result;
+
+        if (isnan(correction))
+        {
+            result = Q2;
+        }
+        else if (abs(correction / Q2) < 1.0)
+        {
+            result = Q2 - correction;
+        }
+        else
+        {
+#if 0
+            std::cerr << "Q0 = " << Q0 << std::endl;
+            std::cerr << "Q1 = " << Q1 << std::endl;
+            std::cerr << "Q2 = " << Q2 << std::endl;
+            std::cerr << "Reintegrating with twice the number of data points" << std::endl;
+#endif
+            result = integrate(f, 2 * n, a, b);
+        }
 
         return result;
     }
-#endif
 
     complex<double> integrate(const std::tr1::function<complex<double> (const double &)> & f, unsigned n, const double & a, const double & b)
     {
         if (n & 0x1)
             n += 1;
 
+        if (n < 16)
+            n = 16;
+
         double h = (b - a) / n;
+        std::vector<complex<double>> y;
 
-        complex<double> result = f(a) + f(b);
-        for (unsigned i = 1 ; i <= n / 2 - 1 ; ++i)
+        for (auto k(0) ; k < n + 1 ; ++k)
         {
-            result = result + 2.0 * f(a + h * (2.0 * i));
-        }
-        for (unsigned i = 1 ; i <= n / 2 ; ++i)
-        {
-            result = result + 4.0 * f(a + h * (2.0 * i - 1.0));
+            y.push_back(f(a + k * h));
         }
 
-        result = result * (h / 3.0);
+        complex<double> Q0 = 0.0, Q1 = 0.0, Q2 = 0.0;
+        for (auto k(0) ; k < n / 8 ; ++k)
+        {
+            Q0 += y[8 * k] + 4.0 * y[8 * k + 4] + y[8 * k + 4];
+        }
+        for (auto k(0) ; k < n / 4 ; ++k)
+        {
+            Q1 += y[4 * k] + 4.0 * y[4 * k + 2] + y[4 * k + 4];
+        }
+        for (auto k(0) ; k < n / 2 ; ++k)
+        {
+            Q2 += y[2 * k] + 4.0 * y[2 * k + 1] + y[2 * k + 2];
+        }
+
+        Q0 = Q0 * h / 3.0 * 4.0;
+        Q1 = Q1 * h / 3.0 * 2.0;
+        Q2 = Q2 * h / 3.0;
+
+        double denom_r = real(Q0 + Q2 - 2.0 * Q1), denom_i = imag(Q0 + Q2 - 2.0 * Q1);
+        double num_r = real(Q2 - Q1), num_i = imag(Q2 - Q1);
+        double correction_r = num_r * num_r / denom_r, correction_i = num_i * num_i / denom_i;
+        complex<double> result;
+
+        if (isnan(correction_r) || isnan(correction_i))
+        {
+            result = Q2;
+        }
+        else if ((abs(correction_r / real(Q2)) < 1.0) && (abs(correction_i / imag(Q2) < 1.0)))
+        {
+            result = Q2 - complex<double>(correction_r, correction_i);
+        }
+        else
+        {
+#if 0
+            std::cerr << "Q0 = " << Q0 << std::endl;
+            std::cerr << "Q1 = " << Q1 << std::endl;
+            std::cerr << "Q2 = " << Q2 << std::endl;
+            std::cerr << "Reintegrating with twice the number of data points" << std::endl;
+#endif
+            result = integrate(f, 2 * n, a, b);
+        }
 
         return result;
     }
--- src/utils/integrate_TEST.cc
+++ src/utils/integrate_TEST.cc
@@ -30,14 +30,32 @@ class IntegrateTest :
             return f1(x) / (1.0 - x);
         }
 
+        static double f3(const double & x)
+        {
+            return std::exp(-x);
+        }
+
+        static double f4(const double & x)
+        {
+            return std::log(x);
+        }
+
         virtual void run() const
         {
-            double q1 = integrate(std::tr1::function<double (const double &)>(&f1), 250, 0.0, 1.0), i1 = 1.0;
-            std::cout << "\\int_0.0^1.0 f1(x) dx = " << q1 << " over 250 points" << std::endl;
-            TEST_CHECK(std::abs(i1 - q1) / i1 < 0.04);
+            double q1 = integrate(std::tr1::function<double (const double &)>(&f1), 16, 0.0, 1.0), i1 = 1.0;
+            std::cout << "\\int_0.0^1.0 f1(x) dx = " << q1 << ", eps = " << std::abs(i1 - q1) / q1 << " over 16 points" << std::endl;
+            TEST_CHECK(std::abs(i1 - q1) / i1 < 0.01);
 
-            double q2 = integrate(std::tr1::function<double (const double &)>(&f2), 1000, 0.00, 0.999), i2 = 3.0;
-            std::cout << "\\int_0.0^1.0 f2(x) dx = " << q2 << " over 250 points" << std::endl;
-            TEST_CHECK(std::abs(i2 - q2) / i2 < 0.04);
+            double q2 = integrate(std::tr1::function<double (const double &)>(&f2), 16, 0.00, 0.999999), i2 = 3.0;
+            std::cout << "\\int_0.0^1.0 f2(x) dx = " << q2 << ", eps = " << std::abs(i2 - q2) / q2 << " over 16 points" << std::endl;
+            TEST_CHECK(std::abs(i2 - q2) / i2 < 0.01);
+
+            double q3 = integrate(std::tr1::function<double (const double &)>(&f3), 16, 0.00, 10.0), i3 = 1.0 - std::exp(-10.0);
+            std::cout << "\\int_0.0^10.0 f3(x) dx = " << q3 << ", eps = " << std::abs(i3 - q3) / q3 << " over 16 points" << std::endl;
+            TEST_CHECK(std::abs(i3 - q3) / i3 < 0.01);
+
+            double q4 = integrate(std::tr1::function<double (const double &)>(&f4), 16, 1.0, std::exp(1)), i4 = 1.0;
+            std::cout << "\\int_0.0^10.0 f4(x) dx = " << q4 << ", eps = " << std::abs(i4 - q4) / q4 << " over 16 points" << std::endl;
+            TEST_CHECK(std::abs(i4 - q4) / i4 < 0.01);
         }
 } model_test;
