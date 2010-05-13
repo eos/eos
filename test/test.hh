22bbfd1747ee018c8f1c410d5701a0e77f1c8b4d
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-05-13 16:49:47 +0200

    [test] Add TEST_CHECK_NEARLY_EQUAL

--- test/test.hh
+++ test/test.hh
@@ -4,6 +4,7 @@
 #define WILSON_FITTER_GUARD_TEST_TEST_HH 1
 
 #include <string>
+#include <src/utils/stringify.hh>
 
 namespace test
 {
@@ -47,5 +48,17 @@ namespace test
     } \
     while (false)
 }
+
+#define TEST_CHECK_NEARLY_EQUAL(a, b, eps) \
+    do \
+    { \
+        if (std::abs((a - b)) <= eps) \
+            break; \
+        else \
+            throw TestCaseFailedException(__LINE__, __FILE__, \
+                    "'" #a "' = " + stringify(a) + " is not nearly-equal to '" #b "' = " + stringify(b) + " within '" + stringify(eps) + "'" \
+                    + ", difference is '" + stringify(a - b) + "'"); \
+    } \
+    while (false)
 
 #endif
