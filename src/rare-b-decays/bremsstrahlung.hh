a05cfeb778c8811476c3d8fdedf0e2d3c5a3d4a8
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-05-31 17:46:04 +0200

    [B->X_sll] Added bremsstrahlung building blocks

---
+++ src/rare-b-decays/bremsstrahlung.hh
@@ -0,0 +1,34 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#ifndef WFITTER_GUARD_SRC_RARE_B_DECAYS_BREMSSTRAHLUNG_HH
+#define WFITTER_GUARD_SRC_RARE_B_DECAYS_BREMSSTRAHLUNG_HH 1
+
+#include <src/utils/complex.hh>
+
+namespace wf
+{
+    struct Bremsstrahlung
+    {
+        // cf. [AAGW2002], Eqs. (30), (31), p. 12
+        static complex<double> G_m1(const double & t);
+        static complex<double> G_0(const double & t);
+
+        // cf. [AAGW2002], Eqs. (28), (29), p. 11
+        static complex<double> Deltai_23(const double & s_hat, const double & w, const double & z);
+        static complex<double> Deltai_27(const double & s_hat, const double & w, const double & z);
+
+        // cf. [AAGW2002], Eqs. (23)-(26), p. 10
+        static complex<double> tau_22(const double & s_hat, const double & w, const double & z);
+        static complex<double> tau_27(const double & s_hat, const double & w, const double & z);
+        static complex<double> tau_28(const double & s_hat, const double & w, const double & z);
+        static complex<double> tau_29(const double & s_hat, const double & w, const double & z);
+
+        // Integrals of tau_2x from w = s_hat to w = 1, cf [AAGW2002], Eq. (22)
+        static complex<double> itau_22(const double & s_hat, const double & z);
+        static complex<double> itau_27(const double & s_hat, const double & z);
+        static complex<double> itau_28(const double & s_hat, const double & z);
+        static complex<double> itau_29(const double & s_hat, const double & z);
+    };
+}
+
+#endif
