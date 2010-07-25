587e6cbe904c6197258518d0b0378372c06c222a
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-07-25 20:44:56 +0200

    [utils] Add CartesianProduct<>

---
+++ src/utils/cartesian-product.hh
@@ -0,0 +1,123 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#ifndef WFITTER_GUARD_SRC_UTILS_CARTESIAN_PRODUCT_HH
+#define WFITTER_GUARD_SRC_UTILS_CARTESIAN_PRODUCT_HH 1
+
+#include <vector>
+
+#include <iostream>
+
+template <typename T_> class CartesianProduct
+{
+    private:
+        typedef std::vector<typename T_::const_iterator> IteratorState;
+
+        std::vector<T_> _data;
+
+        IteratorState _begin, _end;
+
+        unsigned long _size;
+
+        template <typename U_> class _Iterator
+        {
+            private:
+                typedef std::vector<typename U_::const_iterator> IteratorState;
+
+                IteratorState _state, _begin, _end;
+
+                _Iterator(const IteratorState & state, const IteratorState & begin, const IteratorState & end) :
+                    _state(state),
+                    _begin(begin),
+                    _end(end)
+                {
+                }
+
+            public:
+                friend class CartesianProduct<U_>;
+
+                bool operator!= (const _Iterator & other)
+                {
+                    auto j = other._state.cbegin();
+
+                    for (auto i = _state.cbegin(), i_end = _state.cend() ; i != i_end ; ++i, ++j)
+                    {
+                        if (*i != *j)
+                            return true;
+                    }
+
+                    return other._state.cend() != j;
+                }
+
+                _Iterator & operator++ ()
+                {
+                    auto ji = _state.rbegin(), jb = _begin.rbegin(), je = _end.rbegin();
+
+                    do
+                    {
+                        ++(*ji);
+
+                        if ((*je) != (*ji))
+                            break;
+
+                        (*ji) = (*jb);
+
+                        ++ji;
+                        ++jb;
+                        ++je;
+                    } while (_state.rend() != ji);
+
+                    if (_state.rend() == ji)
+                        _state = _end;
+
+                    return *this;
+                }
+
+                std::vector<double> operator* () const
+                {
+                    std::vector<double> result;
+                    for (auto i = _state.begin() ; _state.end() != i ; ++i)
+                    {
+                        result.push_back(*(*i));
+                    }
+
+                    return result;
+                }
+        };
+
+    public:
+        typedef _Iterator<T_> Iterator;
+
+        CartesianProduct() :
+            _size(0)
+        {
+        }
+
+        void over(const T_ & x)
+        {
+            _data.push_back(x);
+            _begin.push_back(_data.back().cbegin());
+            _end.push_back(_data.back().cend());
+
+            if (0 == _size)
+                _size = x.size();
+            else
+                _size *= x.size();
+        }
+
+        Iterator begin() const
+        {
+            return Iterator(_begin, _begin, _end);
+        }
+
+        Iterator end() const
+        {
+            return Iterator(_end, _begin, _end);
+        }
+
+        unsigned long size() const
+        {
+            return _size;
+        }
+};
+
+#endif
---
+++ src/utils/cartesian-product_TEST.cc
@@ -0,0 +1,59 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#include <test/test.hh>
+#include <src/utils/cartesian-product.hh>
+
+#include <complex>
+
+#include <iostream>
+
+using namespace test;
+using namespace wf;
+
+class CartesianProductTest :
+    public TestCase
+{
+    public:
+        CartesianProductTest() :
+            TestCase("cartesian_product_test")
+        {
+        }
+
+        static void check_tuples(const std::vector<double> & a, const std::vector<double> & b)
+        {
+            for (auto i = a.cbegin(), i_end = a.cend(), j = b.cbegin() ; i != i_end; ++i, ++j)
+            {
+                TEST_CHECK_EQUAL(*i, *j);
+            }
+        }
+
+        virtual void run() const
+        {
+            /* No Product at all */
+            {
+                static const std::vector<double> input = { -0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5 };
+                static const std::vector<std::vector<double>> result = {
+                    { -0.5 },
+                    { -0.4 },
+                    { -0.3 },
+                    { -0.2 },
+                    { -0.1 },
+                    {  0.0 },
+                    {  0.1 },
+                    {  0.2 },
+                    {  0.3 },
+                    {  0.4 },
+                    {  0.5 },
+                };
+
+                CartesianProduct<std::vector<double>> cp;
+                cp.over(input);
+
+                auto j = result.cbegin();
+                for (auto i = cp.begin() ; cp.end() != i ; ++i, ++j)
+                {
+                    check_tuples(*i, *j);
+                }
+            }
+        }
+} cartesian_product_test;
