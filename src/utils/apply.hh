9f9d70d685436ea43ef101f390ef9c16baadcbe8
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-08-15 03:24:27 +0200

    [utils] Add apply wrapper

---
+++ src/utils/apply.hh
@@ -0,0 +1,69 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#ifndef WFITTER_GUARD_SRC_UTILS_APPLY_HH
+#define WFITTER_GUARD_SRC_UTILS_APPLY_HH 1
+
+#include <functional>
+#include <tuple>
+
+namespace wf
+{
+    namespace impl
+    {
+        template <typename Result_, unsigned n_> struct Applicator
+        {
+            template <typename Function_, typename Tuple_, typename ... CallArgs_>
+            static Result_ apply(const Function_ & f, const Tuple_ & t, CallArgs_ ... a)
+            {
+                return Applicator<Result_, n_ - 1>::apply(f, t, std::get<n_ - 1>(t), a ...);
+            }
+        };
+
+        template <typename Result_> struct Applicator<Result_, 0>
+        {
+            template <typename Function_, typename Tuple_, typename ... CallArgs_>
+            static Result_ apply(const Function_ & f, const Tuple_ & t, CallArgs_ ... a)
+            {
+                return f(a ...);
+            }
+        };
+
+        template <> struct Applicator<void, 0>
+        {
+            template <typename Function_, typename Tuple_, typename ... CallArgs_>
+            static void apply(const Function_ & f, const Tuple_ & t, CallArgs_ ... a)
+            {
+                f(a ...);
+            }
+        };
+    }
+
+    /* freestanding function */
+    template <typename Result_, typename ... FunctionArgs_, typename ... TupleElements_>
+    Result_ apply(Result_ (* f)(FunctionArgs_ ...), const std::tuple<TupleElements_ ...> & t)
+    {
+        static_assert(sizeof...(FunctionArgs_) == sizeof...(TupleElements_), "Cannot apply function of N parameters to tuple of M elements, N != M");
+
+        return impl::Applicator<Result_, sizeof...(FunctionArgs_)>::apply(f, t);
+    }
+
+    /* function wrapped in std::function<> */
+    template <typename Result_, typename ... FunctionArgs_, typename ... TupleElements_>
+    Result_ apply(const std::function<Result_ (FunctionArgs_ ...)> & f, const std::tuple<TupleElements_ ...> & t)
+    {
+        static_assert(sizeof...(FunctionArgs_) == sizeof...(TupleElements_), "Cannot apply function of N parameters to tuple of M elements, N != M");
+
+        return impl::Applicator<Result_, sizeof...(FunctionArgs_)>::apply(f, t);
+    }
+
+    /* pointer to member function */
+    template <typename Result_, typename Class_, typename ... FunctionArgs_, typename ... TupleElements_>
+    Result_ apply(Result_ (Class_::* f)(FunctionArgs_ ...), const std::tuple<Class_ *, TupleElements_ ...> & t)
+    {
+        static_assert(sizeof...(FunctionArgs_) == sizeof...(TupleElements_), "Cannot apply function of N parameters to tuple of M elements, N != M");
+
+        return impl::Applicator<Result_, 1 + sizeof...(FunctionArgs_)>::apply(std::function<Result_ (Class_ *, FunctionArgs_ & ...)>(std::mem_fn(f)), t);
+    }
+}
+
+#endif
---
+++ src/utils/apply_TEST.cc
@@ -0,0 +1,57 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+#include <test/test.hh>
+#include <src/utils/apply.hh>
+
+#include <cmath>
+#include <iostream>
+
+using namespace test;
+using namespace wf;
+
+struct TestPointerToMemberFunction
+{
+    double _x, _y;
+    void f(const double & x, const double & y)
+    {
+        _x = x; _y = y;
+    }
+};
+
+class ApplyTest :
+    public TestCase
+{
+    public:
+        ApplyTest() :
+            TestCase("apply_test")
+        {
+        }
+
+        static double test_2(const double & x)
+        {
+            return x;
+        }
+
+        static double test_3_function()
+        {
+            return std::exp(1.0);
+        }
+
+        virtual void run() const
+        {
+            TestPointerToMemberFunction test_1;
+
+            apply(&TestPointerToMemberFunction::f, std::make_tuple(&test_1, 1.0, 2.0));
+            TEST_CHECK_EQUAL(test_1._x, 1.0);
+            TEST_CHECK_EQUAL(test_1._y, 2.0);
+
+            double result;
+
+            result = apply(&ApplyTest::test_2, std::make_tuple(M_PI));
+            TEST_CHECK_EQUAL(result, M_PI);
+
+            std::function<double ()> test_3(&ApplyTest::test_3_function);
+            result = apply(test_3, std::make_tuple());
+            TEST_CHECK_EQUAL(result, std::exp(1.0));
+        }
+} apply_test;
