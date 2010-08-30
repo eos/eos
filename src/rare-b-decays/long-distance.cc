1517716d9d32d3f4f4fb98d360b7de2323e269ce
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-08-30 14:30:52 +0200

    [B->K^*ll] Fix LongDistance ccbar contributions
    Calculation of real part is now correct and numerically stable.
    Also set fudge factor kappa_V = 1.

--- src/rare-b-decays/long-distance.cc
+++ src/rare-b-decays/long-distance.cc
@@ -11,36 +11,38 @@ namespace wf
     {
         // cf. [KS1996], Eqs. (3.3) and (3.4), p. 5
         static const double alpha = 1.0 / 133.0;
-        static const double m_B = 5.279;
+        // cf. [KS1996], Appendix: Input Parameters, p. 9
+        static const double m_b = 4.8;
         static const double m_D = 1.865;
-        static const double s_0_hat = 4.0 * m_D * m_D / m_B / m_B;
+        static const double s_0_hat = 4.0 * m_D * m_D / m_b / m_b;
         // cf. [KS1996], Eq. (A2), p. 9
         static const double s_1_hat = 0.60;
         static const double s_2_hat = 0.69;
 
         // We us a universal fudge factor, kappa_V = kappa, adjusted
         // so that C_0({C_i}) * kappa ~= 0.72. Using C_0^NLL = 0.61.
-        static const double fudge = 1.2;
+        // This yields fudge O(1), so we simply set:
+        static const double fudge = 1.0;
 
         static const std::vector<double> m_cc_hat
         {
             /* Masses in GeV */
-            3.0969 / m_B,
-            3.6861 / m_B,
-            3.771 / m_B,
-            4.039 / m_B,
-            4.153 / m_B,
-            4.421 / m_B,
+            3.0969 / m_b,
+            3.6861 / m_b,
+            3.771 / m_b,
+            4.039 / m_b,
+            4.153 / m_b,
+            4.421 / m_b,
         };
         static const std::vector<double> gamma_cc_hat
         {
             /* Decay widths in GeV */
-            9.34e-5 / m_B,
-            3.37e-4 / m_B,
-            2.30e-2 / m_B,
-            8.00e-2 / m_B,
-            1.03e-1 / m_B,
-            6.20e-2 / m_B,
+            9.34e-5 / m_b,
+            3.37e-4 / m_b,
+            2.30e-2 / m_b,
+            8.00e-2 / m_b,
+            1.03e-1 / m_b,
+            6.20e-2 / m_b,
         };
         static const std::vector<double> br_cc
         {
@@ -52,7 +54,7 @@ namespace wf
             9.400e-6,
         };
 
-        double s_hat = s / m_B / m_B, alpha2 = alpha * alpha;
+        double s_hat = s / m_b / m_b, alpha2 = alpha * alpha;
 
         double imag_res = 0.0, real_res = 0.0;
         for (auto m = m_cc_hat.cbegin(), g = gamma_cc_hat.cbegin(), b = br_cc.cbegin() ; m_cc_hat.cend() != m ; ++m, ++g, ++b)
@@ -62,9 +64,10 @@ namespace wf
             double cc = (*m) * (*g);
 
             imag_res += aa * s_hat / ((s_hat - bb) * (s_hat - bb) + cc * cc);
-            real_res += aa * s_hat / (6.0 * cc) * (
-                    (bb - s_hat) * (M_PI + 2.0 * atan((bb - s_0_hat) / cc))
-                    + cc * log((s_0_hat - s_hat) * (s_0_hat - s_hat) / ((bb - s_0_hat) * (bb - s_0_hat) + cc * cc)));
+            real_res += aa / (2.0 * cc) * (
+                        (s_hat - bb) * (M_PI - 2.0 * std::atan((bb - s_0_hat) / cc))
+                        - cc * std::log((s_0_hat - s_hat) * (s_0_hat - s_hat) / ((bb - s_0_hat) * (bb - s_0_hat) + cc * cc))
+                    ) / ((bb - s_hat) * (bb - s_hat) + cc * cc);
         }
 
         double imag_cont = 0.0;
@@ -77,13 +80,13 @@ namespace wf
             imag_cont = 1.02;
         }
 
-        double real_cont = s_hat / 3.0 * (
-                0.0 // Contribution from s_hat_prime < 0.60
-                + log(std::abs((s_2_hat - s_hat) / (s_1_hat - s_hat))) // Contribution from 0.60 < s_hat_prime < 0.69
-                - log(std::abs((1.0 - s_hat / s_2_hat))) / s_hat); // Contribution from 0.69 < s_hat_prime < 1
+        double real_cont = (
+                    0.571896
+                    + 34.0 / 3.0 * ((s_1_hat - s_hat) * std::log(std::abs(s_1_hat - s_hat)) - (s_2_hat - s_hat) * std::log(std::abs(s_2_hat - s_hat)))
+                ) / s_hat;
 
-        return complex<double>(real_cont + fudge * real_res, M_PI / 3.0 * (fudge * imag_res + imag_cont))
-            -8.0 / 9.0 * log(m_c / m_B) - 4.0 / 9.0;
+        return complex<double>(s_hat * (real_cont + fudge * real_res) / 3.0, M_PI / 3.0 * (fudge * imag_res + imag_cont))
+            -8.0 / 9.0 * log(m_c / m_b) - 4.0 / 9.0;
     }
 }
 
--- src/rare-b-decays/long-distance_TEST.cc
+++ src/rare-b-decays/long-distance_TEST.cc
@@ -7,6 +7,8 @@
 #include <cmath>
 #include <vector>
 
+#include <iostream>
+
 using namespace test;
 using namespace wf;
 
@@ -25,12 +27,11 @@ class LongDistanceTest :
             static const double eps = 1e-6;
             static const std::vector<double> inputs{ 14.00, 15.00, 16.00, 19.21 };
             static const std::vector<complex<double>> results{
-                complex<double>(1.421627, 0.351849),
-                complex<double>(1.543080, 0.195207),
-                complex<double>(1.756748, 1.254808),
-                complex<double>(2.053897, 1.912082),
+                complex<double>(6.716354, 0.381747),
+                complex<double>(4.064683, 0.766175),
+                complex<double>(3.230018, 2.113815),
+                complex<double>(2.195244, 1.780107),
             };
-
             auto r = results.cbegin();
             for (auto i = inputs.cbegin() ; inputs.cend() != i ; ++i, ++r)
             {
