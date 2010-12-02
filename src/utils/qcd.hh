c18b5b49b2758ea772e160e724cc80ddf0bd351c
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-12-02 17:17:10 +0100

    [doc] Convert QCD documentation to doxygen format

--- src/utils/qcd.hh
+++ src/utils/qcd.hh
@@ -10,65 +10,68 @@ namespace eos
     class QCD
     {
         public:
+            /// The four leading coefficients of the \f$\alpha_s\f$ expansion of the quark mass' anomalous dimension \f$\gamma_m\f$.
             typedef std::array<double, 4> AnomalousMassDimension;
+
+            /// The four leading coefficient of the \f$\alpha_s\f$ expansion of the beta function of QCD.
             typedef std::array<double, 4> BetaFunction;
 
-            /*
-             * Running coupling alpha_s down to mu from mu_0, based on QCD parameters in params.
+            /*!
+             * Calculate RGE running of strong coupling alpha_s from scale mu_0 down to scale mu in the MSbar scheme.
              *
              * Calculation according to [CKS2000].
              *
-             * @mu        : scale at which alpha_s shall be evaluated
-             * @alpha_s_0 : alpha_s at the initial scale mu_0
-             * @mu_0      : initial scale mu_0
-             * @beta      : parameters of QCD beta function that control the running
+             * @param mu          scale at which alpha_s shall be evaluated
+             * @param alpha_s_0   alpha_s at the initial scale mu_0
+             * @param mu_0        initial scale mu_0
+             * @param beta        parameters of QCD beta function that control the running
              */
             static double alpha_s(const double & mu, const double & alpha_s_0, const double & mu_0, const BetaFunction & beta);
 
-            /*
-             * Running quark mass in the MSbar scheme.
+            /*!
+             * Calculate RGE running of quark mass m_q in the MSbar scheme.
              *
              * Calculation according to [CKS2000].
              *
-             * @m_q_0       : quark mass in the MSbar scheme at the scale m_q
-             * @alpha_s_0   : alpha_s at the scale mu_0 = m_q
-             * @alpha_s_mu  : alpha_s at the scale mu
-             * @beta        : parameters of QCD beta function that control the running
-             * @gamma_m     : parameters of QCD anomalous mass dimension that control the running
+             * @param m_q         quark mass in the MSbar scheme at the scale m_q
+             * @param alpha_s_0   alpha_s at the scale mu_0 = m_q
+             * @param alpha_s_mu  alpha_s at the scale mu
+             * @param beta        parameters of QCD beta function that control the running
+             * @param gamma_m     parameters of QCD anomalous mass dimension that control the running
              */
-            static double m_q_msbar(const double & m_q, const double & alpha_s_mu0, const double & alpha_s_mu, const BetaFunction & beta, const AnomalousMassDimension & gamma_m);
+            static double m_q_msbar(const double & m_q, const double & alpha_s_0, const double & alpha_s_mu, const BetaFunction & beta, const AnomalousMassDimension & gamma_m);
 
-            /*
-             * Pole mass shift from the MSbar scheme
+            /*!
+             * Calculate the shift from MSbar scheme to pole mass
              *
              * Calculation according to [CERN2002-002].
              *
-             * @m_q     : quark mass in the MSbar scheme at the scale m_q
-             * @alpha_s : alpha_s at the scale m_q
-             * @nf      : number of active QCD flavors that control the calculation
+             * @param m_q         quark mass in the MSbar scheme at the scale m_q
+             * @param alpha_s     alpha_s at the scale m_q
+             * @param nf          number of active QCD flavors that control the calculation
              */
             static double m_q_pole(const double & m_q, const double & alpha_s, const double & nf);
 
-            /*
-             * Potential-Subtracted mass (PS mass) shift from the MSbar scheme
+            /*!
+             * Calculate the from from MSbar scheme to potential-Subtracted mass (PS mass).
              *
              * Calculation according to [B1998].
              *
-             * @m_q     : quark mass in the MSbar scheme at the scale m_q
-             * @alpha_s : alpha_s at the scalem_q
-             * @mu_f    : Factorization scale
-             * @nf      : number of active QCD flavors that control the calculation
-             * @beta    : parameters of QCD beta function that control the calculation
+             * @param m_q         quark mass in the MSbar scheme at the scale m_q
+             * @param alpha_s     alpha_s at the scalem_q
+             * @param mu_f        Factorization scale
+             * @param nf          number of active QCD flavors that control the calculation
+             * @param beta        parameters of QCD beta function that control the calculation
              */
             static double m_q_ps(const double & m_q, const double & alpha_s, const double & mu_f, const double & nf, const BetaFunction & beta);
 
-            /*
-             * The quadratic casimir operator for the fundamental representation
+            /*!
+             * The quadratic casimir operator for the fundamental representation of SU(3).
              */
             static const double casimir_f;
 
-            /*
-             * The quadratic casimir operator for the ajoint representation
+            /*!
+             * The quadratic casimir operator for the adjoint representation SU(3).
              */
             static const double casimir_a;
     };
