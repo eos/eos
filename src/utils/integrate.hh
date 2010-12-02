74fc786b6b177627c324f024124be6c638009b47
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-12-02 17:17:37 +0100

    [doc] Document integrate()

--- src/utils/integrate.hh
+++ src/utils/integrate.hh
@@ -9,10 +9,20 @@
 
 namespace eos
 {
-    // TODO: Do a proper MonteCarlo integration
+    /// @{
+    /*!
+     * Numerically integrate functions of one real-valued parameter.
+     *
+     * Uses the Delta^2-Rule by Aitkin to refine the result.
+     *
+     * @param f      Integrand.
+     * @param n      Number of evaluations, must be a power of 2.
+     * @param a      Lower limit of the domain of integration.
+     * @param b      Upper limit of the domain of integration.
+     */
     double integrate(const std::function<double (const double &)> & f, unsigned n, const double & a, const double & b);
-
     complex<double> integrate(const std::function<complex<double> (const double &)> & f, unsigned n, const double & a, const double & b);
+    /// @}
 }
 
 #endif
