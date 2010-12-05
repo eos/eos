c0b6ecda2641614b46393ddfcf6fd2f97bedc107
Danny van Dyk <danny.dyk@tu-dortmund.de>
2010-12-05 17:58:52 +0100

    [utils] Tidy up RandomNumberEngine

--- src/utils/random_number_engine.cc
+++ src/utils/random_number_engine.cc
@@ -3,14 +3,16 @@
 #include <src/utils/private_implementation_pattern-impl.hh>
 #include <src/utils/random_number_engine.hh>
 
-#include <tr1/random>
+#include <random>
+
+#include <iostream>
 
 namespace eos
 {
     template <>
     struct Implementation<RandomNumberEngine>
     {
-        std::tr1::mt19937 engine;
+        std::mt19937 engine;
     };
 
     RandomNumberEngine::RandomNumberEngine() :
@@ -22,10 +24,10 @@ namespace eos
     {
     }
 
-    unsigned int
+    double
     RandomNumberEngine::operator() (void)
     {
-        return _imp->engine();
+        return _imp->engine() / static_cast<double>((1L << 32) - 1);
     }
 }
 
--- src/utils/random_number_engine.hh
+++ src/utils/random_number_engine.hh
@@ -5,17 +5,34 @@
 
 #include <src/utils/private_implementation_pattern.hh>
 
+#include <cstdint>
+
 namespace eos
 {
+    /*!
+     * Generate pseudo-random floating point numbers
+     * in the range 0 ... 1.
+     *
+     * RandomNumberEngine keeps its state/seed on copying.
+     */
     class RandomNumberEngine :
         public PrivateImplementationPattern<RandomNumberEngine>
     {
         public:
+            ///@{
+            /// Basic functions.
             RandomNumberEngine();
-
             ~RandomNumberEngine();
+            ///@}
 
-            unsigned int operator() (void);
+            /// Obtain a pseudo-random number in the range 0.0 .. 1.0.
+            double operator() ();
+
+            /// Return the maximal value that can be drawn.
+            double max() const { return 1.0; }
+
+            /// Return the minimal value that can be drawn.
+            double min() const { return 0.0; }
     };
 }
 
