13c5e99a43afa7e0d4e112333d97c87ec04c1bc2
Christian Wacker <christian.wacker@tu-dortmund.de>
2010-12-15 13:29:38 +0100

    [utils] Extend power_of test for std::complex

--- src/utils/power_of_TEST.cc
+++ src/utils/power_of_TEST.cc
@@ -21,6 +21,7 @@ class PowerOfTest :
         virtual void run() const
         {
             {
+                // Test power_of<n, double>
                 static const double eps = 1e-14;
 
                 TEST_CHECK_NEARLY_EQUAL(1.0,      power_of<0>(1.2), eps);
@@ -38,6 +39,18 @@ class PowerOfTest :
                 TEST_CHECK_NEARLY_EQUAL(0.0256,   power_of<4>(0.4), eps);
                 TEST_CHECK_NEARLY_EQUAL(0.01024,  power_of<5>(0.4), eps);
                 TEST_CHECK_NEARLY_EQUAL(0.004096, power_of<6>(0.4), eps);
+            }
+
+            {
+                // Test power_of<n, std::complex<double>>
+                static const double eps = 1e-14;
+
+                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(1.0, 0.0),       power_of<0>(std::complex<double>(1.2, 0.9)), eps);
+                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(0.0, -1.0),      power_of<3>(std::complex<double>(0.0, 1.0)), eps);
+                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(1.0, 0.0),       power_of<4>(std::complex<double>(0.0, 1.0)), eps);
+                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(-9.6719, -1.56), power_of<4>(std::complex<double>(1.2, 1.3)), eps);
+                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(2.985984, 0.0),  power_of<6>(std::complex<double>(1.2, 0.0)), eps);
+                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(0.004096, 0.0),  power_of<6>(std::complex<double>(0.4, 0.0)), eps);
             }
         }
 } power_of_test;
