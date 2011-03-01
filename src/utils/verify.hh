bbf1f9aff418a6e817722c626c049dc6f7248aff
Frederik Beaujean <beaujean@mpp.mpg.de>
2011-03-01 19:16:26 +0100

    [utils] Expand documentation and tests for VerifiedRange

--- src/utils/verify.hh
+++ src/utils/verify.hh
@@ -1,5 +1,23 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
+/*
+ * Copyright (c) 2011 Danny van Dyk
+ * Copyright (c) 2011 Frederik Beaujean
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 #ifndef EOS_GUARD_SRC_UTILS_VERIFY_HH
 #define EOS_GUARD_SRC_UTILS_VERIFY_HH 1
 
@@ -7,6 +25,10 @@
 
 namespace eos
 {
+    /*!
+     * VerifiedRangeError is thrown whenever a VerifiedRange is assigned a value
+     * that exceeds its allowed range.
+     */
     class VerifiedRangeError :
         public Exception
     {
@@ -14,18 +36,36 @@ namespace eos
             VerifiedRangeError(const std::string & message);
     };
 
+    /*!
+     * VerifiedRangeOverflow is thrown whenever a VerifiedRange is assigned a value
+     * that is larger than its allowed maximum.
+     */
     struct VerifiedRangeOverflow :
         public VerifiedRangeError
     {
         VerifiedRangeOverflow(const std::string & value, const std::string & maximum);
     };
 
+    /*!
+     * VerifiedRangeUnderflow is thrown whenever a VerifiedRange is assigned a value
+     * that is smaller than its allowed minimum.
+     */
     struct VerifiedRangeUnderflow :
         public VerifiedRangeError
     {
         VerifiedRangeUnderflow(const std::string & value, const std::string & minimum);
     };
 
+    /*!
+     * VerifiedRange is a wrapper around a variable that allows assignment
+     * only within a given range.
+     *
+     * @code
+     * VerifiedRange<double> value_between_zero_and_one(0.0, 1.0, 0.5);
+     * test_range = +10; // not allowed, throws a VerifiedRangeOverflow
+     * test_range = -10; // not allowed, throws a VerifiedRangeUnderflow
+     * @endcode
+     */
     template <typename T_>
     class VerifiedRange
     {
@@ -48,22 +88,54 @@ namespace eos
             }
 
         public:
+            ///@name Basic Functions
+            ///@{
+            /*!
+             * Constructor.
+             *
+             * @param min   Minimal value that is allowed for any value of this object.
+             * @param max   Maximal value that is allowed for any value of this object.
+             * @param value Initial value of this object.
+             */
             VerifiedRange(const T_ & min, const T_ & max, const T_ & value) :
                 _min(min),
                 _max(max),
                 _value(_verify(value))
             {
             }
+            ///@}
 
+            ///@name Operations
+            ///@{
+            /// Conversion operator its native type T_
             operator T_ () const
             {
                 return _value;
             }
 
-            const T_ & operator= (const T_ & value)
+            /*!
+             * Assignment operator.
+             *
+             * @param rhs Right-hand-side of an assignment to this object.
+             */
+            const T_ & operator= (const T_ & rhs)
             {
-                _value = _verify(value);
+                _value = _verify(rhs);
+                return _value;
             }
+
+            /// Retrieve the minimal value that is allowed for this object.
+            T_ min() const
+            {
+                return _min;
+            }
+
+            /// Retrieve the maximal value that is allowed for this object.
+            T_ max() const
+            {
+                return _max;
+            }
+            ///@}
     };
 }
 
--- src/utils/verify_TEST.cc
+++ src/utils/verify_TEST.cc
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2011 Danny van Dyk
+ * Copyright (c) 2011 Frederik Beaujean
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -31,14 +32,26 @@ class VerifiedRangeTest :
 {
     public:
         VerifiedRangeTest() :
-            TestCase("verified_rage_test")
+            TestCase("verified_range_test")
         {
         }
 
         virtual void run() const
         {
+            // proper construction
             TEST_CHECK_NO_THROW(VerifiedRange<double> test_range(0.0, 1.0, 0.5));
+            VerifiedRange<double> test_range(0.0, 1.0, 0.5);
 
+            // false assignments
+            TEST_CHECK_THROWS(VerifiedRangeUnderflow, test_range = -800);
+            TEST_CHECK_THROWS(VerifiedRangeOverflow, test_range = +100);
+
+            // retrieve value
+            TEST_CHECK_EQUAL(test_range, 0.5);
+            TEST_CHECK_NO_THROW(test_range = 0.9);
+            TEST_CHECK_EQUAL(test_range, 0.9);
+
+            // constructor exceptions
             TEST_CHECK_THROWS(VerifiedRangeUnderflow, VerifiedRange<double> test_range(0.0, 1.0, -0.1));
             TEST_CHECK_THROWS(VerifiedRangeOverflow, VerifiedRange<double> test_range(0.0, 1.0, 1.1));
         }
