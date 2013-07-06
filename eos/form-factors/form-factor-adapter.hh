2c45c1b2162f3e82b446a4e5dbfc53ea3d737487
Danny van Dyk <danny.van.dyk@gmail.com>
2013-07-06 12:08:57 +0200

    [form-factors] Add FormFactorRatioAdapter to allow for generic form factor ratio observables

--- eos/form-factors/form-factor-adapter.hh
+++ eos/form-factors/form-factor-adapter.hh
@@ -134,6 +134,126 @@ namespace eos
                 return ObservablePtr(new FormFactorAdapter<Transition_>(_name, _process, parameters, kinematics, options, _form_factor_function));
             }
     };
+
+    /* Form factor ratio adapter class for interfacing Observable */
+    template <typename Transition_>
+    class FormFactorRatioAdapter :
+        public Observable
+    {
+        private:
+            std::string _name;
+
+            std::string _process;
+
+            Parameters _parameters;
+
+            Kinematics _kinematics;
+
+            KinematicVariable _s;
+
+            Options _options;
+
+            std::shared_ptr<FormFactors<Transition_>> _form_factors;
+
+            std::function<double (const FormFactors<Transition_> *, const double &)> _form_factor_numerator;
+
+            std::function<double (const FormFactors<Transition_> *, const double &)> _form_factor_denominator;
+
+        public:
+            FormFactorRatioAdapter(const std::string & name,
+                    const std::string & process,
+                    const Parameters & parameters,
+                    const Kinematics & kinematics,
+                    const Options & options,
+                    const std::function<double (const FormFactors<Transition_> *, const double &)> & form_factor_numerator,
+                    const std::function<double (const FormFactors<Transition_> *, const double &)> & form_factor_denominator) :
+                _name(name),
+                _process(process),
+                _parameters(parameters),
+                _kinematics(kinematics),
+                _s(kinematics["s"]),
+                _options(options),
+                _form_factor_numerator(form_factor_numerator),
+                _form_factor_denominator(form_factor_denominator)
+            {
+                if (! _options.has("form-factors"))
+                    throw UnknownOptionError("form-factors");
+
+                _form_factors = FormFactorFactory<Transition_>::create(process + '@' + options["form-factors"], _parameters);
+            }
+
+            virtual const std::string & name() const
+            {
+                return _name;
+            }
+
+            virtual double evaluate() const
+            {
+                double s = _s();
+
+                return _form_factor_numerator(_form_factors.get(), s) / _form_factor_denominator(_form_factors.get(), s);
+            };
+
+            virtual Parameters parameters()
+            {
+                return _parameters;
+            };
+
+            virtual Kinematics kinematics()
+            {
+                return _kinematics;
+            };
+
+            virtual Options options()
+            {
+                return _options;
+            }
+
+            virtual ObservablePtr clone() const
+            {
+                return ObservablePtr(new FormFactorRatioAdapter(_name, _process, _parameters.clone(), _kinematics.clone(), _options, _form_factor_numerator, _form_factor_denominator));
+            }
+
+            virtual ObservablePtr clone(const Parameters & parameters) const
+            {
+                return ObservablePtr(new FormFactorRatioAdapter(_name, _process, parameters, _kinematics.clone(), _options, _form_factor_numerator, _form_factor_denominator));
+            }
+    };
+
+    template <typename Transition_>
+    class FormFactorRatioAdapterFactory :
+        public ObservableFactory
+    {
+        private:
+            std::string _name;
+
+            std::string _process;
+
+            std::function<double (const FormFactors<Transition_> *, const double &)> _form_factor_numerator;
+
+            std::function<double (const FormFactors<Transition_> *, const double &)> _form_factor_denominator;
+
+        public:
+            FormFactorRatioAdapterFactory(const std::string & name,
+                    const std::string & process,
+                    const std::function<double (const FormFactors<Transition_> *, const double &)> & form_factor_numerator,
+                    const std::function<double (const FormFactors<Transition_> *, const double &)> & form_factor_denominator) :
+                _name(name),
+                _process(process),
+                _form_factor_numerator(form_factor_numerator),
+                _form_factor_denominator(form_factor_denominator)
+            {
+            }
+
+            ~FormFactorRatioAdapterFactory()
+            {
+            }
+
+            virtual ObservablePtr make(const Parameters & parameters, const Kinematics & kinematics, const Options & options) const
+            {
+                return ObservablePtr(new FormFactorRatioAdapter<Transition_>(_name, _process, parameters, kinematics, options, _form_factor_numerator, _form_factor_denominator));
+            }
+    };
 }
 
 #endif
