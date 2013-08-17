6e0d7a41c71e29e601647eb5a35cdfaa961df569
Danny van Dyk <danny.van.dyk@gmail.com>
2013-08-17 13:16:01 +0200

    [b-decays] Adjust B->pilnu to use b->u Wilson coefficients

--- eos/b-decays/b-to-pi-l-nu.hh
+++ eos/b-decays/b-to-pi-l-nu.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2013, 2014 Danny van Dyk
+ * Copyright (c) 2013, 2014, 2015 Danny van Dyk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
