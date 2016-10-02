bb2040772ae48418148a57da8c18ea2ef125bf91
Danny van Dyk <danny.van.dyk@gmail.com>
2016-10-02 10:53:58 +0200

    [manual] Fix commandline examples wrt new qualified name syntax

--- manual/examples/propagate-uncertainty-btotaunu.bash
+++ manual/examples/propagate-uncertainty-btotaunu.bash
@@ -3,7 +3,7 @@ eos-propagate-uncertainty \
     --global-option form-factors BCL2008 \
     --vary "CKM::abs(V_ub)"          2e-3   5e-3  --prior gaussian 4.19e-3 4.45e-3 4.71e-3 \
     --vary "decay-constant::B_u"     0.167  0.209 --prior gaussian 0.181   0.188   0.195   \
-    --observable "B_u->lnu::BR,l=tau" \
+    --observable "B_u->lnu::BR;l=tau" \
     --workers 4 \
     --samples 100000 \
     --output /tmp/unc_btotaunu.hdf5
