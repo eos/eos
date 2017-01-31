782255243881c12daf2a44cc36a00bfd1e7345cf
Danny van Dyk <danny.van.dyk@gmail.com>
2017-01-31 19:43:54 +0100

    [clients] Convert eos-list-signal-pdfs to new CLI library

--- src/clients/eos-list-signal-pdfs.cc
+++ src/clients/eos-list-signal-pdfs.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2016 Danny van Dyk
+ * Copyright (c) 2016, 2017 Danny van Dyk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -17,59 +17,193 @@
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "cli_handler.hh"
+#include "cli_error.hh"
+
 #include <eos/signal-pdf.hh>
 #include <eos/utils/destringify.hh>
 #include <eos/utils/instantiation_policy-impl.hh>
 #include <eos/utils/log.hh>
 #include <eos/utils/parameters.hh>
+#include <eos/utils/qualified-name.hh>
 #include <eos/utils/stringify.hh>
 
-#include <cmath>
-#include <iomanip>
 #include <iostream>
-#include <vector>
 
 using namespace eos;
 
-class DoUsage
+using std::cerr;
+using std::cout;
+using std::endl;
+
+struct CommandLine :
+    cli::DefaultHandler
 {
-    private:
-        std::string _what;
+    virtual std::string app_name() const { return "eos-list-signal-pdfs"; }
 
-    public:
-        DoUsage(const std::string & what) :
-            _what(what)
-        {
-        }
+    virtual std::string app_synopsis() const
+    {
+        return "A commandline client to list the available Signal PDFs implemented in EOS.";
+    }
 
-        const std::string & what() const
-        {
-            return _what;
-        }
+    virtual std::string app_description() const { return ""; }
+
+    // filter options
+    cli::Group g_filter_options;
+    cli::StringListArg a_filter_by_name;
+    cli::StringListArg a_filter_by_prefix;
+
+    CommandLine() :
+        g_filter_options(main_options_section(), "Filter Options", "Options that filter out specific constraints"),
+        a_filter_by_name(&g_filter_options,   "filter-by-name",   'n', "add a filter for the full constraint name"),
+        a_filter_by_prefix(&g_filter_options, "filter-by-prefix", 'p', "add a filter for the constraint prefixes")
+    {
+    }
 };
 
-class CommandLine :
-    public InstantiationPolicy<CommandLine, Singleton>
+struct Filter
 {
-    public:
-        CommandLine()
+    std::set<qnp::Name> names;
+    std::set<qnp::Prefix> prefixes;
+
+    Filter(const CommandLine & cmd) :
+        names(cmd.a_filter_by_name.begin_args(), cmd.a_filter_by_name.end_args()),
+        prefixes(cmd.a_filter_by_prefix.begin_args(), cmd.a_filter_by_prefix.end_args())
+    {
+    }
+
+    bool empty()
+    {
+        return names.empty() && prefixes.empty();
+    }
+
+    bool operator() (const std::pair<const QualifiedName, const SignalPDFEntry *> & arg)
+    {
+        if (prefixes.find(arg.first.prefix_part()) != prefixes.end())
+            return true;
+
+        if (names.find(arg.first.name_part()) != names.end())
+            return true;
+
+        return false;
+    }
+};
+
+struct Printer
+{
+    void print(const SignalPDFEntry * rhs)
+    {
+        cout << rhs->name() << endl;
+
+        cout << "    " << rhs->description() << endl;
+
+        cout << endl;
+
+        for (const auto & k : rhs->kinematic_ranges())
         {
+            cout.fill(' ');
+
+            cout.width(20);
+            cout << std::right;
+            cout << k.name;
+            cout << std::left;
+
+            cout.width(0);
+            cout << " [ ";
+
+            cout.width(10);
+            cout << k.min;
+
+            cout.width(0);
+            cout << " , ";
+
+            cout.width(10);
+            cout << k.max;
+
+            cout.width(0);
+            cout << " ] : ";
+
+            cout << k.description;
+
+            cout << endl;
         }
 
-        void parse(int argc, char ** argv)
+        cout << endl;
+    }
+};
+
+int main(int argc, char ** argv)
+{
+    try
+    {
+        CommandLine cmdline;
+        cmdline.run(argc, argv, "eos-list-signal-pdfs");
+
+        if (cmdline.a_help.specified())
         {
-            Log::instance()->set_program_name("eos-list-signal-pdfs");
-
-            std::shared_ptr<Kinematics> kinematics(new Kinematics);
-
-            for (char ** a(argv + 1), ** a_end(argv + argc) ; a != a_end ; ++a)
+            if (cmdline.begin_usage_lines() != cmdline.end_usage_lines())
             {
-                std::string argument(*a);
-
-                throw DoUsage("Unknown command line argument: " + argument);
+                cout << "usage: ";
+                for (cli::Handler::UsageLineConstIterator u_begin(cmdline.begin_usage_lines()), u(u_begin), u_end(cmdline.end_usage_lines()) ;
+                        u != u_end ; ++u)
+                {
+                    if (u != u_begin)
+                        cout << "       ";
+                    cout << cmdline.app_name() << " " << *u << endl;
+                }
             }
+            cout << endl;
+            cout << cmdline;
+            return EXIT_SUCCESS;
         }
-};
+        else if (cmdline.a_version.specified())
+        {
+            cout << "0.0";
+            cout << endl;
+            return EXIT_SUCCESS;
+        }
+
+        SignalPDFs signal_pdfs;
+        Filter filter(cmdline);
+        Printer printer;
+
+        if (filter.empty())
+        {
+            for (auto s : signal_pdfs)
+                printer.print(s.second);
+
+            return EXIT_SUCCESS;
+        }
+
+        for (auto s : signal_pdfs)
+        {
+            if (! filter(s))
+                continue;
+
+            printer.print(s.second);
+        }
+    }
+    catch (const cli::DoHelp & h)
+    {
+        if (h.message.empty())
+            cout << "Usage: " << argv[0] << " COMMAND [ARGS]" << endl;
+        else
+            cerr << "Usage error: " << h.message << endl;
+
+        return EXIT_FAILURE;
+    }
+    catch (const Exception & e)
+    {
+        cerr << endl;
+        cerr << "Error:" << endl;
+        cerr << "  * " << e.what() << endl;
+//        cerr << "  * " << e.backtrace("\n  * ") << e.message() << " (" << e.what() << ")" << endl;
+        cerr << endl;
+        return EXIT_FAILURE;
+    }
+}
+
+#if 0
 
 int
 main(int argc, char * argv[])
@@ -108,3 +242,4 @@ main(int argc, char * argv[])
 
     return EXIT_SUCCESS;
 }
+#endif
