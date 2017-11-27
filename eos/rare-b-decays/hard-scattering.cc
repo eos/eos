76f02612534cb6f1e85206dcff62a7a582782792
Danny van Dyk <danny.van.dyk@gmail.com>
2017-11-27 14:38:31 +0100

    [rare-b-decays] Add hard scattering kernels for numerical evaluation of QCDF integrals

--- eos/rare-b-decays/hard-scattering.cc
+++ eos/rare-b-decays/hard-scattering.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2010 Danny van Dyk
+ * Copyright (c) 2010, 2016 Danny van Dyk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -17,12 +17,19 @@
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <eos/rare-b-decays/charm-loops.hh>
 #include <eos/rare-b-decays/hard-scattering.hh>
+#include <eos/utils/power_of.hh>
 
 #include <gsl/gsl_sf_dilog.h>
 
 namespace eos
 {
+    inline double lcda_tw2(const double & u, const double & a_1, const double & a_2)
+    {
+        return 6.0 * u * (1.0 - u) * (1.0 + a_1 * 3.0 * (2.0 * u - 1.0) + a_2 * 3.0 / 2.0 * (5.0 * power_of<2>(2.0 * u - 1.0) - 1.0));
+    }
+
     complex<double>
     HardScattering::I1(const double & s, const double & u, const double & m_q, const double & m_B)
     {
@@ -98,5 +105,174 @@ namespace eos
         }
 
         return 1.0 + 2.0 * m_q2 / (ubar * (m_B2 - s)) * (LxpLxm - LypLym);
+    }
+
+    complex<double>
+    HardScattering::t_perp_s0(const double & u, const double & m_q, const double & m_B)
+    {
+        const double ub = 1 - u;
+        const double m2 = power_of<2>(m_q);
+        const double m_B2 = power_of<2>(m_B);
+        double a, a2, sign, sq;
+        complex<double> dilogArg, dilog1, dilog2;
+        complex<double> LxpLxm;
+
+        gsl_sf_result res_re, res_im;
+
+        if (m_q > 0) { // m != 0
+            if (1 - 4 * m2/(m_B2 - u * m_B2) > 0) {
+                sq = std::sqrt(1 - 4 * m2/(m_B2 - u * m_B2));
+                a  = (1 - sq)/(1 + sq);
+                LxpLxm = -1./3 * power_of<2>(M_PI) + std::log(a) * (std::log(a) + complex<double>(0, M_PI)) +
+                        gsl_sf_dilog(-a) + gsl_sf_dilog(-1./a);
+            } else {
+                a2 = 4. * m2/(m_B2 - u * m_B2) - 1;
+                a  = std::sqrt(a2);
+                if (a2 - 1 > 0)
+                    sign = +1.;
+                else
+                    sign = -1.;
+                dilogArg = complex<double>((a2 - 1)/(a2 + 1), -2 * a/(a2 + 1));
+                gsl_sf_complex_dilog_e(abs(dilogArg), arg(dilogArg), &res_re, &res_im);
+                dilog1 = complex<double>(res_re.val, res_im.val);
+                dilogArg = complex<double>((a2 - 1)/(a2 + 1), +2. * a/(a2 + 1));
+                gsl_sf_complex_dilog_e(abs(dilogArg), arg(dilogArg), &res_re, &res_im);
+                dilog2 = complex<double>(res_re.val, res_im.val);
+                sq = std::atan(2 *a/(a2 - 1));
+                LxpLxm = -1./3 * power_of<2>(M_PI) - sq * (sq - M_PI * sign) + dilog1 + dilog2;
+            }
+            return 4.0 / ub * (1.0 + 2.0 * m2 / ub / m_B2 * LxpLxm);
+        } else {     // m == 0
+            return complex<double>(4.0 / ub, 0);
+        }
+    }
+
+    complex<double>
+    HardScattering::t_perp(const double & s, const double & u, const double & m_q,
+                           const double & m_B, const double & m_M)
+    {
+        if (s == 0)
+            return t_perp_s0(u, m_q, m_B);
+
+        const double ub = 1.0 - u;
+        const double E = 1.0 / (2.0 * m_B) * (power_of<2>(m_B)+ power_of<2>(m_M) - s);
+
+        if (m_q > 0)
+            return 2 * m_B / (ub * E) * I1(s, u, m_q, m_B) +
+                    s / (ub * ub * E * E) * (CharmLoops::B0(ub * power_of<2>(m_B)+ u * s, m_q) - CharmLoops::B0(s, m_q));
+        return 2 * m_B / (ub * E) * I1(s, u, m_q, m_B);
+    }
+
+    complex<double>
+    HardScattering::t_par(const double & s, const double & u, const double & m_q,
+                          const double & m_B, const double & m_M)
+    {
+        const double ub = 1.0 - u;
+        const double E = 1. / (2 * m_B) * (power_of<2>(m_B)+ power_of<2>(m_M) - s);
+
+        if (m_q > 0)
+            return 2 * m_B / (ub * E) * I1(s, u, m_q, m_B) +
+                   (ub * power_of<2>(m_B) + u * s) / (ub * ub * E * E)
+                   * (CharmLoops::B0(ub * power_of<2>(m_B)+ u * s, m_q) - CharmLoops::B0(s, m_q));
+        return 2 * m_B / (ub * E) * I1(s, u, m_q, m_B);
+    }
+
+    double
+    HardScattering::j0(const double & s, const double & u, const double & m_B, const double & a_1, const double & a_2)
+    {
+        const double ubar = 1.0 - u;
+        const double s_hat = s / power_of<2>(m_B);
+
+        return lcda_tw2(u, a_1, a_2) / (ubar + u * s_hat);
+    }
+
+    complex<double>
+    HardScattering::j1(const double & s, const double & u, const double & m_q, const double & m_B,
+            const double & a_1, const double & a_2)
+    {
+        const double ubar = 1.0 - u;
+
+        return lcda_tw2(u, a_1, a_2) / ubar * I1(s, u, m_q, m_B);
+    }
+
+    complex<double>
+    HardScattering::j2(const double & s, const double & u, const double & m_q, const double & m_B,
+            const double & a_1, const double & a_2)
+    {
+        const double ubar = 1.0 - u, ubar2 = ubar * ubar;
+        const double m_B2 = m_B * m_B;
+
+        return lcda_tw2(u, a_1, a_2) * (CharmLoops::B0(ubar * m_B2 + u * s, m_q) - CharmLoops::B0(s, m_q)) / ubar2;
+    }
+
+    complex<double>
+    HardScattering::j2_massless(const double & s, const double & u, const double & m_B,
+            const double & a_1, const double & a_2)
+    {
+        const double ubar = 1.0 - u, ubar2 = ubar * ubar;
+        const double s_hat = s / power_of<2>(m_B);
+
+        return lcda_tw2(u, a_1, a_2) * std::log(s_hat / (ubar + u * s_hat)) / ubar2;
+    }
+
+    complex<double>
+    HardScattering::j3(const double & s, const double & u, const double & m_q, const double & m_B,
+            const double & a_1, const double & a_2)
+    {
+        const double ubar = 1.0 - u;
+        const double s_hat = s / (m_B * m_B);
+
+        return HardScattering::j2(s, u, m_q, m_B, a_1, a_2) * (ubar + u * s_hat);
+    }
+
+    complex<double>
+    HardScattering::j3_massless(const double & s, const double & u, const double & m_B,
+            const double & a_1, const double & a_2)
+    {
+        const double ubar = 1.0 - u, ubar2 = ubar * ubar;
+        const double s_hat = s / power_of<2>(m_B);
+
+        return HardScattering::j2_massless(s, u, m_B, a_1, a_2) * (ubar + u * s_hat);
+    }
+
+    complex<double>
+    HardScattering::j4(const double & s, const double & u, const double & m_q, const double & m_B,
+            const double & mu, const double & a_1, const double & a_2)
+    {
+        const double ubar = 1.0 - u;
+
+        return lcda_tw2(u, a_1, a_2) * CharmLoops::h(mu, ubar * power_of<2>(m_B) + u * s, m_q);
+    }
+
+    complex<double>
+    HardScattering::j5(const double & s, const double & u, const double & m_q, const double & m_B,
+            const double & mu, const double & a_1, const double & a_2)
+    {
+        const double ubar = 1.0 - u;
+        const double s_hat = s / power_of<2>(m_B);
+
+        return lcda_tw2(u, a_1, a_2) / (ubar + u * s_hat) * CharmLoops::h(mu, ubar * power_of<2>(m_B) + u * s, m_q);
+    }
+
+    complex<double>
+    HardScattering::j6(const double & s, const double & u, const double & m_q, const double & m_B,
+            const double & mu, const double & a_1, const double & a_2)
+    {
+        // cf. [BFS2004], eq. (52): this integral does not involve the lcda
+        // itself, but its first inverse partial moment, as the weight function.
+        const double weight = power_of<2>(u) * (3.0 + a_1 * (-9.0 + 12.0 * u) +
+                a_2 * (18.0 - 60.0 * u + 45.0 * power_of<2>(u)));
+        const double ubar = 1.0 - u;
+
+        return weight * CharmLoops::h(mu, ubar * power_of<2>(m_B) + u * s, m_q);
+    }
+
+    double
+    HardScattering::j7(const double & s, const double & u, const double & m_B, const double & a_1,
+            const double & a_2)
+    {
+        const double ubar = 1.0 - u;
+
+        return lcda_tw2(u, a_1, a_2) / power_of<2>(ubar + u * s / power_of<2>(m_B));
     }
 }
--- eos/rare-b-decays/hard-scattering.hh
+++ eos/rare-b-decays/hard-scattering.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2010 Danny van Dyk
+ * Copyright (c) 2010, 2016 Danny van Dyk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -28,13 +28,172 @@ namespace eos
 
     struct HardScattering
     {
-        // cf. [BFS2001], Eqs. (30)-(32), p. 8
-        // @s    : Dilepton invariant mass
-        // @u    : Relative contribution of the quark (versus ubar = 1-u for the
-        //         antiquark) to the light meson's energy.
-        // @m_q  : Mass of the internal loop quark
-        // @m_B  : Mass of the B meson
+        /*!
+         * cf. [BFS2001], Eqs. (30)-(32), p. 8
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         */
         static complex<double> I1(const double & s, const double & u, const double & m_q, const double & m_B);
+
+        /*!
+         * Two-particle light-cone distribution amplitude up to 2nd order
+         *         in Gegenbauer expansion of light mesons
+         * cf. [BFS2001], Eq. (48)
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the
+                 antiquark) to the light meson's energy.
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static double LCDA_2pt(const double & u, const double & a_1, const double & a_2);
+
+        /*!
+         * t_perp for q^2 = 0; cf. [BFS2001], Eq. (33)
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         */
+        static complex<double> t_perp_s0(const double & u, const double & m_q, const double & m_B);
+
+        /*!
+         * t_perp; cf. [BFS2001], Eq. (27)
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         * @param m_M Mass of the K(K*) meson
+         */
+        static complex<double> t_perp(const double & s, const double & u, const double & m_q,
+                                      const double & m_B, const double & m_M);
+
+        /*!
+         * t_par; cf. [BFS2001], Eq. (28)
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         * @param m_M Mass of the K(K*) meson
+         */
+        static complex<double> t_par(const double & s, const double & u, const double & m_q,
+                                     const double & m_B, const double & m_M);
+
+        /*!
+         * [BFS2001], Eq. (26), term \propto C_8_eff
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_B Mass of the B meson
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static double j0(const double & s, const double & u, const double & m_B, const double & a_1,
+                const double & a_2);
+
+        /*!
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static complex<double> j1(const double & s, const double & u, const double & m_q, const double & m_B,
+                const double & a_1, const double & a_2);
+
+        /*!
+         * [BFS2001], Eq. (25)
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static complex<double> j2(const double & s, const double & u, const double & m_q, const double & m_B,
+                const double & a_1, const double & a_2);
+
+        /*!
+         * [BFS2001], Eq. (25)
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_B Mass of the B meson
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static complex<double> j2_massless(const double & s, const double & u, const double & m_B,
+                const double & a_1, const double & a_2);
+
+        /*!
+         * [BFS2001], Eq. (25)
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static complex<double> j3(const double & s, const double & u, const double & m_q, const double & m_B,
+                const double & a_1, const double & a_2);
+
+        /*!
+         * [BFS2001], Eq. (25)
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_B Mass of the B meson
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static complex<double> j3_massless(const double & s, const double & u, const double & m_B,
+                const double & a_1, const double & a_2);
+
+        /*!
+         * [BFS2001], Eq. (26), term \propto C1 - C6
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         * @param mu Renormalization scale
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static complex<double> j4(const double & s, const double & u, const double & m_q, const double & m_B,
+                const double & mu, const double & a_1, const double & a_2);
+
+        /*!
+         * [BFS2001], Eq. (23), term \propto C_8_eff
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         * @param mu Renormalization scale
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static complex<double> j5(const double & s, const double & u, const double & m_q, const double & m_B,
+                const double & mu, const double & a_1, const double & a_2);
+
+        /*!
+         * [BFS2001], Eq. (23), term \propto C_8_eff
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_q Mass of the internal loop quark
+         * @param m_B Mass of the B meson
+         * @param mu Renormalization scale
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static complex<double> j6(const double & s, const double & u, const double & m_q, const double & m_B,
+                const double & mu, const double & a_1, const double & a_2);
+
+        /*!
+         * [BFS2004], Eq. (52), term \propto C_8_eff
+         * @param s Dilepton invariant mass
+         * @param u Relative contribution of the quark (versus ubar = 1-u for the antiquark) to the light meson's energy.
+         * @param m_B Mass of the B meson
+         * @param a_1 First Gegenbauer coefficient
+         * @param a_2 Second Gegenbauer coefficient
+         */
+        static double j7(const double & s, const double & u, const double & m_B, const double & a_1, const double & a_2);
     };
 }
 
