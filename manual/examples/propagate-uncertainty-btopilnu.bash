e3f7cf68b1adae576fe5dd921a1af20e7eb881a8
Danny van Dyk <danny.van.dyk@gmail.com>
2018-03-05 18:17:26 +0100

    [manual] Change the B->pilnu example for uncertainty propagation to use a differential BR

--- manual/examples/propagate-uncertainty-btopilnu.bash
+++ manual/examples/propagate-uncertainty-btopilnu.bash
@@ -1,7 +1,22 @@
 eos-propagate-uncertainty \
-    --kinematics s_min  0.0 \
-    --kinematics s_max 12.0 \
-    --observable "B->pilnu::BR;l=e,form-factors=BCL2008" \
+    --kinematics s  0.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  0.25 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  0.50 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  0.75 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  1.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  1.50 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  2.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  2.50 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  3.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  3.50 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  4.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  6.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  7.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  8.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s  9.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s 10.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s 11.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
+    --kinematics s 12.00 --observable "B->pilnu::dBR/ds;l=e,form-factors=BCL2008,model=CKMScan" \
     --pmc-input /tmp/pmc_monolithic_btopi+ff.hdf5 0 10000 \
     --pmc-sample-directory "/data/final/" \
     --output /tmp/unc_btopilnu.hdf5
