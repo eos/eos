df452ba07a90eda7461ff5e5f5d8ccc07d838235
Danny van Dyk <danny.van.dyk@gmail.com>
2018-03-09 10:18:45 +0100

    [travis] Avoid conditional exits by using bash features

--- .travis/build-and-test.bash
+++ .travis/build-and-test.bash
@@ -1,19 +1,22 @@
 #/bin/bash
 
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+
 function build_and_test_ubuntu() {
     pushd /src
-    ./autogen.bash || exit 1
+    ./autogen.bash
     popd
     pushd /build
     export CXXFLAGS="-O2 -g"
     /src/configure \
         --enable-pmc \
         --enable-python \
-        --prefix=/usr \
-        || exit 1
-    make distcheck -j2 DISTCHECK_CONFIGURE_FLAGS="--enable-pmc --enable-python --prefix=/usr" VERBOSE=1 || exit 1
-    make install || exit 1
-    make -C /src/manual/examples examples || exit 1
+        --prefix=/usr
+    make distcheck -j2 DISTCHECK_CONFIGURE_FLAGS="--enable-pmc --enable-python --prefix=/usr" VERBOSE=1
+    make install
+    make -C /src/manual/examples examples
     popd
 }
 
@@ -24,17 +27,16 @@ function build_and_coverage_ubuntu() {
     /src/configure \
         --enable-pmc \
         --enable-python \
-        --prefix=/usr \
-        || exit 1
-    make all -j2 || exit 1
-    make check -j2 || exit 1
-    lcov --directory . --capture --output-file /tmp/coverage.info || exit 1
+        --prefix=/usr
+    make all -j2
+    make check -j2
+    lcov --directory . --capture --output-file /tmp/coverage.info
     popd
     lcov \
         --remove /tmp/coverage.info '/usr/*' '/test/*' \
-        --output-file /tmp/coverage.info || exit 1
+        --output-file /tmp/coverage.info
     pushd /src
-    coveralls-lcov --repo-token ${COVERALLS_TOKEN} /tmp/coverage.info || exit 1
+    coveralls-lcov --repo-token ${COVERALLS_TOKEN} /tmp/coverage.info
     popd
 }
 
