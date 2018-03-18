5ff3445f4a9164fbb7e6c5e46997f5ff4678f781
Danny van Dyk <danny.van.dyk@gmail.com>
2018-03-18 15:31:02 +0100

    [manual] Document outputting the mode information from eos-find-mode

--- manual/examples/find-mode-btopi-ff.bash
+++ manual/examples/find-mode-btopi-ff.bash
@@ -1,10 +1,10 @@
 eos-find-mode \
     --global-option model CKMScan \
     --global-option form-factors BCL2008 \
-    --scan "CKM::abs(V_ub)"          2e-3   5e-3  --prior flat \
-    --scan "B->pi::f_+(0)@BCL2008"   0      1     --prior flat \
-    --scan "B->pi::b_+^1@BCL2008"  -20    +20     --prior flat \
-    --scan "B->pi::b_+^2@BCL2008"  -20    +20     --prior flat \
+    --scan     "CKM::abs(V_ub)"          2e-3   5e-3  --prior flat \
+    --nuisance "B->pi::f_+(0)@BCL2008"   0      1     --prior flat \
+    --nuisance "B->pi::b_+^1@BCL2008"  -20    +20     --prior flat \
+    --nuisance "B->pi::b_+^2@BCL2008"  -20    +20     --prior flat \
     --constraint "B->pi::f_+@IKMvD-2014" \
     --constraint "B^0->pi^+lnu::BR@BaBar-2010B" \
     --constraint "B^0->pi^+lnu::BR@Belle-2010A" \
@@ -12,4 +12,5 @@ eos-find-mode \
     --constraint "B^0->pi^+lnu::BR@Belle-2013A" \
     --starting-point { 3.5e-3 0.31 0 0 } \
     --max-iterations 1000 \
-    --target-precision 1e-8
+    --target-precision 1e-8 \
+    --output /tmp/mode-btopipi-ff.yaml
