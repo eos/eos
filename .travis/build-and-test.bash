69599ae8fd3f24f64b83a2fa96abaf70856407ba
Danny van Dyk <danny.van.dyk@gmail.com>
2018-04-17 19:20:04 +0200

    [travis] Automatically build and deploy deb packages

--- .travis/build-and-test.bash
+++ .travis/build-and-test.bash
@@ -8,13 +8,19 @@ function build_and_test_ubuntu() {
     ./autogen.bash
     popd
     pushd /build
-    export CXXFLAGS="-O2 -g"
+    export CXXFLAGS="-O2 -g -march=x86-64" # build for generic x86-64 architecture
     CONFIGURE_FLAGS="--enable-pmc --enable-python --prefix=/usr"
     /src/configure ${CONFIGURE_FLAGS}
     make distcheck -j2 DISTCHECK_CONFIGURE_FLAGS="${CONFIGURE_FLAGS}" VERBOSE=1
     make install
     export PYTHONPATH+=":$(make print-pythondir)"
     make -C /src/manual/examples examples
+    if [[ -n ${TAG} ]] && [[ "g++" == ${CXX} ]]; then
+        echo Building debian package for ${OS}
+        export DESTDIR=/tmp/eos-${TAG#v}
+        make deb DESTDIR=${DESTDIR} OS=${OS}
+        package_cloud push eos/eos/ubuntu/${OS} /tmp/eos-${TAG#v}.deb
+    fi
     popd
 }
 
@@ -44,10 +50,16 @@ shift 1
 export CXX=${1}
 shift 1
 
+export TAG=${1}
+shift 1
+
 echo "==========="
 ${CXX} --version
 
 [[ -n ${COVERALLS_TOKEN} ]] || echo 'Skipping coverage report since $COVERALLS_TOKEN is empty'
+echo "==========="
+
+[[ -n ${PACKAGECLOUD_TOKEN} ]] || echo 'Skipping packagecloud.io deployment since $PACKAGECLOUD_TOKEN is empty'
 echo "==========="
 
 if [[ "xenial" == ${OS} ]] || [[ "bionic" == ${OS} ]]; then
