cd0075dc7acabe29beb2d1294cb98ce53c26576e
Danny van Dyk <danny.van.dyk@gmail.com>
2018-04-22 14:03:18 +0200

    [eos] Remove constraint 'B^0_s->mu^+mu^-::BR_limit@LHCb-CMS-2011-Bayes'
    Due to its use of the deprecated AmorosoTripleLimitConstraintEntry class.

--- eos/constraint.cc
+++ eos/constraint.cc
@@ -2091,24 +2091,6 @@ namespace eos
             Options{ { "q", "d"  }, { "l", "mu" } },
             +0.26, +0.10, -0.08, +0.03, -0.03
         };
-
-
-        /*
-         * LHCb and CMS Collaboration
-         *
-         * Data taken from [LHCb:2011A]
-         */
-        // use the data from the Bayes-Heinrich method
-        // the mode is not at zero, but around 3.1e-9
-        static const AmorosoTripleLimitConstraintEntry Bzero_to_dimuon_LHCb_CMS_2011_Bayes
-        {
-            "B^0_s->mu^+mu^-::BR_limit@LHCb-CMS-2011-Bayes",
-            "B_q->ll::BR@Untagged",
-            Kinematics{ },
-            Options{ { "q", "s"  }, { "l", "mu" } },
-            0.0, 0.132749474699e-8, 0.446663009589e-8, 0.932149816388e-8,
-            6.4184393253e-09, 8.1583565997e-01, 1.8230347158
-        };
         ///@}
 
         ///@name 2012 Data
@@ -5009,10 +4991,6 @@ namespace eos
             ValueType{ "B^0->K^*0mu^+mu^-::F_L[1.00,6.00]@LHCb-2011", &entries::Bzero_to_Kstarzero_dimuon_F_L_1_to_6_LHCb_2011 },
             ValueType{ "B^0->K^*0mu^+mu^-::F_L[14.18,16.00]@LHCb-2011", &entries::Bzero_to_Kstarzero_dimuon_F_L_14dot18_to_16_LHCb_2011 },
             ValueType{ "B^0->K^*0mu^+mu^-::F_L[16.00,19.21]@LHCb-2011", &entries::Bzero_to_Kstarzero_dimuon_F_L_16_to_19dot21_LHCb_2011 },
-
-            // limit on B^0_s -> mu^+ mu^-
-            // LHCb + CMS
-            ValueType{ "B^0_s->mu^+mu^-::BR_limit@LHCb-CMS-2011-Bayes", &entries::Bzero_to_dimuon_LHCb_CMS_2011_Bayes },
 
             /* 2012 */
             // BaBar
