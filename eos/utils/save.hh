4bbd637c937f98d21094e4d1f1c4e5fc2aa74aa3
Ahmet Kokulu <ahmet.kokulu@tum.de>
2018-05-03 17:59:33 +0200

    [utils] Make SwitchOption saveable

--- eos/utils/save.hh
+++ eos/utils/save.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2010, 2011 Danny van Dyk
+ * Copyright (c) 2010, 2011, 2018 Danny van Dyk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
