d2ec045b08e1052e971a71ccddcfa90818a9f218
Nico Gubernari <nicogubernari@gmail.com>
2019-05-24 09:46:17 +0200

    [form-factors] Move declarations of struct BToV in the cc file

--- eos/form-factors/analytic-b-to-v-lcsr.cc
+++ eos/form-factors/analytic-b-to-v-lcsr.cc
@@ -24,6 +24,33 @@ namespace eos
 {
     namespace lcsr
     {
+        struct BToRho
+        {
+            constexpr static const char * V    = "rho";
+            constexpr static const char * m_V  = "mass::rho^+";
+            constexpr static const char * f_V  = "decay-constant::rho";
+            constexpr static const char   q_v  = 'u';
+            constexpr static const double chi2 = 1.0;
+        };
+
+        struct BToKstar
+        {
+            constexpr static const char * V    = "K^*";
+            constexpr static const char * m_V  = "mass::K^*_d";
+            constexpr static const char * f_V  = "B->K^*::f_Kstar_par";
+            constexpr static const char   q_v  = 's';
+            constexpr static const double chi2 = 1.0;
+        };
+
+        struct BToDstar
+        {
+            constexpr static const char * V    = "D^*";
+            constexpr static const char * m_V  = "mass::D^*_d";
+            constexpr static const char * f_V  = "decay-constant::D^*";
+            constexpr static const char   q_v  = 'c';
+            constexpr static const double chi2 = 1.0;
+        };
+
         // B -> rho
         constexpr const char * BToRho::V;
         constexpr const char * BToRho::m_V;
