893f916e7ae75c7c779273cf52de3c3669e86e66
Danny van Dyk <danny.van.dyk@gmail.com>
2020-03-31 00:14:18 +0200

    [python] Add class eos.data.Prediction

--- python/eos/data/pypmc.py
+++ python/eos/data/pypmc.py
@@ -229,3 +229,71 @@ class PMCSampler:
             yaml.dump(description, description_file, default_flow_style=False)
         _np.save(os.path.join(path, 'samples.npy'), samples)
         _np.save(os.path.join(path, 'weights.npy'), weights)
+
+
+class Prediction:
+    def __init__(self, path):
+        """ Read a Prediction object from disk.
+
+        :param path: Path to the storage location.
+        :type path: str
+        """
+        if not os.path.exists(path) or not os.path.isdir(path):
+            raise RuntimeError('Path {} does not exist or is not a directory'.formt(path))
+
+        f = os.path.join(path, 'description.yaml')
+        if not os.path.exists(f) or not os.path.isfile(f):
+            raise RuntimeError('Description file {} does not exist or is not a file'.format(f))
+
+        with open(f, 'r') as df:
+            description = yaml.load(df, Loader=yaml.SafeLoader)
+
+        if not description['type'] == 'Prediction':
+            raise RuntimeError('Path {} not pointing to a Prediction'.format(path))
+
+        self.type = 'Prediction'
+        self.varied_parameters = description['observables']
+
+        f = os.path.join(path, 'samples.npy')
+        if not os.path.exists(f) or not os.path.isfile(f):
+            raise RuntimeError('Samples file {} does not exist or is not a file'.format(f))
+        self.samples = _np.load(f)
+
+        f = os.path.join(path, 'weights.npy')
+        if not os.path.exists(f) or not os.path.isfile(f):
+            raise RuntimeError('Weights file {} does not exist or is not a file'.format(f))
+        self.weights = _np.load(f)
+
+
+    @staticmethod 
+    def create(path, observables, samples, weights):
+        """ Write a new Prediction object to disk.
+
+        :param path: Path to the storage location, which will be created as a directory.
+        :type path: str
+        :param observables: Observables as a 1D array of shape (O, ).
+        :type observables: list or iterable of eos.Observable
+        :param samples: Samples as a 2D array of shape (N, O).
+        :type samples: 2D numpy array
+        :param weights: Weights on a linear scale as a 1D array of shape (N, ).
+        :type weights: 1D numpy array
+        """
+        description = {}
+        description['version'] = eos.version()
+        description['type'] = 'Prediction'
+        description['observables'] = [{
+            'name': str(o.name()),
+            'kinematics': { k.name(): float(k) for k in o.kinematics() }
+        } for o in observables]
+
+        if not samples.shape[1] == len(observables):
+            raise RuntimeError('Shape of samples {} incompatible with number of observables {}'.format(samples.shape, len(observables)))
+
+        if not samples.shape[0] == weights.shape[0]:
+            raise RuntimeError('Shape of weights {} incompatible with shape of samples {}'.format(weights.shape, samples.shape))
+
+        os.makedirs(path, exist_ok=True)
+        with open(os.path.join(path, 'description.yaml'), 'w') as description_file:
+            yaml.dump(description, description_file, default_flow_style=False)
+        _np.save(os.path.join(path, 'samples.npy'), samples)
+        _np.save(os.path.join(path, 'weights.npy'), weights)
