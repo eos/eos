1bf9523b29b1f1311461c06c6475116a1fbef9dc
Danny van Dyk <danny.van.dyk@gmail.com>
2020-04-03 11:29:13 +0200

    [eos] Disable default GSL error handler before calls to main()

---
+++ eos/init.cc
@@ -0,0 +1,36 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2020 Danny van Dyk
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <gsl/gsl_errno.h>
+
+namespace eos
+{
+    namespace init
+    {
+        /*
+         * Turn off the default GSLerror handler.
+         */
+        void disable_gsl_error_handler() __attribute__((constructor(101)));
+
+        void disable_gsl_error_handler()
+        {
+            gsl_set_error_handler_off();
+        }
+    }
+}
