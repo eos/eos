ffb2f87b720c5bd4d4ccd738997162a2814d8f0f
Danny van Dyk <danny.van.dyk@gmail.com>
2020-04-17 10:26:09 +0200

    [form-factors] Acknowledge Dom Leljak in the B->pi FF source code

--- eos/form-factors/analytic-b-to-pi.hh
+++ eos/form-factors/analytic-b-to-pi.hh
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2014, 2015 Danny van Dyk
+ * Copyright (c) 2019, 2020 Domagoj Leljak
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
