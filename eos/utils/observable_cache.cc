963eaed2b798b6502a3c7c6acbfcc89f6b076cc6
Danny van Dyk <danny.van.dyk@gmail.com>
2020-06-24 23:05:43 +0200

    [utils] Add multithreading support to ObservableCache
    Github: resolves #9

--- eos/utils/observable_cache.cc
+++ eos/utils/observable_cache.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2011, 2016 Danny van Dyk
+ * Copyright (c) 2011, 2016, 2020 Danny van Dyk
  * Copyright (c) 2011 Frederik Beaujean
  *
  * This file is part of the EOS project. EOS is free software;
@@ -18,9 +18,11 @@
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include <eos/utils/log.hh>
 #include <eos/utils/observable_cache.hh>
 #include <eos/utils/observable_set.hh>
 #include <eos/utils/private_implementation_pattern-impl.hh>
+#include <eos/utils/thread_pool.hh>
 
 #include <algorithm>
 #include <limits>
@@ -83,13 +85,50 @@ namespace eos
     void
     ObservableCache::update()
     {
+#ifdef EOS_ENABLE_PMC
+#else
         // evaluate all observables
+        std::vector<Ticket> tickets;
+        tickets.reserve(_imp->predictions.size());
+#endif
+
         auto p = _imp->predictions.begin();
 
         for (auto o = _imp->observables.begin(), o_end = _imp->observables.end() ; o != o_end ; ++o, ++p)
         {
+#ifdef EOS_ENABLE_PMC
+            // evaluate observables one by one
             *p = (*o)->evaluate();
+#else
+            // parallelize the evaluation of the observables
+            // since the EOS builtin PMC sampler is parallelized,
+            // parallelizing the ObservableCache can and will lead
+            // to a deadlock.
+            auto f = [=]() {
+                try
+                {
+                    *p = (*o)->evaluate();
+                }
+                catch (eos::Exception & e)
+                {
+                    Log::instance()->message("ObservableCache::update", ll_error)
+                        << "Exception encountered when evaluating observable '" << (*o)->name() << "[" << (*o)->kinematics().as_string() << "];" << (*o)->options().as_string() << "': "
+                        << e.what();
+                    *p = std::numeric_limits<double>::quiet_NaN();
+
+                }
+            };
+            tickets.push_back(ThreadPool::instance()->enqueue(std::function<void (void)>(f)));
+#endif
         }
+
+#ifdef EOS_ENABLE_PMC
+#else
+        for (auto ticket : tickets)
+        {
+            ticket.wait();
+        }
+#endif
     }
 
     Parameters
