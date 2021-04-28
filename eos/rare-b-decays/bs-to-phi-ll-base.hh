55800b78cce09e9d35b66b33ff518441d62ebdaf
Méril Reboud <meril.reboud@cern.ch>
2021-04-28 16:24:00 +0200

    [rare-b-decays] Implement B_s->phi ll decays

---
+++ eos/rare-b-decays/bs-to-phi-ll-base.hh
@@ -0,0 +1,83 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2021 Méril Reboud
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef MASTER_GUARD_EOS_RARE_B_DECAYS_BS_TO_PHI_LL_BASE_HH
+#define MASTER_GUARD_EOS_RARE_B_DECAYS_BS_TO_PHI_LL_BASE_HH 1
+
+#include <eos/utils/model.hh>
+#include <eos/form-factors/mesonic.hh>
+#include <eos/rare-b-decays/bs-to-phi-ll.hh>
+
+namespace eos
+{
+    class BsToPhiDilepton::AmplitudeGenerator :
+        public ParameterUser
+    {
+        public:
+            std::shared_ptr<Model> model;
+            std::shared_ptr<FormFactors<PToV>> form_factors;
+
+            UsedParameter mu;
+            UsedParameter alpha_e;
+            UsedParameter g_fermi;
+            UsedParameter tau;
+
+            UsedParameter m_B;
+            UsedParameter m_V;
+            UsedParameter m_l;
+
+            bool cp_conjugate;
+            std::string lepton_flavour;
+
+            AmplitudeGenerator(const Parameters &, const Options &);
+
+            double s_hat(const double & q2) const;
+            double beta_l(const double & q2) const;
+            double energy(const double & q2) const;
+            double lambda(const double & q2) const;
+
+            virtual ~AmplitudeGenerator();
+            virtual BsToPhiDilepton::Amplitudes amplitudes(const double & q2) const = 0;
+    };
+
+    struct BsToPhiDilepton::DipoleFormFactors
+    {
+        complex<double> calT_perp_left;
+        complex<double> calT_perp_right;
+        complex<double> calT_parallel;
+    };
+
+    template <typename Tag_> class BsToPhiDileptonAmplitudes;
+
+    namespace tag
+    {
+        /*
+         * Approaches that work at small q^2, or large recoil.
+         */
+        //struct BFS2004;
+        struct GvDV2020;
+
+        /*
+         * Approaches that work at large q^2, or low recoil.
+         */
+        //struct GP2004;
+    }
+}
+
+#endif
---
+++ eos/rare-b-decays/bs-to-phi-ll-gvdv2020_TEST.cc
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2021 Méril Reboud
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <test/test.hh>
+#include <eos/observable.hh>
+#include <eos/rare-b-decays/bs-to-phi-ll.hh>
+#include <eos/utils/complex.hh>
+
+#include <iostream>
+
+using namespace test;
+using namespace eos;
+
+class BsToPhiDileptonGvDV2020Test :
+    public TestCase
+{
+    public:
+    BsToPhiDileptonGvDV2020Test() :
+        TestCase("bs_to_phi_dilepton_GvDV2020_test")
+    {
+    }
+
+    virtual void run() const
+    {
+        {
+            Parameters p = Parameters::Defaults();
+
+            p["mass::B_s"]                               = 5.36688;
+            p["mass::phi"]                               = 1.019461;
+            p["mass::J/psi"]                             = 3.0969;
+            p["mass::psi(2S)"]                           = 3.6860;
+            p["mass::D^0"]                               = 1.86723;
+            p["b->sccbar::t_0"]                          = 9.0;
+            p["b->sccbar::t_s"]                          = -17.4724;
+            p["b->sccbar::chiOPE@GvDV2020"]              = 1.81e-4;
+
+            p["B_s->phiccbar::Re{alpha_0^perp}@GvDV2020"]  = 0.0002;
+            p["B_s->phiccbar::Im{alpha_0^perp}@GvDV2020"]  = 0.0003;
+            p["B_s->phiccbar::Re{alpha_1^perp}@GvDV2020"]  = 0.0004;
+            p["B_s->phiccbar::Im{alpha_1^perp}@GvDV2020"]  = 0.0005;
+            p["B_s->phiccbar::Re{alpha_2^perp}@GvDV2020"]  = 0.0006;
+            p["B_s->phiccbar::Im{alpha_2^perp}@GvDV2020"]  = 0.0007;
+            p["B_s->phiccbar::Re{alpha_0^para}@GvDV2020"]  = 0.0008;
+            p["B_s->phiccbar::Im{alpha_0^para}@GvDV2020"]  = 0.0009;
+            p["B_s->phiccbar::Re{alpha_1^para}@GvDV2020"]  = 0.0010;
+            p["B_s->phiccbar::Im{alpha_1^para}@GvDV2020"]  = 0.0011;
+            p["B_s->phiccbar::Re{alpha_2^para}@GvDV2020"]  = 0.0012;
+            p["B_s->phiccbar::Im{alpha_2^para}@GvDV2020"]  = 0.0013;
+            p["B_s->phiccbar::Re{alpha_0^long}@GvDV2020"]  = 0.0014;
+            p["B_s->phiccbar::Im{alpha_0^long}@GvDV2020"]  = 0.0015;
+            p["B_s->phiccbar::Re{alpha_1^long}@GvDV2020"]  = 0.0016;
+            p["B_s->phiccbar::Im{alpha_1^long}@GvDV2020"]  = 0.0017;
+            p["B_s->phiccbar::Re{alpha_2^long}@GvDV2020"]  = 0.0018;
+            p["B_s->phiccbar::Im{alpha_2^long}@GvDV2020"]  = 0.0019;
+
+            p["b->s::Re{c7}"] = -0.3370422989 + 0.1;
+            p["b->s::Im{c7}"] = 0.2;
+            p["b->s::Re{c7'}"] = 0.3;
+            p["b->s::Im{c7'}"] = 0.4;
+            p["b->s::c8"] = -0.1827530948;
+            p["b->smumu::Re{c9}"] = 4.294489364 + 1;
+            p["b->smumu::Im{c9}"] = 0.5;
+            p["b->smumu::Re{c9'}"] = 2;
+            p["b->smumu::Im{c9'}"] = 1.5;
+            p["b->smumu::Re{c10}"] = -4.196294696 + 3;
+            p["b->smumu::Im{c10}"] = 2.5;
+            p["b->smumu::Re{c10'}"] = 4;
+            p["b->smumu::Im{c10'}"] = 3.5;
+
+            Options oo;
+            oo.set("model",                   "WilsonScan");
+            oo.set("tag",                     "GvDV2020");
+            oo.set("nonlocal-formfactors",    "GvDV2020");
+            oo.set("form-factors",            "BZ2004");
+            oo.set("l",                       "mu");
+
+            static const double eps = 1e-5;
+            static const double q2 = 6.0;
+
+            BsToPhiDilepton d(p, oo);
+            auto amps = d.amplitudes(q2);
+
+
+            std::cout << real(amps.a_long_left)   << std::endl;
+            std::cout << imag(amps.a_long_left)   << std::endl;
+            std::cout << real(amps.a_long_right)  << std::endl;
+            std::cout << imag(amps.a_long_right)  << std::endl;
+            std::cout << real(amps.a_para_left)   << std::endl;
+            std::cout << imag(amps.a_para_left)   << std::endl;
+            std::cout << real(amps.a_para_right)  << std::endl;
+            std::cout << imag(amps.a_para_right)  << std::endl;
+            std::cout << real(amps.a_perp_left)   << std::endl;
+            std::cout << imag(amps.a_perp_left)   << std::endl;
+            std::cout << real(amps.a_perp_right)  << std::endl;
+            std::cout << imag(amps.a_perp_right)  << std::endl;
+
+
+
+            TEST_CHECK_RELATIVE_ERROR(real(amps.a_long_left),  -1.04871e-09, eps);
+            TEST_CHECK_RELATIVE_ERROR(imag(amps.a_long_left),  -2.41479e-10, eps);
+
+            TEST_CHECK_RELATIVE_ERROR(real(amps.a_long_right),  5.26771e-11, eps);
+            TEST_CHECK_RELATIVE_ERROR(imag(amps.a_long_right), -2.95225e-11, eps);
+
+            TEST_CHECK_RELATIVE_ERROR(real(amps.a_para_left),   1.79215e-10, eps);
+            TEST_CHECK_RELATIVE_ERROR(imag(amps.a_para_left),   5.94937e-10, eps);
+
+            TEST_CHECK_RELATIVE_ERROR(real(amps.a_para_right),  8.0494e-10, eps);
+            TEST_CHECK_RELATIVE_ERROR(imag(amps.a_para_right),  7.15354e-10, eps);
+
+            TEST_CHECK_RELATIVE_ERROR(real(amps.a_perp_left),   3.17172e-11, eps);
+            TEST_CHECK_RELATIVE_ERROR(imag(amps.a_perp_left),  -2.52525e-10, eps);
+
+            TEST_CHECK_RELATIVE_ERROR(real(amps.a_perp_right),  3.30606e-10, eps);
+            TEST_CHECK_RELATIVE_ERROR(imag(amps.a_perp_right),  3.87104e-10, eps);
+
+            TEST_CHECK_RELATIVE_ERROR(real(amps.a_time),       -1.10463e-09, eps);
+            TEST_CHECK_RELATIVE_ERROR(imag(amps.a_time),       -2.12580e-10, eps);
+       }
+    }
+} bs_to_phi_dilepton_GvDV2020_test;
