21275b7e99d6e54c881c6ae6117e005f64fa73ec
Méril Reboud <meril.reboud@cern.ch>
2021-10-04 12:24:23 +0200

    [statistics] Add a forward iterator to iterate on the priors and fix indentation

--- eos/statistics/log-posterior.cc
+++ eos/statistics/log-posterior.cc
@@ -23,6 +23,7 @@
 #include <eos/utils/density-impl.hh>
 #include <eos/utils/log.hh>
 #include <eos/utils/power_of.hh>
+#include <eos/utils/private_implementation_pattern-impl.hh>
 
 #include <gsl/gsl_cdf.h>
 
@@ -48,168 +49,187 @@ namespace eos
     {
     }
 
-   bool
-   LogPosterior::add(const LogPriorPtr & prior, bool nuisance)
-   {
-       // clone has correct Parameters object selected
-       LogPriorPtr prior_clone = prior->clone(_parameters);
-       _informative_priors += 1 ? prior->informative() : 0;
+    bool
+    LogPosterior::add(const LogPriorPtr & prior, bool nuisance)
+    {
+        // clone has correct Parameters object selected
+        LogPriorPtr prior_clone = prior->clone(_parameters);
+        _informative_priors += 1 ? prior->informative() : 0;
 
-       // check if param exists already
-       // read out parameters from prior
-       for (auto d = prior->begin(), d_end = prior->end() ; d != d_end ; ++d)
-       {
-           auto result = _parameter_names.insert(d->parameter->name());
-           if (! result.second)
-               return false;
+        // check if param exists already
+        // read out parameters from prior
+        for (auto d = prior->begin(), d_end = prior->end() ; d != d_end ; ++d)
+        {
+            auto result = _parameter_names.insert(d->parameter->name());
+            if (! result.second)
+                return false;
 
-           d->nuisance = nuisance;
-           _parameter_descriptions.push_back(*d);
-       }
+            d->nuisance = nuisance;
+            _parameter_descriptions.push_back(*d);
+        }
 
-       // then add to prior container
-       _priors.push_back(prior_clone);
+        // then add to prior container
+        _priors.push_back(prior_clone);
 
-       return true;
-   }
+        return true;
+    }
 
-   DensityPtr
-   LogPosterior::clone() const
-   {
-       return DensityPtr(private_clone());
-   }
+    DensityPtr
+    LogPosterior::clone() const
+    {
+        return DensityPtr(private_clone());
+    }
 
-   LogPosteriorPtr
-   LogPosterior::old_clone() const
-   {
-       return LogPosteriorPtr(private_clone());
-   }
+    LogPosteriorPtr
+    LogPosterior::old_clone() const
+    {
+        return LogPosteriorPtr(private_clone());
+    }
 
-   LogPosterior *
-   LogPosterior::private_clone() const
-   {
-       // clone log_likelihood
-       LogLikelihood llh = _log_likelihood.clone();
-       LogPosterior * result = new LogPosterior(llh);
+    LogPosterior *
+    LogPosterior::private_clone() const
+    {
+        // clone log_likelihood
+        LogLikelihood llh = _log_likelihood.clone();
+        LogPosterior * result = new LogPosterior(llh);
 
-       // add parameters via prior clones
-       for (const auto & _prior : _priors)
-       {
-           result->add(_prior->clone(result->parameters()));
-       }
+        // add parameters via prior clones
+        for (const auto & _prior : _priors)
+        {
+            result->add(_prior->clone(result->parameters()));
+        }
 
-       // copy proper range for subspace sampling
-       auto j = result->_parameter_descriptions.begin();
-       for (auto i = _parameter_descriptions.begin(), i_end = _parameter_descriptions.end() ; i != i_end ; ++i, ++j)
-       {
-           j->min = i->min;
-           j->max = i->max;
-           j->nuisance = i->nuisance;
-       }
+        // copy proper range for subspace sampling
+        auto j = result->_parameter_descriptions.begin();
+        for (auto i = _parameter_descriptions.begin(), i_end = _parameter_descriptions.end() ; i != i_end ; ++i, ++j)
+        {
+            j->min = i->min;
+            j->max = i->max;
+            j->nuisance = i->nuisance;
+        }
 
-       return result;
-   }
+        return result;
+    }
 
-   double
-   LogPosterior::evaluate() const
-   {
-       return log_posterior();
-   }
+    double
+    LogPosterior::evaluate() const
+    {
+        return log_posterior();
+    }
 
-   Density::Iterator
-   LogPosterior::begin() const
-   {
-       return Density::Iterator(_parameter_descriptions.cbegin());
-   }
+    Density::Iterator
+    LogPosterior::begin() const
+    {
+        return Density::Iterator(_parameter_descriptions.cbegin());
+    }
 
-   Density::Iterator
-   LogPosterior::end() const
-   {
-       return Density::Iterator(_parameter_descriptions.cend());
-   }
+    Density::Iterator
+    LogPosterior::end() const
+    {
+        return Density::Iterator(_parameter_descriptions.cend());
+    }
 
-   Parameters
-   LogPosterior::parameters() const
-   {
-       return _parameters;
-   }
+    Parameters
+    LogPosterior::parameters() const
+    {
+        return _parameters;
+    }
 
-   unsigned
-   LogPosterior::index(const std::string & name) const
-   {
-       unsigned result = 0;
+    unsigned
+    LogPosterior::index(const std::string & name) const
+    {
+        unsigned result = 0;
 
-       for (auto d = _parameter_descriptions.cbegin(), d_end = _parameter_descriptions.cend() ; d != d_end ; ++d, ++result)
-       {
-           if (name == d->parameter->name())
-               return result;
-       }
+        for (auto d = _parameter_descriptions.cbegin(), d_end = _parameter_descriptions.cend() ; d != d_end ; ++d, ++result)
+        {
+            if (name == d->parameter->name())
+                return result;
+        }
 
-       throw InternalError("Implementation<Analysis>::definition: no such parameter '" + name + "'");
-   }
+        throw InternalError("Implementation<Analysis>::definition: no such parameter '" + name + "'");
+    }
 
-   LogLikelihood
-   LogPosterior::log_likelihood() const
-   {
-       return _log_likelihood;
-   }
+    LogLikelihood
+    LogPosterior::log_likelihood() const
+    {
+        return _log_likelihood;
+    }
 
-   double
-   LogPosterior::log_posterior() const
-   {
-       return log_prior() + _log_likelihood();
-   }
+    double
+    LogPosterior::log_posterior() const
+    {
+        return log_prior() + _log_likelihood();
+    }
 
-   double
-   LogPosterior::log_prior() const
-   {
-       if (_priors.empty())
-           throw InternalError("LogPosterior::log_prior(): prior is undefined");
+    double
+    LogPosterior::log_prior() const
+    {
+        if (_priors.empty())
+            throw InternalError("LogPosterior::log_prior(): prior is undefined");
 
-       double result = 0.0;
+        double result = 0.0;
 
-       // all prior components are assumed independent,
-       // thus the logs can be simply added up
-       for (const auto & _prior : _priors)
-       {
-           result += (*_prior)();
-       }
+        // all prior components are assumed independent,
+        // thus the logs can be simply added up
+        for (const auto & _prior : _priors)
+        {
+            result += (*_prior)();
+        }
 
-       return result;
-   }
+        return result;
+    }
 
-   LogPriorPtr
-   LogPosterior::log_prior(const std::string & name) const
-   {
-       LogPriorPtr prior;
+    LogPriorPtr
+    LogPosterior::log_prior(const std::string & name) const
+    {
+        LogPriorPtr prior;
 
-       // loop over all descriptions of the prior pointers
-       for (const auto & _prior : _priors)
-       {
-           for (auto i = _prior->begin(), i_end = _prior->end() ; i != i_end ; ++i)
-           {
-               if (i->parameter->name() == name)
-                   prior = _prior;
-           }
-       }
+        // loop over all descriptions of the prior pointers
+        for (const auto & _prior : _priors)
+        {
+            for (auto i = _prior->begin(), i_end = _prior->end() ; i != i_end ; ++i)
+            {
+                if (i->parameter->name() == name)
+                    prior = _prior;
+            }
+        }
 
-       return prior;
-   }
+        return prior;
+    }
 
-   bool
-   LogPosterior::nuisance(const std::string& par_name) const
-   {
-       unsigned index = this->index(par_name);
+    template <>
+    struct WrappedForwardIteratorTraits<LogPosterior::PriorIteratorTag>
+    {
+        using UnderlyingIterator = std::vector<LogPriorPtr>::const_iterator;
+    };
+    template class WrappedForwardIterator<LogPosterior::PriorIteratorTag, const LogPriorPtr>;
 
-       if (index >= _parameter_descriptions.size())
-       {
-           return false;
-       }
-       else
-       {
-           return _parameter_descriptions[index].nuisance;
-       }
-   }
+    LogPosterior::PriorIterator
+    LogPosterior::begin_priors() const
+    {
+        return _priors.begin();
+    }
+
+    LogPosterior::PriorIterator
+    LogPosterior::end_priors() const
+    {
+        return _priors.end();
+    }
+
+    bool
+    LogPosterior::nuisance(const std::string& par_name) const
+    {
+        unsigned index = this->index(par_name);
+
+        if (index >= _parameter_descriptions.size())
+        {
+            return false;
+        }
+        else
+        {
+            return _parameter_descriptions[index].nuisance;
+        }
+    }
 
     unsigned
     LogPosterior::informative_priors() const
@@ -217,11 +237,11 @@ namespace eos
         return _informative_priors;
     }
 
-   MutablePtr
-   LogPosterior::operator[] (const unsigned & index) const
-   {
-       return _parameter_descriptions[index].parameter;
-   }
+    MutablePtr
+    LogPosterior::operator[] (const unsigned & index) const
+    {
+        return _parameter_descriptions[index].parameter;
+    }
 
     const std::vector<ParameterDescription> &
     LogPosterior::parameter_descriptions() const
