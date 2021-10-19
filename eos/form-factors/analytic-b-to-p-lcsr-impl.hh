24d91f15b42ddbf834e3e304ff564e5cd7a39939
Danny van Dyk <danny.van.dyk@gmail.com>
2021-10-19 12:04:36 +0200

    [form-factors] Add references to B->P,V form factors using B-LCSRs

--- eos/form-factors/analytic-b-to-p-lcsr-impl.hh
+++ eos/form-factors/analytic-b-to-p-lcsr-impl.hh
@@ -9566,6 +9566,16 @@ namespace eos
     {
         return this->_imp->diagnostics();
     }
+
+    template <typename Process_>
+    const std::set<ReferenceName>
+    AnalyticFormFactorBToPLCSR<Process_>::references
+    {
+        "KMO:2005A"_rn,
+        "KMO:2006A"_rn,
+        "FKKM:2008A"_rn,
+        "GKvD:2018A"_rn
+    };
 }
 
 #endif
--- eos/form-factors/analytic-b-to-v-lcsr.hh
+++ eos/form-factors/analytic-b-to-v-lcsr.hh
@@ -26,6 +26,7 @@
 #include <eos/utils/diagnostics.hh>
 #include <eos/utils/parameters.hh>
 #include <eos/utils/options.hh>
+#include <eos/utils/reference-name.hh>
 
 namespace eos
 {
@@ -83,6 +84,11 @@ namespace eos
 
             /* Diagnostics for unit tests */
             Diagnostics diagnostics() const;
+
+            /*!
+             * References used in the computation of our observables.
+             */
+            static const std::set<ReferenceName> references;
     };
 
     extern template class AnalyticFormFactorBToVLCSR<lcsr::BToRho>;
