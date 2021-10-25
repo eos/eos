4bc924fb09de88e8826add5216a660d37ec3d315
Philip Lüghausen <pluegh@gmail.com>
2021-10-25 18:15:46 +0200

    [doc] Update observables.rst generation
     - use jinja2 template to separate logic and representation
     - handle case where latex string is not available
     - handle group titles that require substitution to render as TeX

--- doc/observables.rst.py
+++ doc/observables.rst.py
@@ -1,70 +1,84 @@
-#!/usr/bin/python3
-# vim: set sw=4 sts=4 et tw=120 :
-
 import eos
 import re
-
-observables = eos.Observables()
+from jinja_util import print_template
 
 def latex_to_rst(s):
     return(re.sub(r'\$([^\$]*)\$', r':math:`\1`', s))
 
-all_data = []
-page_title = 'List of Observables'
-print('#' * len(page_title))
-print(page_title)
-print('#' * len(page_title))
-print('\n')
-print('The following is a list of observables that is available in EOS v{}.\n\n'.format(eos.version()))
-for section in observables.sections():
-    section_title = latex_to_rst(section.name())
-    print('*' * len(section_title))
-    print(section_title)
-    print('*' * len(section_title))
+qn_translation = {
+    ord(':'): 'co', ord('@'): 'at', ord('/'): 'sl', ord('_'): 'un',
+    ord('('): 'po', ord(')'): 'pc', ord('+'): 'pp', ord('-'): 'mm',
+    ord('>'): 'to'
+}
 
-    for group in section:
-        group_title = latex_to_rst(group.name())
-        print(group_title)
-        print('=' * len(group_title))
+tex_translation = {
+    'charmonium': r'\Psi', 'gamma': r'\gamma'
+}
 
-        print('\n')
-        translation = {
-            ord(':'): 'co', ord('@'): 'at', ord('/'): 'sl', ord('_'): 'un',
-            ord('('): 'po', ord(')'): 'pc', ord('+'): 'pp', ord('-'): 'mm',
-            ord('>'): 'to'
-        }
-        for qn, _ in group:
-            print('.. _{qn}:'.format(qn=str(qn).translate(translation).lower()))
-        print('.. list-table::')
-        print('   :widths: 50, 25, 25')
-        print('   :header-rows: 1')
-        print('')
-        print('   * - Qualified Name')
-        print('     - Description')
-        print('     - Kinematic Variables')
-        for qn, entry in group:
-            latex = entry.latex()
-            unit_string = None
-            if entry.unit() == eos.Unit.Unity():
-                unit_string = ''
-            else:
-                unit_string = r'\, \left[ {unit_string} \right]'.format(unit_string=entry.unit().latex())
+eos_sections = eos.Observables().sections()
 
-            if 0 == len(latex):
-                continue
+def make_doc_observables(group):
+    "Make structured string data from eos group"
+    doc_obss = []
+    for qn, entry in group:
 
-            entry_name = str(qn)
+        latex = entry.latex()
+
+        unit_string = None
+        if entry.unit() == eos.Unit.Unity():
+            unit_string = ''
+        else:
+            unit_string = r'\, \left[ {unit_string} \right]'.format(unit_string=entry.unit().latex())
+
+        if len(latex) > 0:
             entry_desc = latex_to_rst(latex + unit_string)
-            entry_kv   = ', '.join([('``' + kv + '``') for kv in entry.kinematic_variables()])
-            print('   * - ``{qn}``'.format(qn=entry_name))
-            print('     - :math:`{desc}`'.format(desc=entry_desc))
-            print('     - {kv}'.format(kv=entry_kv))
+        else:
+            entry_desc = r'' # signal latex string n/a
+
+        entry_name = str(qn)
+        entry_kv   = ', '.join([('``' + kv + '``') for kv in entry.kinematic_variables()])
+
+        doc_obss.append({
+            'qn': entry_name,
+            'description': entry_desc,
+            'kinematics': entry_kv,
+            'link_key': entry_name.translate(qn_translation).lower(),
+        })
+    links = []
+    return doc_obss
 
 
-        print('\n\n')
-        group_desc  = latex_to_rst(group.description())
-        print(group_desc)
-        print('\n\n')
+# mirror the eos object structure with strings
+doc_sections = []
+for section in eos_sections:
 
-    print('\n\n')
-print('\n\n')
+    doc_groups = []
+    for group in section:
+
+        doc_obss = make_doc_observables(group)
+        group_title = group.name()
+        # TODO: this substitution is not safe if the group title contains
+        # the substituted names as TeX commands. Example: we substitute gamma
+        # to \gamma, which gives \\gamma if the command is already used somewhere.
+        for k, v in tex_translation.items():
+            group_title = group_title.replace(k, v)
+        doc_group = {
+            'title': latex_to_rst(group_title),
+            'obss': doc_obss,
+            'description': latex_to_rst(group.description()),
+        }
+        doc_groups.append(doc_group)
+
+    doc_section = {
+        'title': latex_to_rst(section.name()),
+        'groups': doc_groups
+    }
+    doc_sections.append(doc_section)
+
+
+print_template(__file__,
+    len = len,
+    eos_version = eos.version(),
+    sections = doc_sections
+)
+
