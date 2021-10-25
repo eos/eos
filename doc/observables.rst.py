235eb434a4dc82a2779a240990b95510599185a8
Philip Lüghausen <pluegh@gmail.com>
2021-10-25 18:15:46 +0200

    [doc] Update generation of 'List of Observables'
     - use jinja2 template to separate logic and representation
     - handle case where latex string is not available
     - fix bug in generating HTML anchors for the observable group,
       rather than the individual observable

--- doc/observables.rst.py
+++ doc/observables.rst.py
@@ -1,70 +1,64 @@
-#!/usr/bin/python3
-# vim: set sw=4 sts=4 et tw=120 :
-
 import eos
 import re
-
-observables = eos.Observables()
+from jinja_util import print_template
 
 def latex_to_rst(s):
     return(re.sub(r'\$([^\$]*)\$', r':math:`\1`', s))
 
-all_data = []
-page_title = 'List of Observables'
-print('#' * len(page_title))
-print(page_title)
-print('#' * len(page_title))
-print('\n')
-print('The following is a list of observables that is available in EOS v{}.\n\n'.format(eos.version()))
-for section in observables.sections():
-    section_title = latex_to_rst(section.name())
-    print('*' * len(section_title))
-    print(section_title)
-    print('*' * len(section_title))
+qn_translation = {
+    ord(':'): 'co', ord('@'): 'at', ord('/'): 'sl', ord('_'): 'un',
+    ord('('): 'po', ord(')'): 'pc', ord('+'): 'pp', ord('-'): 'mm',
+    ord('>'): 'to'
+}
 
+def make_doc_observables(group):
+    "Make structured string data from eos group"
+    observables = []
+    for qn, entry in group:
+        latex = entry.latex()
+
+        unit_string = None
+        if entry.unit() == eos.Unit.Unity():
+            unit_string = ''
+        else:
+            unit_string = r'\, \left[ {unit_string} \right]'.format(unit_string=entry.unit().latex())
+
+        if len(latex) > 0:
+            description = latex_to_rst(latex + unit_string)
+        else:
+            description = r'' # signal latex string n/a
+
+        kinematics = ', '.join([('``' + kv + '``') for kv in entry.kinematic_variables()])
+
+        observables.append({
+            'qn': str(qn),
+            'description': description,
+            'kinematics': kinematics,
+            'link_key': str(qn).translate(qn_translation).lower(),
+        })
+    return observables
+
+
+# mirror the eos object structure with strings
+sections = []
+for section in eos.Observables().sections():
+    groups = []
     for group in section:
-        group_title = latex_to_rst(group.name())
-        print(group_title)
-        print('=' * len(group_title))
+        groups.append({
+            'name': latex_to_rst(group.name()),
+            'observables': make_doc_observables(group),
+            'description': latex_to_rst(group.description()),
+        })
 
-        print('\n')
-        translation = {
-            ord(':'): 'co', ord('@'): 'at', ord('/'): 'sl', ord('_'): 'un',
-            ord('('): 'po', ord(')'): 'pc', ord('+'): 'pp', ord('-'): 'mm',
-            ord('>'): 'to'
-        }
-        for qn, _ in group:
-            print('.. _{qn}:'.format(qn=str(qn).translate(translation).lower()))
-        print('.. list-table::')
-        print('   :widths: 50, 25, 25')
-        print('   :header-rows: 1')
-        print('')
-        print('   * - Qualified Name')
-        print('     - Description')
-        print('     - Kinematic Variables')
-        for qn, entry in group:
-            latex = entry.latex()
-            unit_string = None
-            if entry.unit() == eos.Unit.Unity():
-                unit_string = ''
-            else:
-                unit_string = r'\, \left[ {unit_string} \right]'.format(unit_string=entry.unit().latex())
-
-            if 0 == len(latex):
-                continue
-
-            entry_name = str(qn)
-            entry_desc = latex_to_rst(latex + unit_string)
-            entry_kv   = ', '.join([('``' + kv + '``') for kv in entry.kinematic_variables()])
-            print('   * - ``{qn}``'.format(qn=entry_name))
-            print('     - :math:`{desc}`'.format(desc=entry_desc))
-            print('     - {kv}'.format(kv=entry_kv))
+    sections.append({
+        'name': latex_to_rst(section.name()),
+        'groups': groups
+    })
 
 
-        print('\n\n')
-        group_desc  = latex_to_rst(group.description())
-        print(group_desc)
-        print('\n\n')
+print_template(__file__,
+    len = len,
+    version = eos.version(),
+    sections = sections
+)
 
-    print('\n\n')
-print('\n\n')
