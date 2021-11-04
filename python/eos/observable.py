83eb7cdaee0ae4612636347edb7eb32e11aaf78b
Danny van Dyk <danny.van.dyk@gmail.com>
2021-11-04 20:09:28 +0100

    [python] Improve display of eos.Observables

--- python/eos/observable.py
+++ python/eos/observable.py
@@ -54,28 +54,162 @@ class Observables(_Observables):
         return True
 
     def _repr_html_(self):
-        result = '<table>\n'
+        result = r'''
+        <script>
+            function toggle_group(group_title, id) {
+                var table = group_title.parentNode.parentNode.parentNode.parentNode
+                var query = 'tbody[id="' + id + '"]'
+                var group = table.querySelector(query)
+                if (group.style.visibility == "collapse") {
+                    group.style.visibility = "visible"
+                } else {
+                    group.style.visibility = "collapse"
+                }
+            }
+            function toggle_av(opt_anchor, id) {
+                var query_dots   = 'span.dots[id="' + id + '"]'
+                var query_values = 'span.values[id="' + id + '"]'
+                var dots   = opt_anchor.querySelector(query_dots)
+                var values = opt_anchor.querySelector(query_values)
+                if (dots.style.display == "none") {
+                    dots.style.display   = "inline"
+                    values.style.display = "none"
+                } else {
+                    dots.style.display   = "none"
+                    values.style.display = "inline"
+                }
+            }
+        </script>
+        <style>
+            td.qn     { text-align: left;   }
+            td.sym    { text-align: center; }
+            td.unit   { text-align: right;  }
+            td.optkey { text-align: left;   }
+            td.optav  { text-align: left;   }
+            td.optdef { text-align: left;   }
+        </style>
+        <table>
+            <colgroup>
+                <col width="25%" id="qn"          style="min-width: 200px; text-align: left">
+                <col width="20%" id="symbol"      style="min-width: 200px">
+                <col width="5%"  id="unit"        style="min-width:  50px">
+                <col width="20%" id="kv"          style="min-width: 200px">
+                <col width="10%" id="opt-key"     style="min-width:  75px">
+                <col width="10%" id="opt-allowed" style="min-width:  75px">
+                <col width="10%" id="opt-default" style="min-width:  75px">
+            </colgroup>
+            <thead>
+                <tr>
+                    <th rowspan="2">qualified name</th>
+                    <th rowspan="2">symbol</th>
+                    <th rowspan="2">unit</th>
+                    <th rowspan="2">kinematic<br> variables</th>
+                    <th colspan=3>options</th>
+                </tr>
+                <tr>
+                    <th>key</th>
+                    <th>values</th>
+                    <th>default</th>
+                </tr>
+            </thead>
+        '''
+        group_id = 0
+        observable_id = 0
         for section in self.sections():
             section_entries = 0
-            section_result = '  <tr><th style="text-align:left" colspan=2><big>{section}</big></th></tr>\n'.format(section=section.name())
+            section_result = fr'''
+                <tr>
+                    <th style="text-align:left" colspan=8><big>{section.name()}</big></th>
+                </tr>'''
             for group in section:
                 group_entries = 0
-                group_result = '    <tr><th style="text-align:left" colspan=2>{group}</th></tr>\n'.format(group=group.name())
+                group_result  = fr'''
+                    <tbody>
+                        <tr>
+                            <th style="text-align:left" colspan=8>
+                                <a style="text-decoration: none" onclick="toggle_group(this, 'grp{group_id}')">{group.name()}</a>
+                            </th>
+                        </tr>
+                    </tbody>
+                '''#.format(group=group.name(), id=group_id)
+                group_result += fr'''
+                    <tbody style="visibility:collapse" id="grp{group_id}">
+                    <tr>
+                        <td style="text-align:left" colspan=8>{group.description()}</td>
+                    </tr>
+                '''
                 for qn, entry in group:
                     latex = entry.latex()
-                    unit_string = entry.unit().latex()
-                    if unit_string == '1':
-                        unit_string = ''
+
+                    unit = entry.unit().latex()
+                    if unit == '1':
+                        unit = '&mdash;'
                     else:
-                        unit_string = r"\, \left[ {unit_string} \right]".format(unit_string=unit_string)
+                        unit = r'$$\left[ {unit} \right]$$'.format(unit=unit)
                     if not self.filter_entry(qn):
                         continue
                     if (0 == len(latex)) and not self.showall:
                         continue
-                    group_result += r'      <tr><th><tt style="color:grey">{qn}</tt></th><td style="text-align:left">$${latex}{unit_string}$$</td></tr>'.format(qn=qn,latex=latex,unit_string=unit_string)
-                    group_entries += 1
 
-                group_result += '    <tr><td style="text-align:left" colspan=2>{desc}</td></tr>\n'.format(desc=group.description())
+                    kinematic_variables = r'<br>'.join(['<tt>' + str(kv) + '</tt>' for kv in entry.kinematic_variables()])
+                    if len(kinematic_variables) == 0:
+                        kinematic_variables = '&mdash;'
+
+                    keys           = []
+                    allowed_values = []
+                    default_values = []
+                    for option_idx, option in enumerate(entry.options()):
+                        id  = fr'grp{group_id}-obs{observable_id}-opt{option_idx}'
+                        av  = fr'''<a onclick="toggle_av(this, '{id}')">
+                            <span class="dots"   id="{id}" style="display: inline; text-align: left">...</span>
+                            <span class="values" id="{id}" style="display: none;   text-align: left">
+                        '''
+                        av += '   ' + r'<br/>'.join([fr'''<tt>{av}</tt>''' for av in option.allowed_values])
+                        av += r'''
+                            </span>
+                        </a>'''
+
+                        keys           += [fr'''<tt>{option.key}</tt>''']
+                        default_values += [fr'''<tt>{option.default_value}</tt>''']
+                        allowed_values += [av]
+                    if len(keys) == 0:
+                        keys           += ['&mdash;']
+                        allowed_values += ['&mdash;']
+                        default_values += ['&mdash;']
+
+                    rows = len(keys)
+                    #print(f'qn = {qn}')
+                    #print(f'len(keys) = {len(keys)}')
+                    #print(f'len(allowed_values) = {len(allowed_values)}')
+                    #print(f'len(default_values) = {len(default_values)}')
+                    #print(f'len(spans) = {len(spans)}')
+
+                    group_result += fr'''
+                        <tr>
+                            <th class="qn"     rowspan="{rows}"><tt>{qn}</tt></th>
+                            <td class="sym"    rowspan="{rows}">$${latex}$$</td>
+                            <td class="unit"   rowspan="{rows}">{unit}</td>
+                            <td class="kv"     rowspan="{rows}">{kinematic_variables}</td>
+                            <td class="optkey" rowspan="1">{keys[0]}</td>
+                            <td class="optav"  rowspan="1">{allowed_values[0]}</td>
+                            <td class="optdef" rowspan="1">{default_values[0]}</td>
+                        </tr>
+                    '''
+                    options = list(zip(keys, allowed_values, default_values))
+                    for key, allowed_value, default_value in options[1:]:
+                        group_result += fr'''
+                            <tr>
+                                <td class="optkey" rowspan="1">{key}</td>
+                                <td class="optav"  rowspan="1">{allowed_value}</td>
+                                <td class="optdef" rowspan="1">{default_value}</td>
+                            </tr>
+                        '''
+
+                    group_entries += 1
+                    observable_id += 1
+
+                group_result += '    </tbody>'
+                group_id += 1
                 section_entries += group_entries
                 if group_entries > 0:
                     section_result += group_result
