c13a71f63276ed88479cf0b3550983bba2281a61
Philip Lüghausen <pluegh@gmail.com>
2021-11-05 13:52:41 +0100

    [doc] Improve documentation code
    Using the `if __name__ = '__main__'` pattern allows for the same
    behaviour as forward declaration C++. The resulting code structure
    reflects the data structure very clearly.

--- doc/observables.rst.py
+++ doc/observables.rst.py
@@ -5,17 +5,31 @@ from jinja_util import print_template
 def latex_to_rst(s):
     return(re.sub(r'\$([^\$]*)\$', r':math:`\1`', s))
 
-qn_translation = {
+qn_to_link_map = {
     ord(':'): 'co', ord('@'): 'at', ord('/'): 'sl', ord('_'): 'un',
     ord('('): 'po', ord(')'): 'pc', ord('+'): 'pp', ord('-'): 'mm',
     ord('>'): 'to'
 }
 
+# Mirror the EOS observables hierarchy as structured string data: arrays of
+# dicts. Observables are organised in groups, which are organised in sections.
+
+def make_doc_sections():
+    return [{
+        'name'  : latex_to_rst(section.name()),
+        'groups': make_doc_groups(section)
+        } for section in eos.Observables().sections()]
+
+def make_doc_groups(section):
+    return [{
+        'name'       : latex_to_rst(group.name()),
+        'description': latex_to_rst(group.description()),
+        'observables': make_doc_observables(group),
+        } for group in section]
+
 def make_doc_observables(group):
-    "Make structured string data from eos group"
     observables = []
     for qn, entry in group:
-        latex = entry.latex()
 
         unit_string = None
         if entry.unit() == eos.Unit.Unity():
@@ -23,42 +37,30 @@ def make_doc_observables(group):
         else:
             unit_string = r'\, \left[ {unit_string} \right]'.format(unit_string=entry.unit().latex())
 
+        latex = entry.latex()
         if len(latex) > 0:
             description = latex_to_rst(latex + unit_string)
         else:
             description = r'' # signal latex string n/a
 
-        kinematics = ', '.join([('``' + kv + '``') for kv in entry.kinematic_variables()])
+        qualified_name = str(qn)
+        kinematics     = ', '.join([('``' + kv + '``') for kv in entry.kinematic_variables()])
 
         observables.append({
-            'qn': str(qn),
-            'description': description,
-            'kinematics': kinematics,
-            'link_key': str(qn).translate(qn_translation).lower(),
+            'qualified_name': qualified_name,
+            'description'   : description,
+            'kinematics'    : kinematics,
+            'link_key'      : qualified_name.translate(qn_to_link_map).lower(),
         })
+
     return observables
 
 
-# mirror the eos object structure with strings
-sections = []
-for section in eos.Observables().sections():
-    groups = []
-    for group in section:
-        groups.append({
-            'name': latex_to_rst(group.name()),
-            'observables': make_doc_observables(group),
-            'description': latex_to_rst(group.description()),
-        })
+if __name__ == '__main__':
 
-    sections.append({
-        'name': latex_to_rst(section.name()),
-        'groups': groups
-    })
-
-
-print_template(__file__,
-    len = len,
-    version = eos.version(),
-    sections = sections
-)
+    print_template(__file__,
+        version = eos.version(),
+        sections = make_doc_sections(),
+        len = len,
+    )
 
