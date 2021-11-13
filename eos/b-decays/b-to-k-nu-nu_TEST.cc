33cc6c180dfb766b34fa58fdb5f8e3463e6afa61
Danny van Dyk <danny.van.dyk@gmail.com>
2021-11-13 16:08:13 +0100

    [b-decays] Add new process B->Knunu
    Github: resolves #107

---
+++ eos/b-decays/b-to-k-nu-nu_TEST.cc
@@ -0,0 +1,79 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2021 Danny van Dyk
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <test/test.hh>
+#include <eos/observable.hh>
+#include <eos/b-decays/b-to-psd-nu-nu.hh>
+#include <eos/utils/complex.hh>
+#include <eos/utils/wilson-polynomial.hh>
+
+#include <array>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <string>
+#include <vector>
+
+
+using namespace test;
+using namespace eos;
+
+class BToKDineutrinoTest :
+    public TestCase
+{
+    public:
+        BToKDineutrinoTest() :
+            TestCase("b_to_k_nu_nu_test")
+        {
+        }
+
+        virtual void run() const
+        {
+            {
+                Parameters p = Parameters::Defaults();
+                p["CKM::abs(V_tb)"]           =  1.00;
+                p["CKM::abs(V_ts)"]           =  4.00e-2;
+                p["B->K::alpha^f+_0@BSZ2015"] = +3.2909e-01;
+                p["B->K::alpha^f+_1@BSZ2015"] = -8.6695e-01;
+                p["B->K::alpha^f+_2@BSZ2015"] = +6.0957e-03;
+                p["mass::B_u"]                =  5.2796;
+                p["mass::K_u"]                =  4.9368e-01;
+
+                Options oo
+                {
+                    { "model",        "CKM"     },
+                    { "form-factors", "BSZ2015" },
+                    { "D",            "s"       },
+                    { "q",            "u"       },
+                    { "I",            "1/2"     }
+                };
+
+                BToPseudoscalarDineutrino d(p, oo);
+
+                const double eps = 1e-4;
+
+                TEST_CHECK_RELATIVE_ERROR(2.05845e-06, d.integrated_branching_ratio( 0.00,  8.00), eps);
+                TEST_CHECK_RELATIVE_ERROR(1.68211e-06, d.integrated_branching_ratio( 8.00, 16.00), eps);
+                TEST_CHECK_RELATIVE_ERROR(0.59978e-06, d.integrated_branching_ratio(16.00, 22.90), eps);
+
+                TEST_CHECK_RELATIVE_ERROR(4.34034e-06, d.integrated_branching_ratio( 0.00, 22.90), eps);
+            }
+        }
+} b_to_k_nu_nu_test;
---
+++ eos/b-decays/b-to-psd-nu-nu.cc
@@ -0,0 +1,271 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2021 Danny van Dyk
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <eos/form-factors/form-factors.hh>
+#include <eos/b-decays/b-to-psd-nu-nu.hh>
+#include <eos/utils/destringify.hh>
+#include <eos/utils/integrate.hh>
+#include <eos/utils/kinematic.hh>
+#include <eos/utils/options.hh>
+#include <eos/utils/model.hh>
+#include <eos/utils/power_of.hh>
+#include <eos/utils/private_implementation_pattern-impl.hh>
+#include <eos/utils/quantum-numbers.hh>
+
+#include <map>
+#include <string>
+
+namespace eos
+{
+    template <>
+    struct Implementation<BToPseudoscalarDineutrino>
+    {
+        std::shared_ptr<Model> model;
+
+        std::shared_ptr<FormFactors<PToP>> form_factors;
+
+        Parameters parameters;
+
+        QuarkFlavorOption opt_D;
+        QuarkFlavorOption opt_q;
+        SpecifiedOption opt_I;
+
+        UsedParameter m_B;
+
+        UsedParameter tau_B;
+
+        UsedParameter m_P;
+
+        UsedParameter g_fermi;
+
+        UsedParameter alpha_e;
+
+        UsedParameter hbar;
+
+        const double isospin_factor;
+
+        UsedParameter mu;
+
+        static const std::vector<OptionSpecification> options;
+
+        std::function<complex<double> ()> lambda_t;
+        std::function<WilsonCoefficients<wc::SBNuNu> ()> wc;
+
+        GSL::QAGS::Config int_config;
+
+        bool cp_conjugate;
+
+        // { D, q, I } -> { process, m_B, m_P, c_I }
+        // D: s, d; the quark flavour in the weak transition
+        // q: u, d, s: the spectar quark flavour
+        // I: 1, 0, 1/2: the total isospin of the daughter meson
+        // process: string that can be used to obtain the form factor
+        // B: name of the B meson
+        // P: name of the daughter meson
+        // c_I: isospin factor by which the amplitudes are multiplied
+        static const std::map<std::tuple<QuarkFlavor, QuarkFlavor, std::string>, std::tuple<std::string, std::string, std::string, double>> process_map;
+
+        inline std::string _process() const
+        {
+            const auto D = opt_D.value();
+            const auto q = opt_q.value();
+            const std::string I = opt_I.value();
+            const auto p = process_map.find(std::make_tuple(D, q, I));
+
+            if (p == process_map.end())
+                throw InternalError("Unsupported combination of D=" + opt_D.str() + ", q=" + opt_q.str() + ", I=" + I);
+
+            return std::get<0>(p->second);
+        }
+
+        inline std::string _B() const
+        {
+            const auto D = opt_D.value();
+            const auto q = opt_q.value();
+            const std::string I = opt_I.value();
+            const auto p = process_map.find(std::make_tuple(D, q, I));
+
+            if (p == process_map.end())
+                throw InternalError("Unsupported combination of D=" + opt_D.str() + ", q=" + opt_q.str() + ", I=" + I);
+
+            return std::get<1>(p->second);
+        }
+
+        inline std::string _P() const
+        {
+            const auto D = opt_D.value();
+            const auto q = opt_q.value();
+            const std::string I = opt_I.value();
+            const auto p = process_map.find(std::make_tuple(D, q, I));
+
+            if (p == process_map.end())
+                throw InternalError("Unsupported combination of D=" + opt_D.str() + ", q=" + opt_q.str() + ", I=" + I);
+
+            return std::get<2>(p->second);
+        }
+
+        inline double _isospin_factor() const
+        {
+            const auto D = opt_D.value();
+            const auto q = opt_q.value();
+            const std::string I = opt_I.value();
+            const auto p = process_map.find(std::make_tuple(D, q, I));
+
+            if (p == process_map.end())
+                throw InternalError("Unsupported combination of D=" + opt_D.str() + ", q=" + opt_q.str() + ", I=" + I);
+
+            return std::get<3>(p->second);
+        }
+
+
+        Implementation(const Parameters & p, const Options & o, ParameterUser & u) :
+            model(Model::make(o.get("model", "SM"), p, o)),
+            parameters(p),
+            opt_D(o, options, "D"),
+            opt_q(o, options, "q"),
+            opt_I(o, options, "I"),
+            m_B(p["mass::" + _B()], u),
+            tau_B(p["life_time::" + _B()], u),
+            m_P(p["mass::" + _P()], u),
+            g_fermi(p["WET::G_Fermi"], u),
+            alpha_e(p["QED::alpha_e(m_b)"], u),
+            hbar(p["QM::hbar"], u),
+            isospin_factor(_isospin_factor()),
+            mu(p[opt_D.str() + "b" + "nunu::mu"], u),
+            int_config(GSL::QAGS::Config().epsrel(0.5e-3)),
+            cp_conjugate(destringify<bool>(o.get("cp-conjugate", "false")))
+        {
+            form_factors = FormFactorFactory<PToP>::create(_process() + "::" + o.get("form-factors", "BSZ2015"), p, o);
+
+            if (! form_factors.get())
+                throw InternalError("Form factors not found!");
+
+            switch (opt_D.value())
+            {
+                case QuarkFlavor::strange:
+                    lambda_t  = [*this] () { return model->ckm_tb() * std::conj(model->ckm_ts()); };
+                    wc        = [*this] () { return model->wet_sbnunu(cp_conjugate); };
+                    break;
+
+                default:
+                    throw InternalError("Unexpected quark flavor: '" + opt_D.str() + "'");
+            }
+
+            u.uses(*form_factors);
+            u.uses(*model);
+        }
+
+        // differential decay width
+        double differential_decay_width(const double & q2) const
+        {
+            const double m_B = this->m_B(), m_B2 = m_B * m_B;
+            const double m_P = this->m_P(), m_P2 = m_P * m_P;
+            const double lambda = eos::lambda(m_B2, m_P2, q2), sqrt_lambda = std::sqrt(lambda);
+
+            const auto wc = this->wc();
+
+            const double f_p = form_factors->f_p(q2);
+
+            // using different normalization than [FLS:2021A], eq. (1)
+            // note that eq. (1) is a Lagrangian, while we use the Hamiltonian definition
+            const double norm = power_of<2>(4.0 * g_fermi() * alpha_e / (2.0 * M_PI)) / 2.0 * std::norm(lambda_t())
+                // remainder as in [FLS:2021A], eq. (8), except for moving the q2 factor into the square brackets
+                * sqrt_lambda / (power_of<3>(4.0 * M_PI * m_B));
+
+            // first term in square brackets in [FLS:2021A], eq. (8)
+            const double contr_vector = lambda / 24.0 * f_p * f_p * std::norm(wc.cL() +  wc.cR());
+
+            // assume the production of 3 diagonal neutrino flavors (nu_i nubar_i)
+            return 3.0 * norm * contr_vector;
+        }
+
+        // differential branching_ratio
+        double differential_branching_ratio(const double & s) const
+        {
+            return differential_decay_width(s) * tau_B / hbar;
+        }
+    };
+
+    const std::map<std::tuple<QuarkFlavor, QuarkFlavor, std::string>, std::tuple<std::string, std::string, std::string, double>>
+    Implementation<BToPseudoscalarDineutrino>::Implementation::process_map
+    {
+        { { QuarkFlavor::strange, QuarkFlavor::up,   "1/2" }, { "B->K",     "B_u", "K_u",  1.0                  } },
+        { { QuarkFlavor::strange, QuarkFlavor::down, "1/2" }, { "B->K",     "B_d", "K_d",  1.0                  } }
+    };
+
+    const std::vector<OptionSpecification>
+    Implementation<BToPseudoscalarDineutrino>::options
+    {
+        Model::option_specification(),
+        FormFactorFactory<PToP>::option_specification(),
+        { "D", { "s" },             "s" },
+        { "q", { "u", "d" },        "u" },
+        { "I", { "1", "0", "1/2" }, ""  },
+    };
+
+    BToPseudoscalarDineutrino::BToPseudoscalarDineutrino(const Parameters & parameters, const Options & options) :
+        PrivateImplementationPattern<BToPseudoscalarDineutrino>(new Implementation<BToPseudoscalarDineutrino>(parameters, options, *this))
+    {
+    }
+
+    BToPseudoscalarDineutrino::~BToPseudoscalarDineutrino()
+    {
+    }
+
+    double
+    BToPseudoscalarDineutrino::differential_branching_ratio(const double & q2) const
+    {
+        return _imp->differential_branching_ratio(q2);
+    }
+
+    double
+    BToPseudoscalarDineutrino::integrated_branching_ratio(const double & q2_min, const double & q2_max) const
+    {
+        std::function<double (const double &)> f = std::bind(&Implementation<BToPseudoscalarDineutrino>::differential_branching_ratio,
+                _imp.get(), std::placeholders::_1);
+
+        return integrate<GSL::QAGS>(f, q2_min, q2_max, _imp->int_config);
+    }
+
+    const std::string
+    BToPseudoscalarDineutrino::description = "\
+    The decay B->P nu nu, where both B=(B qbar) and P=(U qbar) are pseudoscalars, and l=e,mu,tau is a lepton.";
+
+    const std::string
+    BToPseudoscalarDineutrino::kinematics_description_q2 = "\
+    The invariant mass of the nu-nubar pair in GeV^2.";
+
+    const std::set<ReferenceName>
+    BToPseudoscalarDineutrino::references
+    {
+        "BGNS:2014A"_rn
+    };
+
+    std::vector<OptionSpecification>::const_iterator
+    BToPseudoscalarDineutrino::begin_options()
+    {
+        return Implementation<BToPseudoscalarDineutrino>::options.cbegin();
+    }
+
+    std::vector<OptionSpecification>::const_iterator
+    BToPseudoscalarDineutrino::end_options()
+    {
+        return Implementation<BToPseudoscalarDineutrino>::options.cend();
+    }
+}
