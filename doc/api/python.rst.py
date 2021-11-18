b8c43b920f77de6b60b60872dafd5e3c76b9cb22
Danny van Dyk <danny.van.dyk@gmail.com>
2021-11-18 08:34:48 +0100

    [doc] Restructure generating the plot description format

--- doc/api/python.rst.py
+++ doc/api/python.rst.py
@@ -1,16 +1,19 @@
 import eos
 from jinja_util import print_template
 
-plot_types_attrs = eos.plot.Plotter.plot_types
-
 # get docstrings from classes
-plot_type_descs = dict()
-for key in plot_types_attrs:
-    PlotterClass = plot_types_attrs[key]
-    desc = PlotterClass.__doc__.split('\n')[0]
-    plot_type_descs.update({key: desc})
+plot_types = {}
+for key, PlotterClass in eos.plot.Plotter.plot_types.items():
+    oneline = PlotterClass.__doc__.split('\n')[0]
+    content = PlotterClass._api_doc if '_api_doc' in dir(PlotterClass) else ''
+    plot_types.update({
+        key: {
+            'oneline': oneline,
+            'content': content
+        }
+    })
 
 print_template(__file__,
-    plot_type_descs = plot_type_descs
+    plot_types = plot_types
 )
 
