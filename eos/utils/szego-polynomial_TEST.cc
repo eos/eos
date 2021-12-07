98584b5d920849e441a7625e8a2c5fd8a89a5906
Méril Reboud <meril.reboud@cern.ch>
2021-12-07 18:15:54 +0100

    [utils] Re-indent correctly, add the #ifndef header, add an evaluation for complex inputs and test it

--- eos/utils/szego-polynomial_TEST.cc
+++ eos/utils/szego-polynomial_TEST.cc
@@ -37,45 +37,56 @@ class SzegoPolynomialTest :
 
         virtual void run() const
         {
-			// test case
-			{
-				SzegoPolynomial<5u> p{
-					2.47895, // norm of the measure
-					{ 0.762914, -0.7988, 0.807686, -0.81062, 0.811894 }
-				};
+            // test case
+            {
+                SzegoPolynomial<5u> p{
+                    2.47895, // norm of the measure
+                    { 0.762914, -0.7988, 0.807686, -0.81062, 0.811894 }
+                };
 
-				{
-					const auto [p0, p1, p2, p3, p4, p5] = p(-0.1);
+                {
+                    const auto [p0, p1, p2, p3, p4, p5] = p(-0.1);
 
-					TEST_CHECK_RELATIVE_ERROR(p0, +0.6351351032391984, 1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p1, -0.847745,           1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p2, +1.54489,            1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p3, -2.82388,            1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p4, +5.166081,           1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p5, -9.464102,           1.0e-5);
-				}
+                    TEST_CHECK_RELATIVE_ERROR(p0, +0.6351351032391984, 1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p1, -0.847745,           1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p2, +1.54489,            1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p3, -2.82388,            1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p4, +5.166081,           1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p5, -9.464102,           1.0e-5);
+                }
 
-				{
-					const auto [p0, p1, p2, p3, p4, p5] = p(+0.0);
+                {
+                    const auto [p0, p1, p2, p3, p4, p5] = p(+0.0);
 
-					TEST_CHECK_RELATIVE_ERROR(p0, +0.6351351032391984, 1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p1, -0.749503,           1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p2, +1.304458,           1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p3, -2.237009,           1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p4, +3.834085,           1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p5, -6.577731,           1.0e-5);
-				}
+                    TEST_CHECK_RELATIVE_ERROR(p0, +0.6351351032391984, 1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p1, -0.749503,           1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p2, +1.304458,           1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p3, -2.237009,           1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p4, +3.834085,           1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p5, -6.577731,           1.0e-5);
+                }
 
-				{
-					const auto [p0, p1, p2, p3, p4, p5] = p(+0.1);
+                {
+                    const auto [p0, p1, p2, p3, p4, p5] = p(+0.1);
 
-					TEST_CHECK_RELATIVE_ERROR(p0, +0.6351351032391984, 1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p1, -0.651261,           1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p2, +1.09668,            1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p3, -1.76188,            1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p4, +2.82970,            1.0e-5);
-					TEST_CHECK_RELATIVE_ERROR(p5, -4.54691,            1.0e-5);
-				}
-			}
+                    TEST_CHECK_RELATIVE_ERROR(p0, +0.6351351032391984, 1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p1, -0.651261,           1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p2, +1.09668,            1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p3, -1.76188,            1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p4, +2.82970,            1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR(p5, -4.54691,            1.0e-5);
+                }
+
+                {
+                    const auto [p0, p1, p2, p3, p4, p5] = p(complex<double>(0.4, 0.916515139));
+
+                    TEST_CHECK_RELATIVE_ERROR(real(p0), +0.6351351032391984,                    1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR_C(p1, complex<double>(-0.35653430,  0.90040363),  1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR_C(p2, complex<double>(-0.70241504, -0.85660005),  1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR_C(p3, complex<double>( 1.06106731, -0.16955667),  1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR_C(p4, complex<double>(-0.31896585,  0.83869890),  1.0e-5);
+                    TEST_CHECK_RELATIVE_ERROR_C(p5, complex<double>(-0.40550250, -0.45614032),  1.0e-5);
+                }
+            }
         }
 } szego_polynomial_test;
