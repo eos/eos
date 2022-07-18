cff66891d1d5d23a3230b24375b88ed0f42a1551
Philip Lueghausen <pluegh@gmail.com>
2022-07-18 18:12:13 +0200

    [form-factors] Refactor B-LCDAs
    - Make B-LCDA abstract base class
    - Refactor exponential model into derived class

--- eos/form-factors/analytic-b-to-v-lcsr-impl.hh
+++ eos/form-factors/analytic-b-to-v-lcsr-impl.hh
@@ -35,8 +35,8 @@
 #include <eos/utils/stringify.hh>
 
 #include <functional>
-
 #include <iostream>
+#include <memory>
 
 namespace eos
 {
@@ -76,7 +76,7 @@ namespace eos
         // renormalization scale
         UsedParameter mu;
 
-        BMesonLCDAs b_lcdas;
+        std::shared_ptr<BMesonLCDAs> b_lcdas;
 
         // switches to enable/disable certain contributions
         SwitchOption opt_2pt;
@@ -120,7 +120,7 @@ namespace eos
             s0_1_T23B(p[stringify(Process_::B) + "->" + stringify(Process_::V) + "::s_0^T23B,1@B-LCSR"], u),
             M2(p[stringify(Process_::B) + "->" + stringify(Process_::V) + "::M^2@B-LCSR"], u),
             mu(p[stringify(Process_::B) + "->" + stringify(Process_::V) + "::mu@B-LCSR"], u),
-            b_lcdas(p, o + Options{ { "q", stringify(Process_::q_s) } }), // operator+ is ordered!
+            b_lcdas(BMesonLCDAs::make("exponential", p, o + Options{ { "q", stringify(Process_::q_s) } })), // operator+ is ordered!
             opt_2pt(o, "2pt", { "tw2+3", "all", "off" }, "all"),
             opt_3pt(o, "3pt", { "tw3+4", "all", "off" }, "all"),
             switch_2pt_phi(1.0),
@@ -129,7 +129,7 @@ namespace eos
             opt_method(o, "method", { "borel", "dispersive" }, "borel"),
             switch_borel(opt_method.value() == "borel")
         {
-            u.uses(b_lcdas);
+            u.uses(*b_lcdas);
 
             switch (Process_::q_v)
             {
@@ -249,133 +249,133 @@ namespace eos
         inline
         double phi_plus(const double & omega) const
         {
-            return switch_2pt_phi * b_lcdas.phi_plus(omega);
+            return switch_2pt_phi * b_lcdas->phi_plus(omega);
         }
 
         inline
         double phi_bar(const double & omega) const
         {
-            return switch_2pt_phi * b_lcdas.phi_bar(omega);
+            return switch_2pt_phi * b_lcdas->phi_bar(omega);
         }
 
         inline
         double phi_bar_d1(const double & omega) const
         {
-            return switch_2pt_phi * b_lcdas.phi_bar_d1(omega);
+            return switch_2pt_phi * b_lcdas->phi_bar_d1(omega);
         }
 
         inline
         double g_plus(const double & omega) const
         {
-            return switch_2pt_g * b_lcdas.g_plus(omega);
+            return switch_2pt_g * b_lcdas->g_plus(omega);
         }
 
         inline
         double g_plus_d1(const double & omega) const
         {
-            return switch_2pt_g * b_lcdas.g_plus_d1(omega);
+            return switch_2pt_g * b_lcdas->g_plus_d1(omega);
         }
 
         inline
         double g_plus_d2(const double & omega) const
         {
-            return switch_2pt_g * b_lcdas.g_plus_d2(omega);
+            return switch_2pt_g * b_lcdas->g_plus_d2(omega);
         }
 
         inline
         double g_bar(const double & omega) const
         {
-            return switch_2pt_g * b_lcdas.g_bar(omega);
+            return switch_2pt_g * b_lcdas->g_bar(omega);
         }
 
         inline
         double g_bar_d1(const double & omega) const
         {
-            return switch_2pt_g * b_lcdas.g_bar_d1(omega);
+            return switch_2pt_g * b_lcdas->g_bar_d1(omega);
         }
 
         inline
         double g_bar_d2(const double & omega) const
         {
-            return switch_2pt_g * b_lcdas.g_bar_d2(omega);
+            return switch_2pt_g * b_lcdas->g_bar_d2(omega);
         }
 
         inline
         double g_bar_d3(const double & omega) const
         {
-            return switch_2pt_g * b_lcdas.g_bar_d3(omega);
+            return switch_2pt_g * b_lcdas->g_bar_d3(omega);
         }
 
         inline
         double phi_3(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.phi_3(omega_1, omega_2);
+            return switch_3pt * b_lcdas->phi_3(omega_1, omega_2);
         }
 
         inline
         double phi_bar_3(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.phi_bar_3(omega_1, omega_2);
+            return switch_3pt * b_lcdas->phi_bar_3(omega_1, omega_2);
         }
 
         inline
         double phi_bar2_3(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.phi_bar2_3(omega_1, omega_2);
+            return switch_3pt * b_lcdas->phi_bar2_3(omega_1, omega_2);
         }
 
         inline
         double phi_bar_bar_3(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.phi_bar_bar_3(omega_1, omega_2);
+            return switch_3pt * b_lcdas->phi_bar_bar_3(omega_1, omega_2);
         }
 
         inline
         double phi_4(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.phi_4(omega_1, omega_2);
+            return switch_3pt * b_lcdas->phi_4(omega_1, omega_2);
         }
 
         inline
         double phi_bar_4(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.phi_bar_4(omega_1, omega_2);
+            return switch_3pt * b_lcdas->phi_bar_4(omega_1, omega_2);
         }
 
         inline
         double phi_bar2_4(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.phi_bar2_4(omega_1, omega_2);
+            return switch_3pt * b_lcdas->phi_bar2_4(omega_1, omega_2);
         }
 
         inline
         double phi_bar_bar_4(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.phi_bar_bar_4(omega_1, omega_2);
+            return switch_3pt * b_lcdas->phi_bar_bar_4(omega_1, omega_2);
         }
 
         inline
         double psi_bar_4(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.psi_bar_4(omega_1, omega_2);
+            return switch_3pt * b_lcdas->psi_bar_4(omega_1, omega_2);
         }
 
         inline
         double psi_bar_bar_4(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.psi_bar_bar_4(omega_1, omega_2);
+            return switch_3pt * b_lcdas->psi_bar_bar_4(omega_1, omega_2);
         }
 
         inline
         double chi_bar_4(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.chi_bar_4(omega_1, omega_2);
+            return switch_3pt * b_lcdas->chi_bar_4(omega_1, omega_2);
         }
 
         inline
         double chi_bar_bar_4(const double omega_1, const double omega_2) const
         {
-            return switch_3pt * b_lcdas.chi_bar_bar_4(omega_1, omega_2);
+            return switch_3pt * b_lcdas->chi_bar_bar_4(omega_1, omega_2);
         }
 
         // }}}
---
+++ eos/form-factors/b-lcdas-exponential.hh
@@ -0,0 +1,151 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2017, 2022 Danny van Dyk
+ * Copyright (c) 2018 Nico Gubernari
+ * Copyright (c) 2018 Ahmet Kokulu
+ * Copyright (c) 2022 Philip Lüghausen
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef EOS_GUARD_EOS_FORM_FACTORS_B_LCDAS_EXPONENTIAL_HH
+#define EOS_GUARD_EOS_FORM_FACTORS_B_LCDAS_EXPONENTIAL_HH 1
+
+#include <eos/form-factors/b-lcdas.hh>
+#include <eos/utils/diagnostics.hh>
+#include <eos/utils/parameters.hh>
+#include <eos/utils/options.hh>
+#include <eos/utils/options-impl.hh>
+
+#include <string>
+
+namespace eos
+{
+    namespace b_lcdas
+    {
+        /*!
+         * Decomposition of B-meson to vacuum matrix elements of light-cone
+         * dominated operators.
+         *
+         * For the two-particle decomposition, we use the parametrization as
+         * defined in [KMO2006], eq. (17), p. 7.
+         *
+         * For the three-particle decomposition, we use the parametrization
+         * as defined in [KMO2006], eq. (28), p. 10.
+         */
+        class Exponential :
+            public BMesonLCDAs
+        {
+            private:
+                SwitchOption opt_q;
+                SwitchOption opt_gminus;
+
+                UsedParameter lambda_B_inv;
+                UsedParameter lambda_E2;
+                UsedParameter lambda_H2;
+
+                double switch_gminus;
+
+                inline double lambda_B() const;
+                std::string parameter(const char * _name) const;
+
+            public:
+                Exponential(const Parameters & parameters, const Options & options);
+                virtual ~Exponential() = default;
+
+                static BMesonLCDAs * make(const Parameters & parameters, const Options & options);
+
+                /*!
+                 * Leading twist two-particle LCDAs
+                 *
+                 * omega: plus-component of the spectator momentum
+                 */
+                virtual double phi_plus(const double &omega) const final override;
+                virtual double phi_minus(const double &omega) const final override;
+                virtual double phi_bar(const double &omega) const final override;
+                virtual double phi_bar_d1(const double &omega) const final override;
+
+                /*!
+                 * Next-to-leading twist two-particle LCDAs
+                 *
+                 * omega: plus-component of the spectator momentum
+                 */
+                virtual double g_plus(const double &omega) const final override;
+                virtual double g_plus_d1(const double &omega) const final override;
+                virtual double g_plus_d2(const double &omega) const final override;
+
+                virtual double g_minusWW(const double &omega) const final override;
+                virtual double g_minusWW_d1(const double &omega) const final override;
+                virtual double g_minusWW_d2(const double &omega) const final override;
+
+                virtual double g_bar(const double &omega) const final override;
+                virtual double g_bar_d1(const double &omega) const final override;
+                virtual double g_bar_d2(const double &omega) const final override;
+                virtual double g_bar_d3(const double &omega) const final override;
+
+                /*!
+                 * Leading power three-particle LCDAs
+                 *
+                 * omega_1: plus-component of the spectator momentum
+                 * omega_2: plus-component of the gluon momentum
+                 */
+                virtual double phi_3(const double &omega_1, const double &omega_2) const final override;
+                virtual double phi_4(const double &omega_1, const double &omega_2) const final override;
+
+                virtual double phi_bar_3(const double &omega_1, const double &omega_2) const final override;
+                virtual double phi_bar_4(const double &omega_1, const double &omega_2) const final override;
+
+                virtual double phi_bar2_3(const double &omega_1, const double &omega_2) const final override;
+                virtual double phi_bar2_4(const double &omega_1, const double &omega_2) const final override;
+
+                virtual double phi_bar_bar_3(const double &omega_1, const double &omega_2) const final override;
+                virtual double phi_bar_bar_4(const double &omega_1, const double &omega_2) const final override;
+
+                virtual double psi_bar_4(const double &omega_1, const double &omega_2) const final override;
+                virtual double chi_bar_4(const double &omega_1, const double &omega_2) const final override;
+
+                virtual double psi_bar_bar_4(const double &omega_1, const double &omega_2) const final override;
+                virtual double chi_bar_bar_4(const double &omega_1, const double &omega_2) const final override;
+                /*!
+                 * Pseudo observables for the two-particle LCDAs
+                 */
+                virtual double inverse_lambda_plus() const final override;
+
+                /*!
+                 * Leading power three-particle LCDAs
+                 *
+                 * omega: plus-component of the spectator momentum
+                 * xi:    plus-component of the gluon momentum
+                 */
+                virtual double psi_A(const double &omega, const double &xi) const final override;
+                virtual double psi_V(const double &omega, const double &xi) const final override;
+                virtual double X_A(const double &omega, const double &xi) const final override;
+                virtual double Y_A(const double &omega, const double &xi) const final override;
+
+                /*!
+                 * Auxiliary functions for the three-particle LCDAs
+                 *
+                 * See [KMO2006], below eq. (72), p. 28 for their definition.
+                 */
+                virtual double Xbar_A(const double &omega, const double &xi) const final override;
+                virtual double Ybar_A(const double &omega, const double &xi) const final override;
+
+                /* Internal diagnostics */
+                virtual Diagnostics diagnostics() const final override;
+        };
+    }
+}
+
+#endif
