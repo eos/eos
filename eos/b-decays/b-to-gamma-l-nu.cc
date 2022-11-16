66e8cfd15eccf5dd2557aba7a5bacdbac48ca70d
Philip Lüghausen <pluegh@gmail.com>
2022-11-16 13:19:13 +0100

    [b-decays] Add B->gamma l nubar BR and A_FB

---
+++ eos/b-decays/b-to-gamma-l-nu.cc
@@ -0,0 +1,246 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2022 Philip Lüghausen
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <eos/b-decays/b-to-gamma-l-nu.hh>
+#include <eos/form-factors/mesonic.hh>
+#include <eos/maths/integrate.hh>
+#include <eos/maths/power-of.hh>
+#include <eos/models/model.hh>
+#include <eos/utils/options-impl.hh>
+#include <eos/utils/private_implementation_pattern-impl.hh>
+
+#include <tuple>
+
+namespace eos
+{
+    using std::norm;
+
+    /*
+     * Decay: B_u -> gamma l nubar, cf. [BBJW:2018A], [BR:2011A]
+     *
+     * Lepton and neutrino are assumed to be massless
+     */
+    template <>
+    struct Implementation<BToGammaLeptonNeutrino>
+    {
+        std::shared_ptr<Model> model;
+        std::shared_ptr<FormFactors<PToGamma>> form_factors;
+
+        UsedParameter alpha_qed;
+        UsedParameter g_fermi;
+        UsedParameter v_ub_abs;
+        UsedParameter hbar;
+
+        UsedParameter m_B;
+        UsedParameter f_B;
+        UsedParameter tau_B;
+
+        static const constexpr double e_l = -1.0;
+
+        static const std::vector<OptionSpecification> options;
+
+        Implementation(const Parameters & p, const Options & o, ParameterUser & u) :
+            model(Model::make(o.get("model", "SM"), p, o)),
+            form_factors(FormFactorFactory<PToGamma>::create("B->gamma::" + o.get("form-factors", "FLvD2022QCDF"), p, o)),
+            alpha_qed(p["QED::alpha_e(m_b)"] ,u),
+            g_fermi(p["WET::G_Fermi"], u),
+            v_ub_abs(p["CKM::abs(V_ub)"], u),
+            hbar(p["QM::hbar"], u),
+            m_B(p["mass::B_u"], u),
+            f_B(p["decay-constant::B_u"], u),
+            tau_B(p["life_time::B_u"], u)
+        {
+            if(model == nullptr)
+                throw InternalError("BToGammaLeptonNeutrino: construction of model failed");
+            u.uses(*model);
+
+            if(form_factors == nullptr)
+                throw InternalError("BToGammaLeptonNeutrino: construction of form_factors failed");
+            u.uses(*form_factors);
+        }
+
+        double fully_differential_decay_width(const double & E_gamma, const double & costheta) const
+        {
+            const double E_ell = 1.0 / 2.0 * ((-1.0 + costheta) * E_gamma + m_B);
+            const double dEell_dcostheta = E_gamma / 2.0;
+
+            const double x_gamma = 2.0 * E_gamma / m_B;
+            const double x_ell   = 2.0 * E_ell   / m_B;
+            const double x_nu    = 2.0 * (1.0 - (E_gamma + E_ell) / m_B);
+
+            const double F_V = form_factors->F_V(E_gamma);
+            const double F_A = form_factors->F_A(E_gamma) + e_l * f_B / E_gamma; // mind different definitions of F_A between [BBJW:2018A] and [BR:2011A]
+
+            // cf. [BR:2011A], eq. (2.7)
+            const double dGamma_dEgamma_dEell =
+                alpha_qed * power_of<2>(g_fermi * v_ub_abs) / (16.0 * power_of<2>(M_PI))
+                * power_of<3>(m_B) * (1.0 - x_gamma)
+                * (
+                      power_of<2>(1.0 - x_nu ) * power_of<2>(F_A + F_V)
+                    + power_of<2>(1.0 - x_ell) * power_of<2>(F_A - F_V)
+                  );
+            return dGamma_dEgamma_dEell * dEell_dcostheta;
+        }
+
+        double differential_decay_width_dEgamma(const double & E_gamma) const
+        {
+            // Use analytic result of the angular integration
+            // cf. [BBJW:2018A], eq. (2.6)
+            const double prefactor = alpha_qed * power_of<2>(g_fermi * v_ub_abs) / (6.0 * power_of<2>(M_PI));
+            const double F_V = form_factors->F_V(E_gamma);
+            const double F_A = form_factors->F_A(E_gamma);
+
+            return prefactor * m_B * power_of<3>(E_gamma) * (1.0 - 2.0 * E_gamma / m_B)
+                * ( power_of<2>(std::abs(F_V)) + power_of<2>( F_A + e_l * f_B / E_gamma ) );
+        }
+
+        double integrated_decay_width(const double & E_gamma_min) const
+        {
+            return integrate<GSL::QAGS>([&](const double & E_gamma) { return differential_decay_width_dEgamma(E_gamma); }, E_gamma_min, m_B / 2.0);
+        }
+
+        double integrated_branching_ratio(const double & E_gamma_min) const
+        {
+            return integrated_decay_width(E_gamma_min) * tau_B / hbar;
+        }
+
+        std::tuple<double, double> forward_backward_decay_widths(const double & E_gamma_min) const
+        {
+            // Use analytic result of the angular integration
+
+            const double m_B2 = m_B * m_B, m_B3 = m_B2 * m_B;
+
+            auto dGamma_dEgamma_forward = [&](const double & E_gamma) -> double {
+                const double F_V = form_factors->F_V(E_gamma);
+                const double F_A = form_factors->F_A(E_gamma) + e_l * f_B / E_gamma; // mind different definitions of F_A between [BBJW:2018A] and [BR:2011A]
+
+                return (2 * 1. / m_B3 * (2 * E_gamma - m_B) * (3 * F_A * F_V + 2 * power_of<2>(F_A) + 2 * power_of<2>(F_V)) * power_of<3>(E_gamma)) / 3.;
+            };
+
+            auto dGamma_dEgamma_backward = [&](const double & E_gamma) -> double {
+                const double F_V = form_factors->F_V(E_gamma);
+                const double F_A = form_factors->F_A(E_gamma) + e_l * f_B / E_gamma; // mind different definitions of F_A between [BBJW:2018A] and [BR:2011A]
+
+                return (2 * 1. / m_B3 * (2 * E_gamma - m_B) * (-3 * F_A * F_V + 2 * power_of<2>(F_A) + 2 * power_of<2>(F_V)) * power_of<3>(E_gamma)) / 3.;
+            };
+
+            const double prefactor = alpha_qed * power_of<2>(g_fermi * v_ub_abs)
+                    / (16.0 * power_of<2>(M_PI)) * power_of<3>(m_B);
+
+            const double Gamma_forward  = prefactor * integrate<GSL::QAGS>(dGamma_dEgamma_forward,  E_gamma_min, m_B / 2.0);
+            const double Gamma_backward = prefactor * integrate<GSL::QAGS>(dGamma_dEgamma_backward, E_gamma_min, m_B / 2.0);
+
+            return { Gamma_forward, Gamma_backward };
+        }
+
+        double forward_backward_asymmetry(const double & E_gamma_min) const
+        {
+            const auto [Gamma_forward, Gamma_backward] = forward_backward_decay_widths(E_gamma_min);
+
+            return (Gamma_forward - Gamma_backward) / (Gamma_forward + Gamma_backward);
+        }
+
+        Diagnostics diagnostics() const
+        {
+            Diagnostics results;
+
+            // check consitency of Gamma_F_Gamma_B with forward_backward_asymmetry
+            {
+                const constexpr double E_gamma_min = 2.0;
+                auto [Gamma_F, Gamma_B] = this->forward_backward_decay_widths(E_gamma_min);
+                const double Gamma = this->integrated_decay_width(E_gamma_min);
+
+                results.add({ Gamma_F + Gamma_B - Gamma, "Gamma_F + Gamma_B - Gamma" });
+            }
+
+            return results;
+        }
+    };
+
+    const std::vector<OptionSpecification>
+    Implementation<BToGammaLeptonNeutrino>::options
+    {
+        { "form-factors", { "FLvD2022QCDF" }, "FLvD2022QCDF" }
+    };
+
+    BToGammaLeptonNeutrino::BToGammaLeptonNeutrino(const Parameters & parameters, const Options & options) :
+        PrivateImplementationPattern<BToGammaLeptonNeutrino>(new Implementation<BToGammaLeptonNeutrino>(parameters, options, *this))
+    {
+    }
+
+    BToGammaLeptonNeutrino::~BToGammaLeptonNeutrino()
+    {
+    }
+
+    double
+    BToGammaLeptonNeutrino::integrated_branching_ratio(const double & E_gamma_min) const
+    {
+        return _imp->integrated_branching_ratio(E_gamma_min);
+    }
+
+    double
+    BToGammaLeptonNeutrino::forward_backward_asymmetry(const double & E_gamma_min) const
+    {
+        return _imp->forward_backward_asymmetry(E_gamma_min);
+    }
+
+    double
+    BToGammaLeptonNeutrino::fully_differential_decay_width(const double & E_gamma, const double & costheta) const
+    {
+        return _imp->fully_differential_decay_width(E_gamma, costheta);
+    }
+
+    double
+    BToGammaLeptonNeutrino::differential_decay_width_dEgamma(const double & E_gamma) const
+    {
+        return _imp->differential_decay_width_dEgamma(E_gamma);
+    }
+
+    double
+    BToGammaLeptonNeutrino::integrated_decay_width(const double & E_gamma_min) const
+    {
+        return _imp->integrated_decay_width(E_gamma_min);
+    }
+
+    Diagnostics
+    BToGammaLeptonNeutrino::diagnostics() const
+    {
+        return _imp->diagnostics();
+    }
+
+    const std::set<ReferenceName>
+    BToGammaLeptonNeutrino::references
+    {
+        "BBJW:2018A"_rn,
+        "BR:2011A"_rn
+    };
+
+    std::vector<OptionSpecification>::const_iterator
+    BToGammaLeptonNeutrino::begin_options()
+    {
+        return Implementation<BToGammaLeptonNeutrino>::options.cbegin();
+    }
+
+    std::vector<OptionSpecification>::const_iterator
+    BToGammaLeptonNeutrino::end_options()
+    {
+        return Implementation<BToGammaLeptonNeutrino>::options.cend();
+    }
+}
+
---
+++ eos/b-decays/b-to-gamma-l-nu_TEST.cc
@@ -0,0 +1,139 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2022 Philip Lüghausen
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <test/test.hh>
+#include <eos/b-decays/b-to-gamma-l-nu.hh>
+#include <eos/maths/integrate.hh>
+#include <eos/maths/integrate-impl.hh>
+#include <eos/observable.hh>
+
+using namespace test;
+using namespace eos;
+
+class BToGammaLeptonNeutrinoTest :
+    public TestCase
+{
+    public:
+        BToGammaLeptonNeutrinoTest() :
+            TestCase("b_to_gamma-l_nu_test")
+        {
+        }
+
+        virtual void run() const
+        {
+            static const double eps = 1e-6;
+
+            Parameters p = Parameters::Defaults();
+
+            // Values from [BBJW:2018A]
+            p["B_u::mu_0@FLvD2022"]  = 1.5;
+            p["B->gamma::mu@FLvD2022QCDF"]  = 1.5;
+            p["B->gamma::mu_h1@FLvD2022QCDF"]  = 4.7;
+            p["B->gamma::mu_h2@FLvD2022QCDF"]  = 4.5;
+            p["B->gamma::s_0@FLvD2022QCDF"] = 1.59;
+            p["B->gamma::M^2@FLvD2022QCDF"] = 1.35;
+            p["decay-constant::B_u"] = 0.192;
+            p["mass::B_u"] = 5.27929;
+            p["mass::b(MSbar)"] = 4.453796188717916; // fix m_b_pole@1-loop to 4.8
+            p["mass::rho^+"] = 0.77526;
+            p["B::lambda_E^2"] = 0.0625;
+            p["B::lambda_H^2"] = 0.125;
+            p["B::LambdaBar"] = 1.033232013955; // m_B - m_b
+            p["CKM::abs(V_ub)"] = 3.7e-3;
+            p["life_time::B_u"] = 1.638e-12;
+            p["WET::G_Fermi"] = 1.166378e-5;
+
+            // Compare to the exponential model
+            p["B_u::omega_0@FLvD2022"]  = 0.2; // Corresponds to lambda_B
+            p["B_u::a^phi+_0@FLvD2022"] = 1.0;
+            p["B_u::a^phi+_1@FLvD2022"] = 0.0;
+            p["B_u::a^phi+_2@FLvD2022"] = 0.0;
+            p["B_u::a^phi+_3@FLvD2022"] = 0.0;
+            p["B_u::a^phi+_4@FLvD2022"] = 0.0;
+            p["B_u::a^phi+_5@FLvD2022"] = 0.0;
+            p["B_u::a^phi+_6@FLvD2022"] = 0.0;
+            p["B_u::a^phi+_7@FLvD2022"] = 0.0;
+            p["B_u::a^phi+_8@FLvD2022"] = 0.0;
+
+            Options oo
+            {
+                { "form-factors", "FLvD2022QCDF" },
+            };
+
+            // Branching ratio tests
+            {
+                auto BR = [p, oo](const double & E_gamma_min, const double & lambda_B) -> double {
+                    auto k = Kinematics({ { "E_gamma_min", E_gamma_min } });
+                    auto parameters = p; parameters["B_u::omega_0@FLvD2022"] = lambda_B; // a_k fixed to exp. model => omega_0 = lambda_B
+
+                    return Observable::make("B_u->gammalnu::BR(E_gamma_min)", p, k, oo)->evaluate();
+                };
+
+                // Values taken from Ref. [BBJW:2018A], Fig. (9)
+                TEST_CHECK_NEARLY_EQUAL(BR(1.0, 0.2), 5.0e-6, 0.5e-6);
+                TEST_CHECK_NEARLY_EQUAL(BR(1.0, 0.3), 2.4e-6, 0.4e-6);
+                TEST_CHECK_NEARLY_EQUAL(BR(1.0, 0.4), 1.2e-6, 0.2e-6);
+                TEST_CHECK_NEARLY_EQUAL(BR(1.0, 0.6), 0.2e-6, 0.2e-6);
+
+                TEST_CHECK_NEARLY_EQUAL(BR(1.5, 0.2), 3.2e-6,  0.5e-6);
+                TEST_CHECK_NEARLY_EQUAL(BR(2.0, 0.2), 1.25e-6, 0.5e-6);
+            }
+
+            // Consistency check of the angular-differential decay witdh and the integrated decay width
+            {
+                BToGammaLeptonNeutrino obs(p, oo);
+                const double E_gamma_min = 2.0;
+
+                auto integrand = [&](const std::array<double, 2> & x) -> double {
+                    const double E_gamma = std::get<0>(x);
+                    const double costheta = std::get<1>(x);
+                    return obs.fully_differential_decay_width(E_gamma, costheta);
+                };
+
+                const double decay_width_analytical = obs.integrated_decay_width(E_gamma_min);
+                const double A_FB_analytical = obs.forward_backward_asymmetry(E_gamma_min);
+
+                // Check analytical decay width numerically
+                {
+                    const double integrated_angular_decay_width = integrate<2>(integrand, { E_gamma_min, 1.0 }, { p["mass::B_u"] / 2.0, -1.0 });
+                    TEST_CHECK_NEARLY_EQUAL(decay_width_analytical, integrated_angular_decay_width, eps);
+                }
+
+                // Check analytical implementation of A_FB numerically
+                {
+                    const double Gamma_F = integrate<2>(integrand, { E_gamma_min, 1.0 }, { p["mass::B_u"] / 2.0,  0.0 });
+                    const double Gamma_B = integrate<2>(integrand, { E_gamma_min, 0.0 }, { p["mass::B_u"] / 2.0, -1.0 });
+                    TEST_CHECK_NEARLY_EQUAL((Gamma_F - Gamma_B)/(Gamma_F + Gamma_B), A_FB_analytical, eps);
+                }
+
+            }
+
+            // Diagnostics
+            {
+                static const std::vector<std::pair<double, double>> reference = {
+                    std::make_pair(0.0, 1e-9), // Gamma_F + Gamma_B - Gamma
+                };
+
+                BToGammaLeptonNeutrino obs(p, oo);
+                Diagnostics diagnostics = obs.diagnostics();
+                TEST_CHECK_DIAGNOSTICS(diagnostics, reference);
+            }
+        }
+} b_to_gamma_l_nu_test;
+
