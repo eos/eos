7ad6e522404a3654bb4b69f1b2c851842ca3dd82
Stephan Kürten <stephan.kuerten91@gmail.com>
2023-03-07 09:51:16 +0100

    [form-factors] Add subtracted [KKvDZ:2022A] form factors

--- eos/form-factors/parametric-kkvdz2022_TEST.cc
+++ eos/form-factors/parametric-kkvdz2022_TEST.cc
@@ -24,6 +24,27 @@
 using namespace test;
 using namespace eos;
 
+Parameters
+subtraction_parameters()
+{
+    Parameters result = Parameters::Defaults();
+    result["B->gamma^*::c_1_0@KKvDZ2022"] = 0.1;
+    result["B->gamma^*::c_1_1@KKvDZ2022"] = 0.2;
+    result["B->gamma^*::c_1_2@KKvDZ2022"] = 0.3;
+    result["B->gamma^*::c_2_0@KKvDZ2022"] = -0.1;
+    result["B->gamma^*::c_2_1@KKvDZ2022"] = -0.2;
+    result["B->gamma^*::c_2_2@KKvDZ2022"] = -0.3;
+    result["B->gamma^*::c_3_0@KKvDZ2022"] = 0.121;
+    result["B->gamma^*::c_3_1@KKvDZ2022"] = -0.2;
+    result["B->gamma^*::c_3_2@KKvDZ2022"] = 0.2313;
+    result["B->gamma^*::c_4_0@KKvDZ2022"] = 0.91;
+    result["B->gamma^*::c_4_1@KKvDZ2022"] = -0.2;
+    result["B->gamma^*::c_4_2@KKvDZ2022"] = -0.3;
+    result["B->gamma^*::s_0@KKvDZ2022"] = 0.4;
+
+    return result;
+}
+
 class ParametricKKvDZ2022FormFactorTest :
     public TestCase
 {
@@ -38,9 +59,11 @@ class ParametricKKvDZ2022FormFactorTest :
             static const double eps = 1.0e-5;
 
             Parameters p = Parameters::Defaults();
+            Parameters sub_p = subtraction_parameters();
 
             {
                 KKvDZ2022FormFactors kkvdz2022(p, Options{ });
+                KKvDZ2022FormFactors kkvdz2022_sub(sub_p, Options{ {"subtracted","on"} });
 
                 // Tested against an IPython/Jupyter notebook implementation of Stephan Kürten
                 TEST_CHECK_NEARLY_EQUAL( complex<double>( 0.0713            , 0.0               ),  kkvdz2022.F_1(0.0, 0.0),  eps);
@@ -82,6 +105,50 @@ class ParametricKKvDZ2022FormFactorTest :
                 TEST_CHECK_NEARLY_EQUAL( complex<double>( 0.095395895472241, -0.030197705486835),  kkvdz2022.F_4(1.2, 0.9),  eps);
                 TEST_CHECK_NEARLY_EQUAL( complex<double>( 0.064471059532659, -0.017505340990020),  kkvdz2022.F_4(1.5, 0.6),  eps);
                 TEST_CHECK_NEARLY_EQUAL( complex<double>( 0.065537628075751, -0.017793972290078),  kkvdz2022.F_4(1.5, 0.9),  eps);
+
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-0.040315453106118, 0.0               ),  kkvdz2022_sub.F_1(0.0,0.0),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-0.157879067495966, 0.0824559890139291),  kkvdz2022_sub.F_1(1.2,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-0.157879067495966, 0.0824559890139291),  kkvdz2022_sub.F_1(1.2,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-0.161813624381275, 0.0835854395822285),  kkvdz2022_sub.F_1(1.2,0.9),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-0.143768399179354, 0.0668158087758451),  kkvdz2022_sub.F_1(1.5,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-0.147509153756751, 0.0677310119135683),  kkvdz2022_sub.F_1(1.5,0.9),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 0.683755659866785, 0.6224967477184292),  kkvdz2022_sub.F_1(0.6,1.2),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-0.198103842587939, 0.1328424699649706),  kkvdz2022_sub.F_1(0.9,1.2),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 0.691549948563531, 0.6311919389614886),  kkvdz2022_sub.F_1(0.6,1.5),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-0.202648396227549, 0.1346919207079736),  kkvdz2022_sub.F_1(0.9,1.5),  eps);
+
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 1.5966688305961358, 0.0                ),  kkvdz2022_sub.F_2(0.0,0.0),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.0064639347474795, -1.0002473383097592),  kkvdz2022_sub.F_2(1.2,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.0064639347474795, -1.0002473383097592),  kkvdz2022_sub.F_2(1.2,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.0490987885176133, -1.0137950850113697),  kkvdz2022_sub.F_2(1.2,0.9),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 2.8364527078961395, -0.8105530723052206),  kkvdz2022_sub.F_2(1.5,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 2.8767700122952955, -0.8215311180858934),  kkvdz2022_sub.F_2(1.5,0.9),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-7.077674203759454 , -7.485189551456463 ),  kkvdz2022_sub.F_2(0.6,1.2),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.4804602984526265, -1.6108308644115616),  kkvdz2022_sub.F_2(0.9,1.2),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-7.17500027605132  , -7.589170522606418 ),  kkvdz2022_sub.F_2(0.6,1.5),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.5304034457122113, -1.6330310861819934),  kkvdz2022_sub.F_2(0.9,1.5),  eps);
+
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 1.817668830596136 , 0.0                ),  kkvdz2022_sub.F_3(0.0,0.0),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.2425465832942786, -1.005030086665565 ),  kkvdz2022_sub.F_3(1.2,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.2425465832942786, -1.005030086665565 ),  kkvdz2022_sub.F_3(1.2,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.2933412747782   , -1.0211626105737832),  kkvdz2022_sub.F_3(1.2,0.9),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.0717020420752887, -0.8144282267341435),  kkvdz2022_sub.F_3(1.5,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.1197286351471765, -0.8275005464793553),  kkvdz2022_sub.F_3(1.5,0.9),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-6.929571280799387 , -7.561035286914558 ),  kkvdz2022_sub.F_3(0.6,1.2),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.737244829324993 , -1.626652973333924 ),  kkvdz2022_sub.F_3(0.9,1.2),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(-7.047636441789464 , -7.686588268074507 ),  kkvdz2022_sub.F_3(0.6,1.5),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>( 3.7973725531421114, -1.653349634358173 ),  kkvdz2022_sub.F_3(0.9,1.5),  eps);
+
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(1.110147091121624  , 0.0                ),  kkvdz2022_sub.F_4(0.0,0.0),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(1.2794201400568068 , -0.1183084144539568),  kkvdz2022_sub.F_4(1.2,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(1.2794201400568068 , -0.1183084144539568),  kkvdz2022_sub.F_4(1.2,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(1.2859256813715785 , -0.1202591567237934),  kkvdz2022_sub.F_4(1.2,0.9),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(1.2591953094874802 , -0.0958683591780728),  kkvdz2022_sub.F_4(1.5,0.6),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(1.2653655441667946 , -0.0974490459246675),  kkvdz2022_sub.F_4(1.5,0.9),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(0.06903406359778763, -0.8970804192739344),  kkvdz2022_sub.F_4(0.6,1.2),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(1.339112662949608  , -0.1916659683324040),  kkvdz2022_sub.F_4(0.9,1.2),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(0.05503920070886814, -0.9123143136327135),  kkvdz2022_sub.F_4(0.6,1.5),  eps);
+                TEST_CHECK_NEARLY_EQUAL( complex<double>(1.3467593608194122 , -0.19489911545919864),  kkvdz2022_sub.F_4(0.9,1.5),  eps);
             }
         }
 } parametric_kkvdz2022_form_factor_test;
