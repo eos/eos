bcd9d5d8352daa6285207ee4670a1d5b009b4161
Méril Reboud <merilreboud@gmail.com>
2023-05-16 18:51:15 +0200

    [rare-b-decays] Add Lambda_b -> Lambda nu nu observables and correct typo in the unit of some BR

--- eos/rare-b-decays/lambda-b-to-lambda-nu-nu.cc
+++ eos/rare-b-decays/lambda-b-to-lambda-nu-nu.cc
@@ -17,15 +17,20 @@
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <eos/rare-b-decays/lambda-b-to-lambda-nu-nu.hh>
 #include <eos/form-factors/form-factors.hh>
 #include <eos/maths/integrate.hh>
+#include <eos/maths/integrate-impl.hh>
 #include <eos/maths/power-of.hh>
 #include <eos/models/model.hh>
+#include <eos/rare-b-decays/lambda-b-to-lambda-nu-nu.hh>
+#include <eos/rare-b-decays/lambda-b-to-lambda-nu-nu-impl.hh>
 #include <eos/utils/destringify.hh>
 #include <eos/utils/kinematic.hh>
 #include <eos/utils/options.hh>
 #include <eos/utils/private_implementation_pattern-impl.hh>
+
+using std::norm;
+using std::sqrt;
 
 namespace eos
 {
@@ -36,34 +41,26 @@ namespace eos
 
         std::shared_ptr<FormFactors<OneHalfPlusToOneHalfPlus>> form_factors;
 
-        Parameters parameters;
-
         UsedParameter m_Lambda_b;
-
         UsedParameter tau_Lambda_b;
-
         UsedParameter m_Lambda;
-
         UsedParameter g_fermi;
-
         UsedParameter alpha_e;
-
         UsedParameter hbar;
-
         UsedParameter mu;
+
+        using IntermediateResult = LambdaBToLambdaDineutrino::IntermediateResult;
+        IntermediateResult intermediate_result;
 
         static const std::vector<OptionSpecification> options;
 
         std::function<complex<double> ()> lambda_t;
         std::function<WilsonCoefficients<wc::SBNuNu> ()> wc;
 
-        GSL::QAGS::Config int_config;
-
         bool cp_conjugate;
 
         Implementation(const Parameters & p, const Options & o, ParameterUser & u) :
             model(Model::make(o.get("model", "SM"), p, o)),
-            parameters(p),
             m_Lambda_b(p["mass::Lambda_b"], u),
             tau_Lambda_b(p["life_time::Lambda_b"], u),
             m_Lambda(p["mass::Lambda"], u),
@@ -71,7 +68,6 @@ namespace eos
             alpha_e(p["QED::alpha_e(m_b)"], u),
             hbar(p["QM::hbar"], u),
             mu(p["sbnunu::mu"], u),
-            int_config(GSL::QAGS::Config().epsrel(0.5e-3)),
             cp_conjugate(destringify<bool>(o.get("cp-conjugate", "false")))
         {
             form_factors = FormFactorFactory<OneHalfPlusToOneHalfPlus>::create("Lambda_b->Lambda::" + o.get("form-factors", "BFvD2014"), p, o);
@@ -86,34 +82,57 @@ namespace eos
             u.uses(*model);
         }
 
-        // differential decay width
-        double differential_decay_width(const double & q2) const
+        inline std::array<double, 2> angular_coefficients_array(const double & s) const
         {
-            const double lambda = eos::lambda(power_of<2>(m_Lambda_b), power_of<2>(m_Lambda), q2),
-                         sqrt_lambda = std::sqrt(lambda);
+            std::array<double, 2> result;
 
             const auto wc = this->wc();
+            const double lambda = eos::lambda(power_of<2>(m_Lambda_b), power_of<2>(m_Lambda), s),
+                         sqrt_lambda = std::sqrt(lambda),
+                         s_minus = power_of<2>(m_Lambda_b - m_Lambda) - s,
+                         s_plus = power_of<2>(m_Lambda_b + m_Lambda) - s;
 
-            const double rho_perp_v = 32 * q2 * sqrt_lambda *
-                (power_of<2>(m_Lambda_b - m_Lambda) - q2) / power_of<4>(m_Lambda_b) * std::norm(form_factors->f_perp_v(q2));
-            const double rho_perp_a = 32 * q2 * sqrt_lambda *
-                (power_of<2>(m_Lambda_b + m_Lambda) - q2) / power_of<4>(m_Lambda_b) * std::norm(form_factors->f_perp_a(q2));
-            const double rho_long_v = 16 * sqrt_lambda * power_of<2>(m_Lambda_b + m_Lambda) *
-                (power_of<2>(m_Lambda_b - m_Lambda) - q2) / power_of<4>(m_Lambda_b) * std::norm(form_factors->f_long_v(q2));
-            const double rho_long_a = 16 * sqrt_lambda * power_of<2>(m_Lambda_b - m_Lambda) *
-                (power_of<2>(m_Lambda_b + m_Lambda) - q2) / power_of<4>(m_Lambda_b) * std::norm(form_factors->f_long_a(q2));
+            std::complex<double> N = g_fermi * alpha_e * lambda_t() * sqrt(s * sqrt_lambda /
+                3.0 / 2048.0 / power_of<3>(m_Lambda_b) / power_of<5>(M_PI));
 
-            const double norm = std::norm(g_fermi * alpha_e * lambda_t() * wc.cVL() *
-                std::sqrt(m_Lambda_b / 3.0 / M_PI) / 16.0 / power_of<2>(M_PI));
+            std::complex<double>
+                a_perp_plus =   2 * sqrt(2.0) * N * (wc.cVL() + wc.cVR()) * (- sqrt(2 * s_minus) * form_factors->f_perp_v(s)),
+                a_para_plus = - 2 * sqrt(2.0) * N * (wc.cVL() - wc.cVR()) * (- sqrt(2 * s_plus) * form_factors->f_perp_a(s)),
+                a_perp_long =   2 * sqrt(2.0) * N * (wc.cVL() + wc.cVR()) * ((m_Lambda_b + m_Lambda) * sqrt(s_minus / s) * form_factors->f_long_v(s)),
+                a_para_long = - 2 * sqrt(2.0) * N * (wc.cVL() - wc.cVR()) * ((m_Lambda_b - m_Lambda) * sqrt(s_plus / s) * form_factors->f_long_a(s));
 
-            // assume the production of 3 diagonal neutrino flavors (nu_i nubar_i)
-            return 3.0 * norm * (rho_perp_v + rho_perp_a + rho_long_v + rho_long_a);
+            // K1ss
+            result[0] = 0.25 * (norm(a_perp_plus) + norm(a_para_plus) + 2 * norm(a_perp_long) + 2 * norm(a_para_long));
+            // K1cc
+            result[1] = 0.5 * (norm(a_perp_plus) + norm(a_para_plus));
+
+            return result;
         }
 
-        // differential branching_ratio
-        double differential_branching_ratio(const double & s) const
+        inline LambdaBToLambdaDineutrino::AngularCoefficients differential_angular_coefficients(const double & s) const
         {
-            return differential_decay_width(s) * tau_Lambda_b / hbar;
+            return LambdaBToLambdaDineutrino::AngularCoefficients(angular_coefficients_array(s));
+        }
+
+        LambdaBToLambdaDineutrino::AngularCoefficients integrated_angular_coefficients(const double & s_min, const double & s_max) const
+        {
+            std::function<std::array<double, 2> (const double &)> integrand =
+                    std::bind(&Implementation<LambdaBToLambdaDineutrino>::angular_coefficients_array, this, std::placeholders::_1);
+            std::array<double, 2> integrated_angular_coefficients_array = integrate1D(integrand, 64, s_min, s_max);
+
+            return LambdaBToLambdaDineutrino::AngularCoefficients(integrated_angular_coefficients_array);
+        }
+
+        const IntermediateResult * prepare(const double & q2_min, const double & q2_max)
+        {
+            intermediate_result.ac = integrated_angular_coefficients(q2_min, q2_max);
+            return &intermediate_result;
+        }
+
+        inline double decay_width(const LambdaBToLambdaDineutrino::AngularCoefficients & a_c)
+        {
+            // assume the production of 3 diagonal neutrino flavors (nu_i nubar_i)
+            return 3.0 * (2.0 * a_c.K1ss + a_c.K1cc);
         }
     };
 
@@ -132,20 +151,53 @@ namespace eos
         Model::option_specification(),
     };
 
+
     double
-    LambdaBToLambdaDineutrino::differential_branching_ratio(const double & q2) const
+    LambdaBToLambdaDineutrino::differential_decay_width(const double & s) const
     {
-        return _imp->differential_branching_ratio(q2);
+        return _imp->decay_width(_imp->differential_angular_coefficients(s));
     }
 
     double
-    LambdaBToLambdaDineutrino::integrated_branching_ratio(const double & q2_min, const double & q2_max) const
+    LambdaBToLambdaDineutrino::differential_branching_ratio(const double & s) const
     {
-        std::function<double (const double &)> f = std::bind(&Implementation<LambdaBToLambdaDineutrino>::differential_branching_ratio,
-                _imp.get(), std::placeholders::_1);
-
-        return integrate<GSL::QAGS>(f, q2_min, q2_max, _imp->int_config);
+        return differential_decay_width(s) * _imp->tau_Lambda_b() / _imp->hbar();
     }
+
+    double
+    LambdaBToLambdaDineutrino::differential_longitudinal_polarisation(const double & s) const
+    {
+        AngularCoefficients a_c = _imp->differential_angular_coefficients(s);
+        // assume the production of 3 diagonal neutrino flavors (nu_i nubar_i)
+        return 3.0 * (2.0 * a_c.K1ss - a_c.K1cc) / _imp->decay_width(a_c);
+    }
+
+    const LambdaBToLambdaDineutrino::IntermediateResult *
+    LambdaBToLambdaDineutrino::prepare(const double & q2_min, const double & q2_max) const
+    {
+        return _imp->prepare(q2_min, q2_max);
+    }
+
+    double
+    LambdaBToLambdaDineutrino::integrated_decay_width(const IntermediateResult * ir) const
+    {
+        return _imp->decay_width(ir->ac);
+    }
+
+    double
+    LambdaBToLambdaDineutrino::integrated_branching_ratio(const IntermediateResult * ir) const
+    {
+        return integrated_decay_width(ir) * _imp->tau_Lambda_b() / _imp->hbar();
+    }
+
+    double
+    LambdaBToLambdaDineutrino::integrated_longitudinal_polarisation(const IntermediateResult * ir) const
+    {
+        AngularCoefficients a_c = ir->ac;
+        // assume the production of 3 diagonal neutrino flavors (nu_i nubar_i)
+        return 3.0 * (2.0 * a_c.K1ss + a_c.K1cc) / _imp->decay_width(a_c);
+    }
+
 
     const std::string
     LambdaBToLambdaDineutrino::description = "\
