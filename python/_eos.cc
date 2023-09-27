d05c3b6a7bd4450480a0989ef5824521c74bdc03
Danny van Dyk <danny.van.dyk@gmail.com>
2023-09-27 14:32:12 +0100

    [python] Move ``ExternalLogLikelihoodBlock`` into separate files

--- python/_eos.cc
+++ python/_eos.cc
@@ -37,86 +37,14 @@
 #include "eos/statistics/log-posterior.hh"
 #include "eos/statistics/log-prior.hh"
 #include "eos/statistics/test-statistic-impl.hh"
-
 #include "eos/nonlocal-form-factors/charm-loops-impl.hh"
+#include "python/_eos/external-log-likelihood-block.hh"
 
 #include <boost/python.hpp>
 #include <boost/python/raw_function.hpp>
 
 using namespace boost::python;
 using namespace eos;
-
-namespace eos
-{
-    class ExternalLogLikelihoodBlock :
-        public LogLikelihoodBlock
-    {
-        private:
-            ObservableCache _cache;
-            object _factory;
-            object _python_llh_block;
-            object _evaluate;
-            unsigned _number_of_observations;
-
-        public:
-            ExternalLogLikelihoodBlock(const ObservableCache & cache, object factory) :
-                _cache(cache),
-                _factory(factory),
-                _python_llh_block(_factory(_cache)),
-                _evaluate(_python_llh_block.attr("evaluate")),
-                _number_of_observations(extract<unsigned>(_python_llh_block.attr("number_of_observations")))
-            {
-                if (! PyCallable_Check(_evaluate.ptr()))
-                {
-                    throw InternalError("ExternalLogLikelihoodBlock encountered a factory that does not yield a callable 'evaluate()' attribute");
-                }
-            }
-
-            ~ExternalLogLikelihoodBlock() = default;
-
-            static LogLikelihoodBlockPtr make(const ObservableCache & cache, object factory)
-            {
-                return LogLikelihoodBlockPtr(new ExternalLogLikelihoodBlock(cache, factory));
-            }
-
-            virtual std::string as_string() const override
-            {
-                return "ExternalLikelihoodBlock";
-            }
-
-            virtual LogLikelihoodBlockPtr clone(ObservableCache cache) const override
-            {
-                return LogLikelihoodBlockPtr(new ExternalLogLikelihoodBlock(cache, this->_factory));
-            }
-
-            virtual double evaluate() const override
-            {
-                return extract<double>(_evaluate());
-            }
-
-            virtual unsigned int number_of_observations() const override
-            {
-                return _number_of_observations;
-            }
-
-            virtual double sample(gsl_rng *) const override
-            {
-                throw InternalError("Not implemented");
-                return 0.0;
-            }
-
-            virtual double significance() const override
-            {
-                throw InternalError("Not implemented");
-                return 0.0;
-            }
-
-            virtual TestStatistic primary_test_statistic() const override
-            {
-                return test_statistics::Empty();
-            }
-    };
-}
 
 namespace impl
 {
---
+++ python/_eos/external-log-likelihood-block.cc
@@ -0,0 +1,92 @@
+/* vim: set sw=4 sts=4 et foldmethod=marker : */
+
+/*
+ * Copyright (c) 2023 Danny van Dyk
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "eos/statistics/test-statistic-impl.hh"
+#include "python/_eos/external-log-likelihood-block.hh"
+
+using boost::python::extract;
+using boost::python::object;
+
+namespace eos
+{
+    ExternalLogLikelihoodBlock::ExternalLogLikelihoodBlock(const ObservableCache & cache, object factory) :
+        _cache(cache),
+        _factory(factory),
+        _python_llh_block(_factory(_cache)),
+        _evaluate(_python_llh_block.attr("evaluate")),
+        _number_of_observations(extract<unsigned>(_python_llh_block.attr("number_of_observations")))
+    {
+        if (! PyCallable_Check(_evaluate.ptr()))
+        {
+            throw InternalError("ExternalLogLikelihoodBlock encountered a factory that does not yield a callable 'evaluate()' attribute");
+        }
+    }
+
+    ExternalLogLikelihoodBlock::~ExternalLogLikelihoodBlock() = default;
+
+    LogLikelihoodBlockPtr
+    ExternalLogLikelihoodBlock::make(const ObservableCache & cache, object factory)
+    {
+        return LogLikelihoodBlockPtr(new ExternalLogLikelihoodBlock(cache, factory));
+    }
+
+    std::string
+    ExternalLogLikelihoodBlock::as_string() const
+    {
+        return "ExternalLikelihoodBlock";
+    }
+
+    LogLikelihoodBlockPtr
+    ExternalLogLikelihoodBlock::clone(ObservableCache cache) const
+    {
+        return LogLikelihoodBlockPtr(new ExternalLogLikelihoodBlock(cache, this->_factory));
+    }
+
+    double
+    ExternalLogLikelihoodBlock::evaluate() const
+    {
+        return extract<double>(_evaluate());
+    }
+
+    unsigned int
+    ExternalLogLikelihoodBlock::number_of_observations() const
+    {
+        return _number_of_observations;
+    }
+
+    double
+    ExternalLogLikelihoodBlock::sample(gsl_rng *) const
+    {
+        throw InternalError("Not implemented");
+        return 0.0;
+    }
+
+    double
+    ExternalLogLikelihoodBlock::significance() const
+    {
+        throw InternalError("Not implemented");
+        return 0.0;
+    }
+
+    TestStatistic
+    ExternalLogLikelihoodBlock::primary_test_statistic() const
+    {
+        return test_statistics::Empty();
+    }
+}
