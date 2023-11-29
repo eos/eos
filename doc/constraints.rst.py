318cc5c244f57b58aa1e879fcbe4bb675492a4e8
Matthew Kirk <matthew.j.kirk@durham.ac.uk>
2023-11-29 16:05:29 +0000

    [doc] Make the constraint links point to the right places

--- doc/constraints.rst.py
+++ doc/constraints.rst.py
@@ -13,6 +13,9 @@ qn_to_link_map = {
 }
 
 def make_constraints():
+    # All observables (pseudo or otherwise), to be used to see if a constraint is an observable or a parameter
+    obs = [qn for qn, _ in eos.Observables()]
+
     result = []
     for qn, entry in eos.Constraints():
         data = yaml.safe_load(entry.serialize())
@@ -25,7 +28,12 @@ def make_constraints():
         observable_entries = []
         for oqn in unique_observables:
             anchor = oqn.translate(qn_to_link_map).lower()
-            observable_entries.append(f'`{ oqn } <observables.html#{ anchor }>`_')
+            if oqn in obs:
+                # We are looking at a (pseudo-)observable so link to the observables page
+                observable_entries.append(f'`{ oqn } <observables.html#{ anchor }>`_')
+            else:
+                # We are looking at a parameter, so link there
+                observable_entries.append(f'`{ oqn } <parameters.html#{ anchor }>`_')
 
         constraint = {
             'observables': ', '.join(observable_entries)
