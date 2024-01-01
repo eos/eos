3fb45919eb8475bebcc724e5027d4955f40a9f27
Danny van Dyk <danny.van.dyk@gmail.com>
2024-01-01 13:45:55 +0000

    [eos] Check names of kinematic variables against a whitelist
    GitHub: resolves #750

--- eos/observable_TEST.cc
+++ eos/observable_TEST.cc
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2021 Méril Reboud
+ * Copyright (c) 2021-2023 Danny van Dyk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -19,6 +20,9 @@
 #include <eos/observable.hh>
 #include <eos/utils/options.hh>
 #include <eos/utils/units.hh>
+
+#include <ranges>
+#include <iostream>
 
 using namespace test;
 using namespace eos;
@@ -126,6 +130,60 @@ class ObservableTest :
                         <<test::obs1>>[q2_min=>q2_min_num] * {q2_min}
                     )")
                 );
+            }
+
+            /* Test the names of the kinematic variables against a whitelist */
+            {
+                static const std::set<std::string> allowed_kinematic_variables
+                {
+                    "q2", "q2_min", "q2_max",
+                    "q2_num", "q2_min_num", "q2_max_num",
+                    "q2_denom", "q2_min_denom", "q2_max_denom",
+                    "Re{q2}", "Im{q2}",
+                    "q2_e", "q2_e_min", "q2_e_max",
+                    "q2_mu", "q2_mu_min", "q2_mu_max",
+                    "q2_tau", "q2_tau_min", "q2_tau_max",
+                    "k2", "k2_min", "k2_max", "k2_min_num", "k2_max_num", "k2_min_denom", "k2_max_denom",
+                    "cos(theta_l)",
+                    "cos(theta_pi)",
+                    "cos(theta_k)",
+                    "phi",
+                    "E", "E_min", "E_max",
+                    "Re{E}", "Im{E}",
+                    "E_gamma", "E_gamma_min",
+                    "E_pi",
+                    "w", "w_min", "w_max", "w_min_num", "w_max_num", "w_min_denom", "w_max_denom",
+                    "mu", "tau",
+                    "z", "z_min", "z_max", "z_min_num", "z_max_num", "z_min_denom", "z_max_denom",
+                    // needs to be unified with the notation of Re{E} and Im{E}
+                    "q2_real", "q2_imag",
+                    // needs to be rewritten in terms of cos(theta_l)
+                    "theta_l",
+                    // deprecated
+                    "s", "s_min", "s_max",
+                    "i",
+                    "c_D", "c_D_min", "c_D_max",
+                    "c_l", "c_l_min", "c_l_max",
+                    "chi", "chi_min", "chi_max",
+                };
+
+                auto observables = Observables();
+
+                bool found_problematic_observable = false;
+
+                for (auto const & [name, entry] : observables)
+                {
+                    for (auto const & kinematic_variable : std::ranges::subrange(entry->begin_kinematic_variables(), entry->end_kinematic_variables()))
+                    {
+                        if (allowed_kinematic_variables.find(kinematic_variable) != allowed_kinematic_variables.end())
+                            continue;
+
+                        found_problematic_observable = true;
+                        std::cerr << "Found problematic observable: " << name << ", with kinematic variable: " << kinematic_variable << std::endl;
+                    }
+                }
+
+                TEST_CHECK(found_problematic_observable == false);
             }
         }
 
