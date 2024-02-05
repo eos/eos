44a73235a3b69643b25457dddb8be4938bfaeadd
Méril Reboud <merilreboud@gmail.com>
2024-02-05 16:57:55 +0100

    [maths] Fix the ordering in the tests

--- eos/maths/power-of_TEST.cc
+++ eos/maths/power-of_TEST.cc
@@ -42,33 +42,33 @@ class PowerOfTest :
                 // Test power_of<n, double>
                 static const double eps = 1e-14;
 
-                TEST_CHECK_NEARLY_EQUAL(1.0,      power_of<0>(1.2), eps);
-                TEST_CHECK_NEARLY_EQUAL(1.2,      power_of<1>(1.2), eps);
-                TEST_CHECK_NEARLY_EQUAL(1.44,     power_of<2>(1.2), eps);
-                TEST_CHECK_NEARLY_EQUAL(1.728,    power_of<3>(1.2), eps);
-                TEST_CHECK_NEARLY_EQUAL(2.0736,   power_of<4>(1.2), eps);
-                TEST_CHECK_NEARLY_EQUAL(2.48832,  power_of<5>(1.2), eps);
-                TEST_CHECK_NEARLY_EQUAL(2.985984, power_of<6>(1.2), eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<0>(1.2), 1.0,      eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<1>(1.2), 1.2,      eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<2>(1.2), 1.44,     eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<3>(1.2), 1.728,    eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<4>(1.2), 2.0736,   eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<5>(1.2), 2.48832,  eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<6>(1.2), 2.985984, eps);
 
-                TEST_CHECK_NEARLY_EQUAL(1.0,      power_of<0>(0.4), eps);
-                TEST_CHECK_NEARLY_EQUAL(0.4,      power_of<1>(0.4), eps);
-                TEST_CHECK_NEARLY_EQUAL(0.16,     power_of<2>(0.4), eps);
-                TEST_CHECK_NEARLY_EQUAL(0.064,    power_of<3>(0.4), eps);
-                TEST_CHECK_NEARLY_EQUAL(0.0256,   power_of<4>(0.4), eps);
-                TEST_CHECK_NEARLY_EQUAL(0.01024,  power_of<5>(0.4), eps);
-                TEST_CHECK_NEARLY_EQUAL(0.004096, power_of<6>(0.4), eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<0>(0.4), 1.0,      eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<1>(0.4), 0.4,      eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<2>(0.4), 0.16,     eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<3>(0.4), 0.064,    eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<4>(0.4), 0.0256,   eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<5>(0.4), 0.01024,  eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<6>(0.4), 0.004096, eps);
             }
 
             {
                 // Test power_of<n, std::complex<double>>
                 static const double eps = 1e-14;
 
-                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(1.0, 0.0),       power_of<0>(std::complex<double>(1.2, 0.9)), eps);
-                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(0.0, -1.0),      power_of<3>(std::complex<double>(0.0, 1.0)), eps);
-                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(1.0, 0.0),       power_of<4>(std::complex<double>(0.0, 1.0)), eps);
-                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(-9.6719, -1.56), power_of<4>(std::complex<double>(1.2, 1.3)), eps);
-                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(2.985984, 0.0),  power_of<6>(std::complex<double>(1.2, 0.0)), eps);
-                TEST_CHECK_NEARLY_EQUAL(std::complex<double>(0.004096, 0.0),  power_of<6>(std::complex<double>(0.4, 0.0)), eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<0>(std::complex<double>(1.2, 0.9)), std::complex<double>(1.0, 0.0),       eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<3>(std::complex<double>(0.0, 1.0)), std::complex<double>(0.0, -1.0),      eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<4>(std::complex<double>(0.0, 1.0)), std::complex<double>(1.0, 0.0),       eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<4>(std::complex<double>(1.2, 1.3)), std::complex<double>(-9.6719, -1.56), eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<6>(std::complex<double>(1.2, 0.0)), std::complex<double>(2.985984, 0.0),  eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<6>(std::complex<double>(0.4, 0.0)), std::complex<double>(0.004096, 0.0),  eps);
             }
 
             {
@@ -77,9 +77,9 @@ class PowerOfTest :
 
                 Parameters p = Parameters::Defaults();
 
-                TEST_CHECK_NEARLY_EQUAL(1.0,                              power_of<0>(p["mass::D^0"]), eps);
-                TEST_CHECK_NEARLY_EQUAL(p["mass::D^0"],                   power_of<1>(p["mass::D^0"]), eps);
-                TEST_CHECK_NEARLY_EQUAL(p["mass::D^0"] * p["mass::D^0"],  power_of<2>(p["mass::D^0"]), eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<0>(p["mass::D^0"]), 1.0,                              eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<1>(p["mass::D^0"]), p["mass::D^0"],                   eps);
+                TEST_CHECK_NEARLY_EQUAL(power_of<2>(p["mass::D^0"]), p["mass::D^0"] * p["mass::D^0"],  eps);
             }
         }
 } power_of_test;
