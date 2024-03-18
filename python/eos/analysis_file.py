22c07913283c1c3cdf8aacd4746b5a26ce7e9ea4
Matthew Kirk <matthew.j.kirk@durham.ac.uk>
2024-03-18 17:23:47 +0000

    [python] Make AnalysisFile use the parsing code

--- python/eos/analysis_file.py
+++ python/eos/analysis_file.py
@@ -20,6 +20,10 @@
 import eos
 import os
 import yaml
+from dataclasses import asdict
+from .analysis_file_description import PriorComponent, LikelihoodComponent, PosteriorDescription, \
+                                       PredictionDescription, ObservableComponent, ParameterComponent, \
+                                       StepComponent, PriorDescription
 
 class AnalysisFile:
     """Represents a collection of statistical analyses and their building blocks.
@@ -39,86 +43,63 @@ class AnalysisFile:
         if not os.path.isfile(analysis_file):
             raise RuntimeError(f'Cannot load analysis file: \'{analysis_file}\' is not a file')
 
-        instructions = None
         with open(analysis_file) as input_file:
-            input_data = yaml.load(input_file, Loader=yaml.SafeLoader)
+            input_data = yaml.safe_load(input_file)
 
         if 'priors' not in input_data:
             raise RuntimeError('Cannot load analysis file: need at least one prior component')
 
-        self._priors = { prior['name'] : dict(prior) for prior in input_data['priors'] }
+        self._priors = { p["name"]: PriorComponent.from_dict(**p) for p in input_data['priors'] }
 
         if 'likelihoods' not in input_data:
             raise RuntimeError('Cannot load analysis file: need at least one likelihood component')
 
-        self._likelihoods = { lh['name'] : dict(lh) for lh in input_data['likelihoods'] }
+        self._likelihoods = { ll["name"]: LikelihoodComponent.from_dict(**ll) for ll in input_data['likelihoods'] }
 
         if 'posteriors' not in input_data:
             raise RuntimeError('Cannot load analysis file: need at least one posterior')
 
-        self._posteriors = { posterior['name'] : dict(posterior) for posterior in input_data['posteriors'] }
+        self._posteriors = { p["name"]: PosteriorDescription.from_dict(**p) for p in input_data['posteriors'] }
+        # Check that the priors and likelihoods referenced by the posteriors are defined
+        for pc in self._posteriors.values():
+            for p in pc.prior:
+                if p not in self._priors:
+                    raise RuntimeError(f'Posterior \'{pc.name}\' references prior \'{p}\' which is not defined')
+            for l in pc.likelihood:
+                if l not in self._likelihoods:
+                    raise RuntimeError(f'Posterior \'{pc.name}\' references likelihood \'{l}\' which is not defined')
 
         # Optional: provide a list of observables for posterior prediction
         if 'predictions' not in input_data:
             self._predictions = []
         else:
-            self._predictions = { pred['name'] : dict(pred) for pred in input_data['predictions'] }
+            self._predictions = { p["name"]: PredictionDescription.from_dict(**p) for p in input_data['predictions'] }
 
         # Optional: insert custom observables using the expression parser
         if 'observables' in input_data:
-            for name in input_data['observables']:
+            _obs = [ObservableComponent.from_dict(name=n, **d) for n,d in input_data['observables'].items()]
+            for o in _obs:
+                eos.Observables().insert(o.name, o.latex, eos.Unit(o.unit), eos.Options(**o.options), o.expression)
+                eos.info(f'Successfully inserted observable: { o.name }')
 
-                required_keys = {'latex', 'unit', 'options', 'expression'}
-                optional_keys = {'alias_of'}
-                provided_keys = input_data['observables'][name].keys()
-
-                missing_keys  = required_keys - provided_keys
-                if missing_keys:
-                    raise KeyError(f'Missing keys for observable { name }: { missing_keys }')
-
-                ignored_keys = provided_keys - required_keys - optional_keys
-                if ignored_keys:
-                    eos.warn(f'Ignoring unknown keys for observable { name }: { ignored_keys }')
-
-                obs = input_data['observables'][name]
-                options = eos.Options(**obs['options'])
-                eos.Observables().insert(name, obs['latex'], eos.Unit(obs['unit']), options, obs['expression'])
-                eos.info(f'Successfully inserted observable: { name }')
-
-        # Optional: declare custom parameters before creating an analysis
         if 'parameters' in input_data:
-            for name in input_data['parameters']:
-
-                required_keys = {'latex', 'unit', 'central', 'min', 'max'}
-                provided_keys = input_data['parameters'][name].keys()
-
-                missing_keys  = required_keys - provided_keys
-                if missing_keys:
-                    raise KeyError(f'Missing keys for parameter { name }: { missing_keys }')
-
-                ignored_keys = provided_keys - required_keys
-                if ignored_keys:
-                    eos.warn(f'Ignoring unknown keys for parameter { name }: { ignored_keys }')
-
-                param = input_data['parameters'][name]
+            _params = [ParameterComponent.from_dict(name=n, **d) for n, d in input_data["parameters"].items()]
+            for p in _params:
                 try:
-                    qn = eos.QualifiedName(name)
-                    unit = eos.Unit(param['unit'])
-                    central, min, max = float(param['central']), float(param['min']), float(param['max'])
-                    id = eos.Parameters.declare(qn, param['latex'], unit, central, min, max)
+                    qn = eos.QualifiedName(p.name)
+                    id = eos.Parameters.declare(qn, p.latex, eos.Unit(p.unit), p.central, p.min, p.max)
                     eos.info(f'Successfully declared parameter: {qn}')
-                    if 'alias_of' in param:
-                        for aliased_qn in param['alias_of']:
+                    if p.alias_of:
+                        for aliased_qn in p.alias_of:
                             eos.Parameters.redirect(eos.QualifiedName(aliased_qn), id)
                             eos.info(f'Successfully declared alias: {aliased_qn} -> {qn}')
                 except eos.Exception as e:
-                    raise ValueError(f'Unexpected value encountered in description of parameter \'{name}\': {e}')
+                    raise ValueError(f'Unexpected value encountered in description of parameter \'{p.name}\': {e}')
 
         if 'steps' not in input_data:
             self._steps = []
         else:
-            self._steps = input_data['steps']
-
+            self._steps = [StepComponent.from_dict(**s) for s in input_data['steps']]
 
     def analysis(self, _posterior):
         """Create an eos.Analysis object for the named posterior."""
@@ -128,53 +109,32 @@ class AnalysisFile:
         posterior = self._posteriors[_posterior]
 
         prior = []
-        for p in posterior['prior']:
-            descriptions = []
-
-            if 'descriptions' in self._priors[p]:
-                if 'parameters' in self._priors[p]:
-                    eos.error(f'Both \'descriptions\' and \'parameters\' are provided for prior component \'{p}\', ignoring legacy support for \'parameters\'')
-
-                descriptions = self._priors[p]['descriptions']
-            elif 'parameters' in self._priors[p]:
-                eos.warn(f'\'parameters\' is in the description of prior component \'{p}\', use \'descriptions\' instead')
-                descriptions = self._priors[p]['parameters']
-            else:
-                raise KeyError(f'Missing entry in prior component \'{p}\': \'descriptions\' is not provided')
-
-            prior.extend(descriptions)
+        for p in posterior.prior:
+            prior.extend(self._priors[p].descriptions)
 
         parameters = eos.Parameters()
 
         likelihood = []
-        manual_constraints = {}
+        manual_constraints = []
         external_likelihood = []
-        for lh in posterior['likelihood']:
-            LH_ALLOWED_KEYS = { 'name', 'constraints', 'manual_constraints', 'pyhf'}
-            for key in self._likelihoods[lh]:
-                if key not in LH_ALLOWED_KEYS:
-                    raise KeyError(f"Unsupported key in 'likelihoods['{lh}']': {key}")
-
-            if 'constraints' not in self._likelihoods[lh] and 'manual_constraints' not in self._likelihoods[lh] and 'pyhf' not in self._likelihoods[lh]:
-                raise KeyError(f'Missing entry in \'likelihoods[\'{lh}\']\': need exactly one of: \'constraints\', \'manual_constraints\' or \'pyhf\'')
-
-            likelihood.extend(self._likelihoods[lh]['constraints'] if 'constraints' in self._likelihoods[lh] else [])
-            manual_constraints.update(self._likelihoods[lh]['manual_constraints'] if 'manual_constraints' in self._likelihoods[lh] else {})
+        for lh in posterior.likelihood:
+            likelihood.extend(self._likelihoods[lh].constraints)
+            manual_constraints.extend(self._likelihoods[lh].manual_constraints)
 
             # create a pyhf likelihood
-            if 'pyhf' in self._likelihoods[lh]:
-                workspace = self._likelihoods[lh]['pyhf']['file']
-                parameter_map = self._likelihoods[lh]['pyhf']['parameter_map'] if 'parameter_map' in self._likelihoods[lh]['pyhf'] else []
+            if self._likelihoods[lh].pyhf:
+                workspace = self._likelihoods[lh].pyhf.file # TODO: Convert this path relative to analysis_file?
+                parameter_map = self._likelihoods[lh].pyhf.parameter_map
                 cache = eos.ObservableCache(parameters)
 
                 # extend prior
                 pyhf_priors = eos.PyhfLogLikelihood.factory(cache, workspace, parameter_map).priors()
-                existing_priors = [prior['parameter'] for prior in prior]
+                existing_priors = [p.parameter for p in prior]
                 # only add missing pyhf priors
                 for pyhf_prior in pyhf_priors:
                     if pyhf_prior['parameter'] not in existing_priors:
                         eos.info(f'pyhf workspace parameter {pyhf_prior["parameter"]} added to prior; manually specify this prior to overwrite settings')
-                        prior.append(pyhf_prior)
+                        prior.append(PriorDescription.from_dict(**pyhf_prior))
 
                 # create likelihood block
                 llh_block = eos.LogLikelihoodBlock.External(
@@ -184,8 +144,13 @@ class AnalysisFile:
 
                 external_likelihood.extend([llh_block])
 
-        global_options = posterior['global_options'] if 'global_options' in posterior else {}
-        fixed_parameters = posterior['fixed_parameters'] if 'fixed_parameters' in posterior else {}
+        global_options = posterior.global_options
+        fixed_parameters = posterior.fixed_parameters
+
+        #  Convert back to dictionaries (for now) as that is what Analysis expects
+        prior = [ asdict(pc) for pc in prior]
+        likelihood = [ d["constraint"] for d in (asdict(lc) for lc in likelihood) ]
+        manual_constraints = { d["name"]: d["info"] for d in (asdict(mc) for mc in manual_constraints) }
 
         return eos.Analysis(prior, likelihood,
                             external_likelihood=external_likelihood,
@@ -205,37 +170,32 @@ class AnalysisFile:
         posterior = self._posteriors[_posterior]
         prediction = self.predictions[_prediction]
 
-        global_options = posterior['global_options'] if 'global_options' in posterior else {}
-        fixed_parameters = posterior['fixed_parameters'] if 'fixed_parameters' in posterior else {}
+        global_options = posterior.global_options
+        fixed_parameters = posterior.fixed_parameters
 
-        if 'global_options' in prediction:
-            global_options.update(prediction['global_options'])
-
-        if 'fixed_parameters' in prediction:
-            fixed_parameters.update(prediction['fixed_parameters'])
+        global_options.update(prediction.global_options)
+        fixed_parameters.update(prediction.fixed_parameters)
 
         options = eos.Options(**global_options)
         for (p, v) in fixed_parameters.items():
             parameters.set(p, v)
 
-        for o in prediction['observables']:
-            options_part = eos.QualifiedName(o['name']).options_part()
+        for o in prediction.observables:
+            options_part = eos.QualifiedName(o.name).options_part()
             for key, value in options_part:
                 if key in global_options and global_options[key] != value:
-                    eos.error(f'Global option {key}={global_options[key]} overrides option part specification {key}={value} for observable {o["name"]} in prediction {_prediction}.')
+                    eos.error(f'Global option {key}={global_options[key]} overrides option part specification {key}={value} for observable {o.name} in prediction {_prediction}.')
 
         observables = []
-        for o in prediction['observables']:
-            if 'kinematics' not in o:
-                kinematics = [{}]
-            elif type(o['kinematics']) == dict:
-                kinematics = [o['kinematics']]
+        for o in prediction.observables:
+            if type(o.kinematics) == dict:
+                kinematics = [o.kinematics]
             else:
-                kinematics = [k for k in o['kinematics']]
+                kinematics = [k for k in o.kinematics]
 
             for k in kinematics:
                 observables.append(eos.Observable.make(
-                    o['name'],
+                    o.name,
                     parameters,
                     eos.Kinematics(k),
                     options
@@ -243,9 +203,9 @@ class AnalysisFile:
 
         if None in observables:
             unknown_observables = set()
-            for p, o in zip(prediction['observables'], observables):
+            for p, o in zip(prediction.observables, observables):
                 if o is None:
-                    unknown_observables.add(p['name'])
+                    unknown_observables.add(p.name)
             raise RuntimeError(f'Prediction \'{_prediction}\' contains unknown observable names: {unknown_observables}')
 
         return observables
@@ -343,11 +303,11 @@ class AnalysisFile:
             return result
 
         def _handle_single_step(step_desc, step_arguments):
-            step_name = step_desc['name']
-            tasks     = step_desc['tasks']
+            step_name = step_desc.name
+            tasks     = step_desc.tasks
 
             for task_desc in tasks:
-                task = task_desc['task']
+                task = task_desc.task
                 if task not in _tasks.keys():
                     raise ValueError(f'Unknown task \"{task}\" encountered in step {step_name}')
 
@@ -358,8 +318,8 @@ class AnalysisFile:
 
                     arguments[key] = value
 
-                if 'arguments' in task_desc:
-                    arguments.update(task_desc['arguments'])
+                if task_desc.arguments:
+                    arguments.update(task_desc.arguments)
 
                 if task in step_arguments:
                     arguments.update(step_arguments[task])
@@ -373,37 +333,28 @@ class AnalysisFile:
 
         # check inputs
         for step_idx, step_desc in enumerate(self._steps):
-            if type(step_desc) is not dict:
-                raise ValueError(f'Description of step #{step_idx} is not a key/value map')
-
-            if 'name' not in step_desc:
-                raise ValueError(f'Description of step #{step_idx} requires a name')
-
-            if 'tasks' not in step_desc:
-                raise ValueError('Description of step #{step_idx} ({step["name"]}) requires a list of tasks')
-
-            if 'iterations' in step_desc:
-                if type(step_desc['iterations']) is not list:
-                    raise ValueError(f'Description of step #{step_idx} ({step["name"]} expects a list of iterations')
+            if step_desc.iterations:
+                if type(step_desc.iterations) is not list:
+                    raise ValueError(f'Description of step #{step_idx} ({step_desc.name} expects a list of iterations')
 
         # resolve dependencies
         graph = nx.MultiDiGraph()
-        graph.add_nodes_from([step['name'] for step in self._steps])
+        graph.add_nodes_from([step.name for step in self._steps])
         for step in self._steps:
-            if not 'depends-on' in step:
+            if not step.depends_on:
                 continue
 
-            for dep in step['depends-on']:
-                graph.add_edge(dep, step['name'])
+            for dep in step.depends_on:
+                graph.add_edge(dep, step.name)
         steps = list(nx.topological_sort(graph))
 
-        # return steps and theirs tasks in the order imposed by the dependency graph
+        # return steps and their tasks in the order imposed by the dependency graph
         result = []
         for step in steps:
-            step_desc = { sd['name']: sd for sd in self._steps }[step]
+            step_desc = { sd.name: sd for sd in self._steps }[step]
             iterations = [{}]
-            if 'iterations' in step_desc:
-                iterations = step_desc['iterations']
+            if step_desc.iterations:
+                iterations = step_desc.iterations
 
             for iteration_arguments in iterations:
                 result.extend(_handle_single_step(step_desc, iteration_arguments))
