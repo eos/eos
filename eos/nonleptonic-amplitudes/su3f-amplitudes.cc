441aa486571cef9f9dfead9f3ea8b4af99c83dbe
Méril Reboud <merilreboud@gmail.com>
2024-03-25 17:30:08 +0100

    [nonleptonic-amplitudes] Implement nonleptonic amplitudes in the topological and su3 representations

---
+++ eos/nonleptonic-amplitudes/su3f-amplitudes.cc
@@ -0,0 +1,279 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2024 Marta Burgos
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#include <eos/nonleptonic-amplitudes/su3f-amplitudes.hh>
+#include <eos/maths/power-of.hh>
+#include <eos/utils/options-impl.hh>
+
+#include <map>
+
+namespace eos
+{
+    using std::sqrt;
+
+    NonleptonicAmplitudes<PToPP> *
+    SU3FRepresentation<PToPP>::make(const Parameters & p, const Options & o)
+    {
+        return new SU3FRepresentation<PToPP>(p, o);
+    }
+
+    SU3FRepresentation<PToPP>::SU3FRepresentation(const Parameters & p, const Options & o):
+        model(Model::make(o.get("model", "SM"), p, o)),
+        opt_q(o, options, "q"),
+        opt_p1(o, options, "P1"),
+        opt_p2(o, options, "P2"),
+        opt_cp_conjugate(o, options, "cp-conjugate"),
+        theta_18(p["eta::theta_18"], *this),
+        B(su3f::psd_b_triplet.find(opt_q.value())->second),
+        H3bar({}),
+        H3tilde({}),
+        P1{{}},
+        P2{{}},
+        H6bar({}),
+        H6tilde({}),
+        H15bar({}),
+        H15tilde({}),
+        Gfermi(p["WET::G_Fermi"], *this),
+        re_AT3( p["nonleptonic::Re{AT3}@SU3F"] , *this),
+        im_AT3( p["nonleptonic::Im{AT3}@SU3F"] , *this),
+        re_CT3( p["nonleptonic::Re{CT3}@SU3F"] , *this),
+        im_CT3( p["nonleptonic::Im{CT3}@SU3F"] , *this),
+        re_AT6( p["nonleptonic::Re{AT6}@SU3F"] , *this),
+        im_AT6( p["nonleptonic::Im{AT6}@SU3F"] , *this),
+        re_CT6( p["nonleptonic::Re{CT6}@SU3F"] , *this),
+        im_CT6( p["nonleptonic::Im{CT6}@SU3F"] , *this),
+        re_AT15(p["nonleptonic::Re{AT15}@SU3F"], *this),
+        im_AT15(p["nonleptonic::Im{AT15}@SU3F"], *this),
+        re_CT15(p["nonleptonic::Re{CT15}@SU3F"], *this),
+        im_CT15(p["nonleptonic::Im{CT15}@SU3F"], *this),
+        re_BT3( p["nonleptonic::Re{BT3}@SU3F"] , *this),
+        im_BT3( p["nonleptonic::Im{BT3}@SU3F"] , *this),
+        re_BT6( p["nonleptonic::Re{BT6}@SU3F"] , *this),
+        im_BT6( p["nonleptonic::Im{BT6}@SU3F"] , *this),
+        re_BT15(p["nonleptonic::Re{BT15}@SU3F"], *this),
+        im_BT15(p["nonleptonic::Im{BT15}@SU3F"], *this),
+        re_DT3( p["nonleptonic::Re{DT3}@SU3F"] , *this),
+        im_DT3( p["nonleptonic::Im{DT3}@SU3F"] , *this),
+        re_AP3( p["nonleptonic::Re{AP3}@SU3F"] , *this),
+        im_AP3( p["nonleptonic::Im{AP3}@SU3F"] , *this),
+        re_CP3( p["nonleptonic::Re{CP3}@SU3F"] , *this),
+        im_CP3( p["nonleptonic::Im{CP3}@SU3F"] , *this),
+        re_AP6( p["nonleptonic::Re{AP6}@SU3F"] , *this),
+        im_AP6( p["nonleptonic::Im{AP6}@SU3F"] , *this),
+        re_CP6( p["nonleptonic::Re{CP6}@SU3F"] , *this),
+        im_CP6( p["nonleptonic::Im{CP6}@SU3F"] , *this),
+        re_AP15(p["nonleptonic::Re{AP15}@SU3F"], *this),
+        im_AP15(p["nonleptonic::Im{AP15}@SU3F"], *this),
+        re_CP15(p["nonleptonic::Re{CP15}@SU3F"], *this),
+        im_CP15(p["nonleptonic::Im{CP15}@SU3F"], *this),
+        re_BP3( p["nonleptonic::Re{BP3}@SU3F"] , *this),
+        im_BP3( p["nonleptonic::Im{BP3}@SU3F"] , *this),
+        re_BP6( p["nonleptonic::Re{BP6}@SU3F"] , *this),
+        im_BP6( p["nonleptonic::Im{BP6}@SU3F"] , *this),
+        re_BP15(p["nonleptonic::Re{BP15}@SU3F"], *this),
+        im_BP15(p["nonleptonic::Im{BP15}@SU3F"], *this),
+        re_DP3( p["nonleptonic::Re{DP3}@SU3F"] , *this),
+        im_DP3( p["nonleptonic::Im{DP3}@SU3F"] , *this)
+    {
+        Context ctx("When constructing B->PP SU3 amplitudes");
+
+        if (opt_cp_conjugate.value())
+        {
+            lamdu = model->ckm_ub() * conj(model->ckm_ud());
+            lamsu = model->ckm_ub() * conj(model->ckm_us());
+            lamdt = model->ckm_tb() * conj(model->ckm_td());
+            lamst = model->ckm_tb() * conj(model->ckm_ts());
+        }
+        else
+        {
+            lamdu = conj(model->ckm_ub()) * model->ckm_ud();
+            lamsu = conj(model->ckm_ub()) * model->ckm_us();
+            lamdt = conj(model->ckm_tb()) * model->ckm_td();
+            lamst = conj(model->ckm_tb()) * model->ckm_ts();
+        }
+
+        H3bar[1] = +lamdu;
+        H3bar[2] = +lamsu;
+
+        H6bar[0][1][0] = +lamdu;
+        H6bar[1][0][0] = -lamdu;
+        H6bar[1][2][2] = +lamdu;
+        H6bar[2][1][2] = -lamdu;
+
+        H6bar[0][2][0] = +lamsu;
+        H6bar[2][0][0] = -lamsu;
+        H6bar[2][1][1] = +lamsu; // Corrected with respect to typo in [HTX:2021A]
+        H6bar[1][2][1] = -lamsu; // Corrected with respect to typo in [HTX:2021A]
+
+        H15bar[0][1][0] = +3.0 * lamdu;
+        H15bar[1][0][0] = +3.0 * lamdu;
+        H15bar[1][1][1] = -2.0 * lamdu;
+        H15bar[1][2][2] = -lamdu;
+        H15bar[2][1][2] = -lamdu;
+
+        H15bar[0][2][0] = +3.0 * lamsu;
+        H15bar[2][0][0] = +3.0 * lamsu;
+        H15bar[2][2][2] = -2.0 * lamsu;
+        H15bar[2][1][1] = -lamsu; // Corrected with respect to typo in [HTX:2021A]
+        H15bar[1][2][1] = -lamsu; // Corrected with respect to typo in [HTX:2021A]
+
+
+        H3tilde[1] = +lamdt;
+        H3tilde[2] = +lamst;
+
+        H6tilde[0][1][0] = +lamdt;
+        H6tilde[1][0][0] = -lamdt;
+        H6tilde[1][2][2] = +lamdt;
+        H6tilde[2][1][2] = -lamdt;
+
+        H6tilde[0][2][0] = +lamst;
+        H6tilde[2][0][0] = -lamst;
+        H6tilde[2][1][1] = +lamst; // Corrected with respect to typo in [HTX:2021A]
+        H6tilde[1][2][1] = -lamst; // Corrected with respect to typo in [HTX:2021A]
+
+        H15tilde[0][1][0] = +3.0 * lamdt;
+        H15tilde[1][0][0] = +3.0 * lamdt;
+        H15tilde[1][1][1] = -2.0 * lamdt;
+        H15tilde[1][2][2] = -lamdt;
+        H15tilde[2][1][2] = -lamdt;
+
+        H15tilde[0][2][0] = +3.0 * lamst;
+        H15tilde[2][0][0] = +3.0 * lamst;
+        H15tilde[2][2][2] = -2.0 * lamst;
+        H15tilde[2][1][1] = -lamst; // Corrected with respect to typo in [HTX:2021A]
+        H15tilde[1][2][1] = -lamst; // Corrected with respect to typo in [HTX:2021A]
+    };
+
+    const std::vector<OptionSpecification>
+    SU3FRepresentation<PToPP>::options
+    {
+        Model::option_specification(),
+        { "cp-conjugate", { "true", "false" },  "false" },
+        { "q", { "u", "d", "s" } },
+        { "P1", { "pi^0", "pi^+", "pi^-", "K_d", "Kbar_d", "K_u", "Kbar_u", "eta", "eta_prime" } },
+        { "P2", { "pi^0", "pi^+", "pi^-", "K_d", "Kbar_d", "K_u", "Kbar_u", "eta", "eta_prime" } },
+    };
+
+    complex<double>
+    SU3FRepresentation<PToPP>::tree_amplitude(su3f::rank2 & p1, su3f::rank2 & p2) const
+    {
+        complex<double> T_ira = 0.0;
+        const complex<double> AT3  = complex<double>(this->re_AT3(),  this->im_AT3()),
+                              CT3  = complex<double>(this->re_CT3(),  this->im_CT3()),
+                              AT6  = complex<double>(this->re_AT6(),  this->im_AT6()),
+                              CT6  = complex<double>(this->re_CT6(),  this->im_CT6()),
+                              AT15 = complex<double>(this->re_AT15(), this->im_AT15()),
+                              CT15 = complex<double>(this->re_CT15(), this->im_CT15()),
+                              BT3  = complex<double>(this->re_BT3(),  this->im_BT3()),
+                              BT6  = complex<double>(this->re_BT6(),  this->im_BT6()),
+                              BT15 = complex<double>(this->re_BT15(), this->im_BT15()),
+                              DT3  = complex<double>(this->re_DT3(),  this->im_DT3());
+
+        for (unsigned i = 0; i < 3; i++)
+        {
+            for (unsigned j = 0; j < 3; j++)
+            {
+                for (unsigned k = 0; k < 3; k++)
+                {
+                    T_ira += AT3 * B[i] * H3bar[i] * p1[j][k] * p2[k][j];
+                    T_ira += CT3 * B[i] * p1[i][j] * p2[j][k] * H3bar[k];
+                    T_ira += BT3 * B[i] * H3bar[i] * p1[k][k] * p2[j][j];
+                    T_ira += DT3 * B[i] * p1[i][j] * H3bar[j] * p2[k][k];
+
+                    for (unsigned l = 0; l < 3; l++)
+                    {
+                        T_ira += AT6 * B[i] * H6bar[i][j][k] * p1[l][j] * p2[k][l];
+                        T_ira += CT6 * B[i] * p1[i][j] * H6bar[j][l][k] * p2[k][l];
+                        T_ira += BT6 * B[i] * H6bar[i][j][k] * p1[k][j] * p2[l][l];
+
+                        T_ira += AT15 * B[i] * H15bar[i][j][k] * p1[l][j] * p2[k][l];
+                        T_ira += CT15 * B[i] * p1[i][j] * H15bar[j][k][l] * p2[l][k];
+                        T_ira += BT15 * B[i] * H15bar[i][j][k] * p1[k][j] * p2[l][l];
+                    }
+                }
+            }
+        }
+
+        return T_ira;
+    }
+
+    complex<double>
+    SU3FRepresentation<PToPP>::penguin_amplitude(su3f::rank2 & p1, su3f::rank2 & p2) const
+    {
+        complex<double> P_ira = 0.0;
+        const complex<double> AP3  = complex<double>(this->re_AP3(),  this->im_AP3()),
+                              CP3  = complex<double>(this->re_CP3(),  this->im_CP3()),
+                              AP6  = complex<double>(this->re_AP6(),  this->im_AP6()),
+                              CP6  = complex<double>(this->re_CP6(),  this->im_CP6()),
+                              AP15 = complex<double>(this->re_AP15(), this->im_AP15()),
+                              CP15 = complex<double>(this->re_CP15(), this->im_CP15()),
+                              BP3  = complex<double>(this->re_BP3(),  this->im_BP3()),
+                              BP6  = complex<double>(this->re_BP6(),  this->im_BP6()),
+                              BP15 = complex<double>(this->re_BP15(), this->im_BP15()),
+                              DP3  = complex<double>(this->re_DP3(),  this->im_DP3());
+
+        for (unsigned i = 0; i < 3; i++)
+        {
+            for (unsigned j = 0; j < 3; j++)
+            {
+                for (unsigned k = 0; k < 3; k++)
+                {
+                    P_ira += AP3 * B[i] * H3tilde[i] * p1[j][k] * p2[k][j];
+                    P_ira += CP3 * B[i] * p1[i][j] * p2[j][k] * H3tilde[k];
+                    P_ira += BP3 * B[i] * H3tilde[i] * p1[k][k] * p2[j][j];
+                    P_ira += DP3 * B[i] * p1[i][j] * H3tilde[j] * p2[k][k];
+
+                    for (unsigned l = 0; l < 3; l++)
+                    {
+                        P_ira += AP6 * B[i] * H6tilde[i][j][k] * p1[l][j] * p2[k][l];
+                        P_ira += CP6 * B[i] * p1[i][j] * H6tilde[j][l][k] * p2[k][l];
+                        P_ira += BP6 * B[i] * H6tilde[i][j][k] * p1[k][j] * p2[l][l];
+
+                        P_ira += AP15 * B[i] * H15tilde[i][j][k] * p1[l][j] * p2[k][l];
+                        P_ira += CP15 * B[i] * p1[i][j] * H15tilde[j][k][l] * p2[l][k];
+                        P_ira += BP15 * B[i] * H15tilde[i][j][k] * p1[k][j] * p2[l][l];
+                    }
+                }
+            }
+        }
+
+        return P_ira;
+    }
+
+    complex<double>
+    SU3FRepresentation<PToPP>::ordered_amplitude() const
+    {
+        this->update();
+
+        return complex<double>(0.0, 1.0) * Gfermi() / sqrt(2.0) * (
+            this->tree_amplitude(P1, P2) + this->penguin_amplitude(P1, P2)
+        );
+    }
+
+    complex<double>
+    SU3FRepresentation<PToPP>::inverse_amplitude() const
+    {
+        this->update();
+
+        return complex<double>(0.0, 1.0) * Gfermi() / sqrt(2.0) * (
+            this->tree_amplitude(P2, P1) + this->penguin_amplitude(P2, P1)
+        );
+    }
+}
---
+++ eos/nonleptonic-amplitudes/topological-amplitudes.cc
@@ -0,0 +1,229 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2024 Méril Reboud
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+#include <eos/nonleptonic-amplitudes/topological-amplitudes.hh>
+#include <eos/maths/power-of.hh>
+#include <eos/utils/options-impl.hh>
+
+#include <map>
+
+namespace eos
+{
+    using std::sqrt;
+
+    NonleptonicAmplitudes<PToPP> *
+    TopologicalRepresentation<PToPP>::make(const Parameters & p, const Options & o)
+    {
+        return new TopologicalRepresentation<PToPP>(p, o);
+    }
+
+    TopologicalRepresentation<PToPP>::TopologicalRepresentation(const Parameters & p, const Options & o) :
+        model(Model::make(o.get("model", "SM"), p, o)),
+        opt_q(o, options, "q"),
+        opt_p1(o, options, "P1"),
+        opt_p2(o, options, "P2"),
+        opt_cp_conjugate(o, options, "cp-conjugate"),
+        theta_18(p["eta::theta_18"], *this),
+        B(su3f::psd_b_triplet.find(opt_q.value())->second),
+        H1tilde({}),
+        P1({}),
+        P2({}),
+        Hbar({}),
+        H3tilde({}),
+        Gfermi(p["WET::G_Fermi"], *this),
+        re_T(  p["nonleptonic::Re{T}@Topological"],   *this),
+        im_T(  p["nonleptonic::Im{T}@Topological"],   *this),
+        re_C(  p["nonleptonic::Re{C}@Topological"],   *this),
+        im_C(  p["nonleptonic::Im{C}@Topological"],   *this),
+        re_A(  p["nonleptonic::Re{A}@Topological"],   *this),
+        im_A(  p["nonleptonic::Im{A}@Topological"],   *this),
+        re_E(  p["nonleptonic::Re{E}@Topological"],   *this),
+        im_E(  p["nonleptonic::Im{E}@Topological"],   *this),
+        re_TES(p["nonleptonic::Re{TES}@Topological"], *this),
+        im_TES(p["nonleptonic::Im{TES}@Topological"], *this),
+        re_TAS(p["nonleptonic::Re{TAS}@Topological"], *this),
+        im_TAS(p["nonleptonic::Im{TAS}@Topological"], *this),
+        re_TS( p["nonleptonic::Re{TS}@Topological"],  *this),
+        im_TS( p["nonleptonic::Im{TS}@Topological"],  *this),
+        re_TPA(p["nonleptonic::Re{TPA}@Topological"], *this),
+        im_TPA(p["nonleptonic::Im{TPA}@Topological"], *this),
+        re_TP( p["nonleptonic::Re{TP}@Topological"],  *this),
+        im_TP( p["nonleptonic::Im{TP}@Topological"],  *this),
+        re_TSS(p["nonleptonic::Re{TSS}@Topological"], *this),
+        im_TSS(p["nonleptonic::Im{TSS}@Topological"], *this),
+        re_P(  p["nonleptonic::Re{P}@Topological"],   *this),
+        im_P(  p["nonleptonic::Im{P}@Topological"],   *this),
+        re_PT( p["nonleptonic::Re{PT}@Topological"],  *this),
+        im_PT( p["nonleptonic::Im{PT}@Topological"],  *this),
+        re_S(  p["nonleptonic::Re{S}@Topological"],   *this),
+        im_S(  p["nonleptonic::Im{S}@Topological"],   *this),
+        re_PC( p["nonleptonic::Re{PC}@Topological"],  *this),
+        im_PC( p["nonleptonic::Im{PC}@Topological"],  *this),
+        re_PTA(p["nonleptonic::Re{PTA}@Topological"], *this),
+        im_PTA(p["nonleptonic::Im{PTA}@Topological"], *this),
+        re_PA( p["nonleptonic::Re{PA}@Topological"],  *this),
+        im_PA( p["nonleptonic::Im{PA}@Topological"],  *this),
+        re_PTE(p["nonleptonic::Re{PTE}@Topological"], *this),
+        im_PTE(p["nonleptonic::Im{PTE}@Topological"], *this),
+        re_PAS(p["nonleptonic::Re{PAS}@Topological"], *this),
+        im_PAS(p["nonleptonic::Im{PAS}@Topological"], *this),
+        re_PSS(p["nonleptonic::Re{PSS}@Topological"], *this),
+        im_PSS(p["nonleptonic::Im{PSS}@Topological"], *this),
+        re_PES(p["nonleptonic::Re{PES}@Topological"], *this),
+        im_PES(p["nonleptonic::Im{PES}@Topological"], *this)
+    {
+        Context ctx("When constructing B->PP topological amplitudes");
+
+        if (opt_cp_conjugate.value())
+        {
+            lamdu = model->ckm_ub() * conj(model->ckm_ud());
+            lamsu = model->ckm_ub() * conj(model->ckm_us());
+            lamdt = model->ckm_tb() * conj(model->ckm_td());
+            lamst = model->ckm_tb() * conj(model->ckm_ts());
+        }
+        else
+        {
+            lamdu = conj(model->ckm_ub()) * model->ckm_ud();
+            lamsu = conj(model->ckm_ub()) * model->ckm_us();
+            lamdt = conj(model->ckm_tb()) * model->ckm_td();
+            lamst = conj(model->ckm_tb()) * model->ckm_ts();
+        }
+
+        H1tilde[1] = lamdt;
+        H1tilde[2] = lamst;
+        Hbar[0][1][0] = lamdu;
+        Hbar[0][2][0] = lamsu;
+        H3tilde[0][1][0] = lamdt;
+        H3tilde[0][2][0] = lamst;
+    };
+
+    const std::vector<OptionSpecification>
+    TopologicalRepresentation<PToPP>::options
+    {
+        Model::option_specification(),
+        { "cp-conjugate", { "true", "false" },  "false" },
+        { "q", { "u", "d", "s" } },
+        { "P1", { "pi^0", "pi^+", "pi^-", "K_d", "Kbar_d", "K_u", "Kbar_u", "eta", "eta_prime" } },
+        { "P2", { "pi^0", "pi^+", "pi^-", "K_d", "Kbar_d", "K_u", "Kbar_u", "eta", "eta_prime" } },
+    };
+
+    complex<double>
+    TopologicalRepresentation<PToPP>::tree_amplitude(su3f::rank2 & p1, su3f::rank2 & p2) const
+    {
+        complex<double> T_tda = 0.0;
+        const complex<double> T   = complex<double>(this->re_T(),   this->im_T()),
+                              C   = complex<double>(this->re_C(),   this->im_C()),
+                              A   = complex<double>(this->re_A(),   this->im_A()),
+                              E   = complex<double>(this->re_E(),   this->im_E()),
+                              TES = complex<double>(this->re_TES(), this->im_TES()),
+                              TAS = complex<double>(this->re_TAS(), this->im_TAS()),
+                              TS  = complex<double>(this->re_TS(),  this->im_TS()),
+                              TPA = complex<double>(this->re_TPA(), this->im_TPA()),
+                              TP  = complex<double>(this->re_TP(),  this->im_TP()),
+                              TSS = complex<double>(this->re_TSS(), this->im_TSS());
+
+        for (unsigned i = 0; i < 3; i++)
+        {
+            for (unsigned j = 0; j < 3; j++)
+            {
+                for (unsigned k = 0; k < 3; k++)
+                {
+                    for (unsigned l = 0; l < 3; l++)
+                    {
+                        T_tda += T * B[i] * p1[i][j] * Hbar[j][l][k] * p2[k][l];
+                        T_tda += C * B[i] * p1[i][j] * Hbar[l][j][k] * p2[k][l];
+                        T_tda += A * B[i] * Hbar[i][l][j] * p1[j][k] * p2[k][l];
+                        T_tda += E * B[i] * Hbar[l][i][j] * p1[j][k] * p2[k][l];
+                        T_tda += TES * B[i] * Hbar[i][j][l] * p1[l][j] * p2[k][k];
+                        T_tda += TAS * B[i] * Hbar[j][i][l] * p1[l][j] * p2[k][k];
+                        T_tda += TS * B[i] * p1[i][j] * Hbar[l][j][l] * p2[k][k];
+                        T_tda += TPA * B[i] * Hbar[l][i][l] * p1[j][k] * p2[k][j];
+                        T_tda += TP * B[i] * p1[i][j] * p2[j][k] * Hbar[l][k][l];
+                        T_tda += TSS * B[i] * Hbar[l][i][l] * p1[j][j] * p2[k][k];
+                    }
+                }
+            }
+        }
+
+        return T_tda;
+    }
+
+    complex<double>
+    TopologicalRepresentation<PToPP>::penguin_amplitude(su3f::rank2 & p1, su3f::rank2 & p2) const
+    {
+        complex<double> P_tda = 0.0;
+        const complex<double> P   = complex<double>(this->re_P(),   this->im_P()),
+                              PT  = complex<double>(this->re_PT(),  this->im_PT()),
+                              S   = complex<double>(this->re_S(),   this->im_S()),
+                              PC  = complex<double>(this->re_PC(),  this->im_PC()),
+                              PTA = complex<double>(this->re_PTA(), this->im_PTA()),
+                              PA  = complex<double>(this->re_PA(),  this->im_PA()),
+                              PTE = complex<double>(this->re_PTE(), this->im_PTE()),
+                              PAS = complex<double>(this->re_PAS(), this->im_PAS()),
+                              PSS = complex<double>(this->re_PSS(), this->im_PSS()),
+                              PES = complex<double>(this->re_PES(), this->im_PES());
+
+        for (unsigned i = 0; i < 3; i++)
+        {
+            for (unsigned j = 0; j < 3; j++)
+            {
+                for (unsigned k = 0; k < 3; k++)
+                {
+                    P_tda += P * B[i] * p1[i][j] * p2[j][k] * H1tilde[k];
+                    P_tda += S * B[i] * p1[i][j] * H1tilde[j] * p2[k][k];
+                    P_tda += PA * B[i] * H1tilde[i] * p1[j][k] * p2[k][j];
+                    P_tda += PSS * B[i] * H1tilde[i] * p1[j][j] * p2[k][k];
+
+                    for (unsigned l = 0; l < 3; l++)
+                    {
+                        P_tda += PT * B[i] * p1[i][j] * H3tilde[j][l][k] * p2[k][l];
+                        P_tda += PC * B[i] * p1[i][j] * H3tilde[l][j][k] * p2[k][l];
+                        P_tda += PTA * B[i] * H3tilde[i][l][j] * p1[j][k] * p2[k][l];
+                        P_tda += PTE * B[i] * H3tilde[j][i][k] * p1[k][l] * p2[l][j];
+                        P_tda += PAS * B[i] * H3tilde[j][i][l] * p1[l][j] * p2[k][k];
+                        P_tda += PES * B[i] * H3tilde[i][j][l] * p1[l][j] * p2[k][k];
+                    }
+                }
+            }
+        }
+
+        return P_tda;
+    }
+
+    complex<double>
+    TopologicalRepresentation<PToPP>::ordered_amplitude() const
+    {
+        this->update();
+
+        return complex<double>(0.0, 1.0) * Gfermi() / sqrt(2.0) * (
+            this->tree_amplitude(P1, P2) + this->penguin_amplitude(P1, P2)
+        );
+    }
+
+    complex<double>
+    TopologicalRepresentation<PToPP>::inverse_amplitude() const
+    {
+        this->update();
+
+        return complex<double>(0.0, 1.0) * Gfermi() / sqrt(2.0) * (
+            this->tree_amplitude(P2, P1) + this->penguin_amplitude(P2, P1)
+        );
+    }
+}
