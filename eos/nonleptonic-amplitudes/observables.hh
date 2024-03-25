cf2819fcde5d972d3d8c8b2aac303095e66bf9d5
Méril Reboud <merilreboud@gmail.com>
2024-03-25 17:30:08 +0100

    [nonleptonic-amplitudes] Implement nonleptonic amplitudes in the topological and su3 representations

---
+++ eos/nonleptonic-amplitudes/observables.hh
@@ -0,0 +1,30 @@
+/* vim: set sw=4 sts=4 et tw=150 foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2024 Méril Reboud
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef EOS_GUARD_EOS_NONLEPTONIC_AMPLITUDES_OBSERVABLES_HH
+#define EOS_GUARD_EOS_NONLEPTONIC_AMPLITUDES_OBSERVABLES_HH 1
+
+#include <eos/observable-fwd.hh>
+
+namespace eos
+{
+    ObservableSection make_nonleptonic_amplitudes_section();
+}
+
+#endif
