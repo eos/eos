80d206bdeec50a23600034746a15a23ee00b716d
Danny van Dyk <danny.van.dyk@gmail.com>
2024-04-18 10:03:32 +0100

    [python] Add missing header and docstring to ``eos.Deserializable``

--- python/eos/deserializable.py
+++ python/eos/deserializable.py
@@ -1,6 +1,24 @@
+# Copyright (c) 2024 Danny van Dyk
+#
+# This file is part of the EOS project. EOS is free software;
+# you can redistribute it and/or modify it under the terms of the GNU General
+# Public License version 2, as published by the Free Software Foundation.
+#
+# EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+
 import yaml as _yaml
 
 class Deserializable:
+    """
+    Utility class to create instances of classes from YAML data.
+    """
     def __init__(self):
         pass
 
