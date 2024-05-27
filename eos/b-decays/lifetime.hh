ea9ced6a7bd06faea54c9dbe6c213a3aba6ac412
Stefan Meiser <s.meiser.2@gmx.de>
2024-05-27 17:09:36 +0100

    [b-decays] Add dimension-3/LO lifetime contributions from sbcu and dbcu operators

--- eos/b-decays/lifetime.hh
+++ eos/b-decays/lifetime.hh
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2023 Danny van Dyk
+ * Copyright (c) 2024 Stefan Meiser
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -37,8 +38,14 @@ namespace eos
             ~Lifetime();
 
             // Observables
-            double decay_width_dbcu() const;
-            double decay_width_sbcu() const;
+            // Partial decay width contributions due to dbcu operators
+            // in the heavy-quark expansion.
+            double decay_width_dbcu_dim3_lo() const;
+            double decay_width_dbcu_dim6_lo() const;
+            // Partial decay width contributions due to sbcu operators
+            // in the heavy-quark expansion
+            double decay_width_sbcu_dim3_lo() const;
+            double decay_width_sbcu_dim6_lo() const;
 
             /*!
              * References used in the computation of our observables.
