2ba1b4bc4d48ea478d8a79ee1d3b4f242a90de9c
Danny van Dyk <danny.van.dyk@gmail.com>
2024-08-26 09:53:14 +0200

    [utils] Include ``<algorithm>`` where needed

--- eos/utils/options.cc
+++ eos/utils/options.cc
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2010, 2011, 2015, 2016, 2018 Danny van Dyk
+ * Copyright (c) 2010-2024 Danny van Dyk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -23,6 +23,7 @@
 #include <eos/utils/private_implementation_pattern-impl.hh>
 #include <eos/utils/wrapped_forward_iterator-impl.hh>
 
+#include <algorithm>
 #include <map>
 #include <algorithm>
 
