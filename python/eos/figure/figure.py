f4054f0f03a53f5c5954b08cbcfba4acba4ccec8
Méril Reboud <merilreboud@gmail.com>
2024-08-28 11:55:18 +0200

    [python] (wip) Implements CornerFigure

--- python/eos/figure/figure.py
+++ python/eos/figure/figure.py
@@ -20,6 +20,8 @@ from eos.deserializable import Deserializable
 
 from .plot import Plot, PlotFactory
 
+import eos
+import os
 import copy as _copy
 import matplotlib.pyplot as plt
 import numpy as np
@@ -60,7 +62,7 @@ class SingleFigure(Figure):
 
 @dataclass(kw_only=True)
 class GridFigure(Figure):
-    r"""Represent a figure with several axes arranged in a grid.
+    r"""Represents a figure with several axes arranged in a grid.
 
     :param shape: The shape of the grid, i.e. the number of rows and columns.
     :type shape: tuple[int, int]
@@ -90,12 +92,155 @@ class GridFigure(Figure):
         return Deserializable.make(cls, **_kwargs)
 
 
+@dataclass(kw_only=True)
+class DataFileContent(Deserializable):
+    r""" Collects the relevant information for a data file to be plotted.
+
+    :param datafilepath: Path to the eos.Prediction or eos.ImportanceSample file to be used.
+    :type datafilepath: str
+    :param label: Name of the data file that will appear in the plot's legend.
+    :type label: str
+    :param color: Color to be used in the plot.
+    :type color: str (optional)
+    """
+    datafilepath:str
+    label:str
+    color:str=None
+
+    def validate(self):
+        os.path.exists(self.datafilepath) or eos.error(f"Data file '{self.datafilepath}' does not exist")
+
+    def datafile(self):
+        self.validate()
+        abspath = os.path.abspath(self.datafilepath)
+        name = os.path.split(abspath)[-1]
+
+        if name == 'samples':
+            # The datafile refers to importance (parameter) samples
+            return eos.data.ImportanceSamples(abspath)
+        elif name.startswith('pred-'):
+            # The datafile refers to observable predictions
+            return eos.data.Prediction(abspath)
+        else:
+            eos.error(f"Data file '{self.datafilepath}' has an unsupported format")
+            raise NotImplementedError
+
+    def variables(self):
+        return self.datafile().lookup_table.keys()
+
+    def variablelabels(self, variablelist):
+        self.validate()
+        abspath = os.path.abspath(self.datafilepath)
+        name = os.path.split(abspath)[-1]
+
+        if name == 'samples':
+            # The datafile refers to importance (parameter) samples
+            df = eos.data.ImportanceSamples(abspath)
+            p = eos.Parameters()
+            return [p[dist].latex() for dist in variablelist]
+        elif name.startswith('pred-'):
+            # The datafile refers to observable predictions
+            df = eos.data.Prediction(abspath)
+            o = eos.Observables()
+            return ["$" + o[dist].latex() + "$" for dist in variablelist]
+        else:
+            eos.error(f"Data file '{self.datafilepath}' has an unsupported format")
+            raise NotImplementedError
+
+
+@dataclass(kw_only=True)
+class CornerFigure(Figure):
+    r"""Produces a grid figure representing the variables described by one or several datafiles.
+    Distributions of the variables are shown on the diagonal, while correlations are plotted on the lower-left corner.
+
+    :param contents: List of contents to be drawn. Contents should be dictionnaries containing the path to the data file, a label and optionnaly a color.
+    :type contents: list[DataFileContent] or iterable[DataFileContent]
+    :param variables: List of the names of the variables to be considered. Defaults to None, in this case all variables are shown.
+    :type variables: list[str] (optional)
+    """
+    type:str=field(repr=False, init=False, default='corner')
+    contents:list[DataFileContent]
+    variables:list[str]=None
+
+    def __post_init__(self):
+        if not self.contents:
+            raise ValueError("At least one content should be plotted.")
+
+
+    def draw(self):
+        # Check that the variables of the data files match
+        for content in self.contents:
+            if self.variables:
+                if not set(self.variables).issubset(content.variables()):
+                    raise ValueError(f"The provided variables are not contained in the data file {content.datafilepath}")
+            elif content.variables() != self.contents[0].variables():
+                raise ValueError("variables defaulted to None but the provided datafiles contain different variables")
+
+        if self.variables:
+            self._variables = self.variables
+        else:
+            self._variables = list(self.contents[0].variables())
+
+        self._labels = self.contents[0].variablelabels(self._variables)
+
+        plots = []
+        size = len(self._variables)
+
+        for i in range(size):
+            for j in range(size):
+
+                if i < j:
+                    plots.append(PlotFactory.from_dict(**{
+                        'type': 'empty'
+                    }))
+
+                elif i == j:
+                    plots.append(PlotFactory.from_dict(**{
+                        'xaxis': { 'ticks': { 'visible': True }, 'label': self._labels[i] } if (i == size - 1) else { 'ticks': { 'visible': False } },
+                        'yaxis': { 'ticks': { 'visible': False } },
+                        'grid': { 'visible': True, 'axis': 'x' },
+                        'items': [
+                            {
+                                'type': 'kde1D', 'label': content.label,
+                                'datafile': content.datafilepath,
+                                'variable': self._variables[i],
+                                'color': content.color,
+                            }
+                        for content in self.contents]
+                    }))
+
+                else:
+                    plots.append(PlotFactory.from_dict(**{
+                        'xaxis': { 'ticks': { 'visible': True }, 'label': self._labels[j] } if (i == size - 1) else { 'ticks': { 'visible': False } },
+                        'yaxis': { 'ticks': { 'visible': True }, 'label': self._labels[i] } if (j == 0) else { 'ticks': { 'visible': False } },
+                        'grid': { 'visible': True},
+                        'items': [
+                            {
+                                'type': 'kde2D', 'label': content.label ,
+                                'datafile': content.datafilepath,
+                                'variables': [self._variables[j], self._variables[i]],
+                                'color': content.color,
+                            }
+                        for content in self.contents]
+                    }))
+
+        GridFigure(shape=[size, size], plots=plots).draw()
+
+    @classmethod
+    def from_dict(cls, **kwargs):
+        _kwargs = _copy.deepcopy(kwargs)
+        if 'contents' in kwargs:
+            _kwargs['contents'] = [DataFileContent.from_dict(**c) for c in kwargs['contents']]
+        return Deserializable.make(cls, **_kwargs)
+
+
 class FigureFactory:
     # Also build the documentation based on ordered registry
     # Initializer is well-defined for python version >= 3.6
     registry = OrderedDict({
         'single': SingleFigure, # default
         'grid':   GridFigure,
+        'corner': CornerFigure,
     })
 
     @staticmethod
