83a455e48b260d7690db11e77da4676b38b99a0b
Méril Reboud <merilreboud@gmail.com>
2024-09-26 17:01:05 +0100

    [nonleptonic-amplitudes] Remove the wrong implementation of S_CP

--- eos/nonleptonic-amplitudes/observables.cc
+++ eos/nonleptonic-amplitudes/observables.cc
@@ -76,17 +76,6 @@ namespace eos
                         &NonleptonicAmplitudes<PToPP>::abs_amplitude, std::make_tuple()),
                 make_nonleptonic_amplitudes_adapter("B->PP::Arg{amplitude}", R"(|\mathcal{A}^{B\to PP}|)",
                         &NonleptonicAmplitudes<PToPP>::arg_amplitude, std::make_tuple()),
-                make_expression_observable("B->PP::S_CP", R"(S_{CP})",
-                        Unit::None(),
-                        R"(
-                        -2*(<<B->PP::Re{amplitude};cp-conjugate=false>> * <<B->PP::Im{amplitude};cp-conjugate=true>>
-                        -
-                        <<B->PP::Im{amplitude};cp-conjugate=false>> * <<B->PP::Re{amplitude};cp-conjugate=true>>)
-                        /
-                        (<<B->PP::Abs{amplitude};cp-conjugate=false>> * <<B->PP::Abs{amplitude};cp-conjugate=false>>
-                        +
-                        <<B->PP::Abs{amplitude};cp-conjugate=true>> * <<B->PP::Abs{amplitude};cp-conjugate=true>>)
-                        )"),
             }
         );
 
