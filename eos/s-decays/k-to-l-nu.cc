608ddb566c3f68b163e20a06d6c5996b2a1fe355
Danny van Dyk <danny.van.dyk@gmail.com>
2025-01-31 18:53:59 +0000

    [s-decays] Add leptonic K decay

---
+++ eos/s-decays/k-to-l-nu.cc
@@ -0,0 +1,149 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2025 Danny van Dyk
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <eos/maths/power-of.hh>
+#include <eos/models/model.hh>
+#include <eos/s-decays/k-to-l-nu.hh>
+#include <eos/utils/destringify.hh>
+#include <eos/utils/options-impl.hh>
+#include <eos/utils/private_implementation_pattern-impl.hh>
+
+#include <string>
+
+namespace eos
+{
+    using std::norm;
+
+    /*
+     * Decay: K^- -> l^- nubar, adapter from cf. [DBG:2013A]
+     */
+    template <> struct Implementation<KToLeptonNeutrino>
+    {
+            SpecifiedOption opt_model;
+
+            std::shared_ptr<Model> model;
+
+            UsedParameter hbar;
+
+            UsedParameter g_fermi;
+
+            UsedParameter m_K;
+
+            UsedParameter f_K;
+
+            UsedParameter tau_K;
+
+            LeptonFlavorOption opt_l;
+
+            UsedParameter m_l;
+
+            BooleanOption opt_cp_conjugate;
+
+            UsedParameter mu;
+
+            static const std::vector<OptionSpecification> options;
+
+            Implementation(const Parameters & p, const Options & o, ParameterUser & u) :
+                opt_model(o, options, "model"),
+                model(Model::make(opt_model.value(), p, o)),
+                hbar(p["QM::hbar"], u),
+                g_fermi(p["WET::G_Fermi"], u),
+                m_K(p["mass::K_u"], u),
+                f_K(p["decay-constant::K_u"], u),
+                tau_K(p["life_time::K_u"], u),
+                opt_l(o, options, "l"),
+                m_l(p["mass::" + opt_l.str()], u),
+                opt_cp_conjugate(o, options, "cp-conjugate"),
+                mu(p["us" + opt_l.str() + "nu" + opt_l.str() + "::mu"], u)
+            {
+                Context ctx("When constructing K_u->lnu observable");
+
+                u.uses(*model);
+            }
+
+            inline double
+            beta_l() const
+            {
+                return (1.0 - power_of<2>(m_l() / m_K()));
+            }
+
+            double
+            decay_width() const
+            {
+                const WilsonCoefficients<ChargedCurrent> wc = model->wet_uslnu(opt_l.value(), opt_cp_conjugate.value());
+
+                // cf. [DBG2013], eq. (5), p. 5
+                const complex<double> ga = wc.cvl() - wc.cvr();
+                const complex<double> gp = wc.csl() - wc.csr();
+
+                // masses
+                const double m_B = this->m_K(), m_K2 = m_K * m_K;
+                const double m_l    = this->m_l();
+                const double msatmu = model->m_s_msbar(mu);
+                const double muatmu = model->m_u_msbar(mu);
+
+                return power_of<2>(g_fermi * std::abs(model->ckm_us()) * f_K * beta_l()) * m_B / (8.0 * M_PI) * norm(ga * m_l - gp * m_K2 / (msatmu + muatmu));
+            }
+
+            double
+            branching_ratio() const
+            {
+                return decay_width() * tau_K / hbar;
+            }
+    };
+
+    const std::vector<OptionSpecification> Implementation<KToLeptonNeutrino>::options{
+        Model::option_specification(),
+        { "cp-conjugate", { "true", "false" }, "false" },
+        {            "l",       { "e", "mu" },    "mu" },
+    };
+
+    KToLeptonNeutrino::KToLeptonNeutrino(const Parameters & parameters, const Options & options) :
+        PrivateImplementationPattern<KToLeptonNeutrino>(new Implementation<KToLeptonNeutrino>(parameters, options, *this))
+    {
+    }
+
+    KToLeptonNeutrino::~KToLeptonNeutrino() {}
+
+    double
+    KToLeptonNeutrino::branching_ratio() const
+    {
+        return _imp->branching_ratio();
+    }
+
+    double
+    KToLeptonNeutrino::decay_width() const
+    {
+        return _imp->decay_width();
+    }
+
+    const std::set<ReferenceName> KToLeptonNeutrino::references{ "DBG:2013A"_rn };
+
+    std::vector<OptionSpecification>::const_iterator
+    KToLeptonNeutrino::begin_options()
+    {
+        return Implementation<KToLeptonNeutrino>::options.cbegin();
+    }
+
+    std::vector<OptionSpecification>::const_iterator
+    KToLeptonNeutrino::end_options()
+    {
+        return Implementation<KToLeptonNeutrino>::options.cend();
+    }
+} // namespace eos
---
+++ eos/s-decays/k-to-l-nu.hh
@@ -0,0 +1,54 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2025 Danny van Dyk
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef EOS_GUARD_EOS_S_DECAYS_K_TO_L_NU_HH
+#define EOS_GUARD_EOS_S_DECAYS_K_TO_L_NU_HH 1
+
+#include <eos/maths/complex.hh>
+#include <eos/utils/options.hh>
+#include <eos/utils/parameters.hh>
+#include <eos/utils/private_implementation_pattern.hh>
+#include <eos/utils/reference-name.hh>
+
+namespace eos
+{
+    class KToLeptonNeutrino : public ParameterUser, public PrivateImplementationPattern<KToLeptonNeutrino>
+    {
+        public:
+            KToLeptonNeutrino(const Parameters & parameters, const Options & options);
+            ~KToLeptonNeutrino();
+
+            // Observables
+            double branching_ratio() const;
+            double decay_width() const;
+
+            /*!
+             * References used in the computation of our observables.
+             */
+            static const std::set<ReferenceName> references;
+
+            /*!
+             * Options used in the computation of our observables.
+             */
+            static std::vector<OptionSpecification>::const_iterator begin_options();
+            static std::vector<OptionSpecification>::const_iterator end_options();
+    };
+} // namespace eos
+
+#endif
