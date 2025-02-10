4669e8018eec76f3d4c130c049bda2e0a290bb21
Matthew Kirk <matthew.j.kirk@durham.ac.uk>
2025-02-10 21:20:04 +0000

    [form-factors] Implement 0->Kpi form factors following [KSvD:2025A]

--- eos/form-factors/parametric-ksvd2025.hh
+++ eos/form-factors/parametric-ksvd2025.hh
@@ -1,8 +1,8 @@
 /* vim: set sw=4 sts=4 et tw=120 foldmethod=syntax : */
 
 /*
- * Copyright (c) 2020-2024 Danny van Dyk
- * Copyright (c) 2024 Matthew J. Kirk
+ * Copyright (c) 2025 Danny van Dyk
+ * Copyright (c) 2025 Matthew Kirk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -18,8 +18,8 @@
  * Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#ifndef EOS_GUARD_EOS_FORM_FACTORS_PARAMETRIC_KKRvD2024_HH
-#define EOS_GUARD_EOS_FORM_FACTORS_PARAMETRIC_KKRvD2024_HH 1
+#ifndef EOS_GUARD_EOS_FORM_FACTORS_PARAMETRIC_KSVD2025_HH
+#define EOS_GUARD_EOS_FORM_FACTORS_PARAMETRIC_KSVD2025_HH 1
 
 #include <eos/form-factors/mesonic.hh>
 #include <eos/form-factors/mesonic-processes.hh>
@@ -32,38 +32,45 @@
 
 namespace eos
 {
-    template <typename Process_> class KKRvD2024FormFactors;
+    template <typename Process_> class KSvD2025FormFactors;
 
-    template <> class KKRvD2024FormFactors<VacuumToPiPi> :
+    template <> class KSvD2025FormFactors<VacuumToKPi> :
         public FormFactors<VacuumToPP>
     {
         private:
-            // parameters for form factor f_+ (I=1 projection)
-            std::array<UsedParameter, 8u> _b_fp_I1;
-            UsedParameter _M_fp_I1;
-            UsedParameter _G_fp_I1;
+            SpecifiedOption n_resonances_1m;
+            SpecifiedOption n_resonances_0p;
+
+            // parameters for form factor f_+
+            std::array<UsedParameter, 9u> _b_fp;
+            std::array<UsedParameter, 3u> _M_fp;
+            std::array<UsedParameter, 3u> _G_fp;
+
+            // parameters for form factor f_0
+            std::array<UsedParameter, 9u> _b_fz;
+            std::array<UsedParameter, 2u> _M_fz;
+            std::array<UsedParameter, 2u> _G_fz;
 
             // hadron masses
+            UsedParameter _m_K;
             UsedParameter _m_pi;
 
             // parameter for zero point of z
             UsedParameter _t_0;
 
-            UsedParameter _hbar;
-
-            inline std::string _par_name(const std::string & ff, const std::string & isospin, const std::string & index) const
+            inline std::string _coeff_name(const std::string & ff, const std::string & index) const
             {
-                return "0->pipi::b_(" + ff + "," + isospin + ")^" + index + "@KKRvD2024";
-            }
-
-            inline double _hbarc() const
-            {
-                return _hbar() * 299792458 * 1e15; // GeV fm
+                return "0->Kpi::b_" + ff + "^" + index + "@KSvD2025";
             }
 
             inline double _t_p() const
             {
-                return 4.0 * _m_pi() * _m_pi();
+                return power_of<2>(_m_K() + _m_pi());
+            }
+
+            inline double _t_m() const
+            {
+                return power_of<2>(_m_K() - _m_pi());
             }
 
             inline complex<double> _z(const complex<double> & q2, const double & t_0) const
@@ -78,12 +85,12 @@ namespace eos
                 return 1.0 / _z(power_of<2>(complex<double>(M, -Gamma/2)), _t_0());
             }
 
-            double _b0_fp_I1(const double & chi, const complex<double> & zr) const;
-            double _b1_fp_I1(const double & chi, const complex<double> & zr) const;
+            double _b0_fp(const double & chi_1m) const;
+            double _b0_fz(const double & chi_1m, const double & chi_0p) const;
 
         public:
-            KKRvD2024FormFactors(const Parameters & p, const Options & o);
-            ~KKRvD2024FormFactors();
+            KSvD2025FormFactors(const Parameters & p, const Options & o);
+            ~KSvD2025FormFactors();
 
             static FormFactors<VacuumToPP> * make(const Parameters & p, const Options & o);
 
@@ -91,11 +98,20 @@ namespace eos
             complex<double> z(const complex<double> & q2) const;
             complex<double> dzdq2(const complex<double> & q2) const;
             complex<double> dzdq2_II(const complex<double> & q2) const;
-            complex<double> w(const complex<double> & z) const;
-            complex<double> phitilde_p(const complex<double> & z, const double & chi) const;
-            complex<double> phitildeprime_p(const complex<double> & z, const double & chi) const;
             complex<double> series_m(const complex<double> & z, const std::array<double, 10u> & c) const;
-            double dFdq2_q2eq0() const;
+
+            /* functions pertaining to f_p */
+            complex<double> w_p(const complex<double> & z) const;
+            complex<double> phitilde_p(const complex<double> & z, const double & chi_1m) const;
+            complex<double> phitildeprime_p(const complex<double> & z, const double & chi_1m) const;
+            complex<double> resonance_product_p(const complex<double> & z) const;
+            complex<double> resonance_productprime_p(const complex<double> & z) const;
+
+            /* functions pertaining to f_z */
+            complex<double> w_z(const complex<double> & z) const;
+            complex<double> phitilde_z(const complex<double> & z, const double & chi_0p) const;
+            complex<double> phitildeprime_z(const complex<double> & z, const double & chi_0p) const;
+            complex<double> resonance_product_z(const complex<double> & z) const;
 
             /* form factors on the real axis */
             virtual complex<double> f_p(const double & q2) const override;
@@ -107,22 +123,15 @@ namespace eos
             virtual complex<double> f_0(const complex<double> & q2) const override;
             virtual complex<double> f_t(const complex<double> & q2) const override;
 
-            /* auxiliary observables */
-            double r_pi_squared() const; // squared charge radius of the pion
+            /* saturation of the dispersive bound */
+            double dispersive_integrand_p(const double & alpha) const;
+            double dispersive_integrand_z(const double & alpha) const;
+            double saturation_p() const;
+            double saturation_z() const;
 
             /* auxiliary pseudo observables */
-            double b_0() const; // value of the series coefficient b_0
-            double b_1() const; // value of the series coefficient b_1
-            complex<double> residue_rho() const; // complex-valued residue of the form factor on the rho pole
-            double re_residue_rho() const;
-            double im_residue_rho() const;
-            complex<double> residue_rho_q2() const; // complex-valued residue in q2 of the form factor on the rho pole
-            double re_residue_rho_q2() const;
-            double im_residue_rho_q2() const;
-
-            /* saturation of the dispersive bound */
-            double dispersive_integrand(const double & alpha) const;
-            double saturation() const;
+            double b0_fp() const; // value of the series coefficient b_0 for f_+
+            double b0_f0() const; // value of the series coefficient b_0 for f_0
 
             static std::vector<OptionSpecification>::const_iterator begin_options();
             static std::vector<OptionSpecification>::const_iterator end_options();
@@ -131,7 +140,7 @@ namespace eos
             static const std::set<ReferenceName> references;
     };
 
-    extern template class KKRvD2024FormFactors<VacuumToPiPi>;
+    extern template class KSvD2025FormFactors<VacuumToKPi>;
 }
 
 #endif
---
+++ eos/form-factors/parametric-ksvd2025_TEST.cc
@@ -0,0 +1,174 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2025 Matthew Kirk
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <test/test.hh>
+#include <eos/form-factors/parametric-ksvd2025.hh>
+
+#include <cmath>
+#include <limits>
+#include <vector>
+
+using namespace test;
+using namespace eos;
+
+class ParametricKSvD2025Test :
+    public TestCase
+{
+    public:
+        ParametricKSvD2025Test() :
+            TestCase("parametric_KSvD2025_test")
+        {
+        }
+
+        virtual void run() const
+        {
+            static const double eps = 1e-7;
+
+            // t0 = -1
+            {
+
+                Parameters p = Parameters::Defaults();
+                p["mass::pi^+"]                    =  0.13957;
+                p["mass::K_d"]                     =  0.497611;
+                p["0->Kpi::t_0@KSvD2025"]         = -1.0;
+                // K*(892)
+                p["0->Kpi::M_(+,0)@KSvD2025"]     =  0.890;
+                p["0->Kpi::Gamma_(+,0)@KSvD2025"] =  0.026;
+                // K*(1410)
+                p["0->Kpi::M_(+,1)@KSvD2025"]     =  1.368;
+                p["0->Kpi::Gamma_(+,1)@KSvD2025"] =  0.106;
+                // K0*(700)
+                p["0->Kpi::M_(0,0)@KSvD2025"]     =  0.680;
+                p["0->Kpi::Gamma_(0,0)@KSvD2025"] =  0.300;
+                 // K0*(1430)
+                // p["0->Kpi::M_(0,1)@KSvD2025"]     =  1.431;
+                // p["0->Kpi::Gamma_(0,1)@KSvD2025"] =  0.110;
+                p["0->Kpi::b_+^1@KSvD2025"]   =  0.1;
+                p["0->Kpi::b_+^2@KSvD2025"]   =  0.05;
+                p["0->Kpi::b_+^3@KSvD2025"]   =  0.01;
+                p["0->Kpi::b_0^1@KSvD2025"]   =  0.07;
+                p["0->Kpi::b_0^2@KSvD2025"]   =  0.03;
+                p["0->Kpi::b_0^3@KSvD2025"]   =  0.009;
+
+                /* 0->PP factory */
+                {
+                    std::shared_ptr<FormFactors<VacuumToPP>> ff = FormFactorFactory<VacuumToPP>::create("0->Kpi::KSvD2025", p, Options{ });
+
+                    TEST_CHECK(nullptr != ff);
+                }
+
+                /* z mapping */
+                {
+                    KSvD2025FormFactors<VacuumToKPi> ff(p, Options{ });
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.z(-2.0)),                      0.133502508,   eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.z(-2.0)),                      0.0,           eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.z(-1.0)),                      0.0,           eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.z(-1.0)),                      0.0,           eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.z( 0.0)),                     -0.300926358,   eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.z( 0.0)),                      0.0,           eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.z(+0.1)),                     -0.363775160,   eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.z(+0.1)),                     -0.0,           eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.z(+0.5)),                     -0.874666169,   eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.z(+0.5)),                     -0.484725791,   eps);
+                }
+
+                /* f_+ at timelike q2 > 0.0 */
+                {
+                    KSvD2025FormFactors<VacuumToKPi> ff(p, Options{ });
+
+                    const auto chi_1m = 3.446e-3;
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitilde_p(ff.z( 0.0), chi_1m)),  0.0462214083,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitilde_p(ff.z( 0.0), chi_1m)),  0.0,           eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitilde_p(ff.z(+0.1), chi_1m)),  0.0401071876,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitilde_p(ff.z(+0.1), chi_1m)),  0.0,           eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitilde_p(ff.z(+0.5), chi_1m)),  0.00904454996, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitilde_p(ff.z(+0.5), chi_1m)), -0.01029258129, eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitildeprime_p(ff.z( 0.0), chi_1m)),  0.106199235,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitildeprime_p(ff.z( 0.0), chi_1m)),  0.0,          eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitildeprime_p(ff.z(+0.1), chi_1m)),  0.0889911105, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitildeprime_p(ff.z(+0.1), chi_1m)),  0.0,          eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitildeprime_p(ff.z(+0.5), chi_1m)),  0.0125456956, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitildeprime_p(ff.z(+0.5), chi_1m)), -0.0196693995, eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.resonance_product_p(ff.z( 0.0))),  1.04722073,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.resonance_product_p(ff.z( 0.0))),  0.0,         eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.resonance_product_p(ff.z( 0.1))),  1.05179947,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.resonance_product_p(ff.z( 0.1))),  0.0,         eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.resonance_product_p(ff.z( 0.5))),  0.493194311, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.resonance_product_p(ff.z( 0.5))), -0.684494289, eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.resonance_productprime_p(ff.z( 0.0))), -0.185712601,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.resonance_productprime_p(ff.z( 0.0))),  0.0,          eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.resonance_productprime_p(ff.z( 0.1))),  0.0419145052, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.resonance_productprime_p(ff.z( 0.1))),  0.0,          eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.resonance_productprime_p(ff.z( 0.5))),  1.95390159,   eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.resonance_productprime_p(ff.z( 0.5))), -1.30168386,   eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(ff.b0_fp(), -0.121144607, eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.f_p(0.0)), -3.33011297,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.f_p(0.0)),  0.0,         eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.f_p(0.1)), -3.97007786,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.f_p(0.1)),  0.0,         eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.f_p(0.5)), -11.28826275, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.f_p(0.5)),  0.10003175,  eps);
+                }
+
+                /* f_0 at timelike q2 > 0.0 */
+                {
+                    KSvD2025FormFactors<VacuumToKPi> ff(p, Options{ {"n-resonances-0p", "1"} });
+
+                    const auto chi_0p = 6e-5;
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitilde_z(ff.z( 0.0), chi_0p)),  0.274342612,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitilde_z(ff.z( 0.0), chi_0p)),  0.0,          eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitilde_z(ff.z(+0.1), chi_0p)),  0.248687978,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitilde_z(ff.z(+0.1), chi_0p)),  0.0,          eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitilde_z(ff.z(+0.5), chi_0p)),  0.1074383665, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitilde_z(ff.z(+0.5), chi_0p)), -0.0543100988, eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitildeprime_z(ff.z( 0.0), chi_0p)),  0.440640206,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitildeprime_z(ff.z( 0.0), chi_0p)),  0.0,          eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitildeprime_z(ff.z(+0.1), chi_0p)),  0.377814489,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitildeprime_z(ff.z(+0.1), chi_0p)),  0.0,          eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.phitildeprime_z(ff.z(+0.5), chi_0p)),  0.0783363955, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.phitildeprime_z(ff.z(+0.5), chi_0p)), -0.0917596853, eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.resonance_product_z(ff.z( 0.0))),  0.547815118, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.resonance_product_z(ff.z( 0.0))),  0.0,         eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.resonance_product_z(ff.z( 0.1))),  0.587349411, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.resonance_product_z(ff.z( 0.1))),  0.0,         eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.resonance_product_z(ff.z( 0.5))),  1.004483094, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.resonance_product_z(ff.z( 0.5))),  0.556192495, eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(ff.b0_f0(), -1.64910772, eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.f_0(0.0)), -3.33011297, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.f_0(0.0)),   0.0,       eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.f_0(0.1)), -3.94663863, eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.f_0(0.1)),   0.0,       eps);
+                    TEST_CHECK_NEARLY_EQUAL(real(ff.f_0(0.5)), -8.9506655,  eps);
+                    TEST_CHECK_NEARLY_EQUAL(imag(ff.f_0(0.5)),-13.4622631,  eps);
+                }
+            }
+        }
+} parametric_KSvD2025_test;
