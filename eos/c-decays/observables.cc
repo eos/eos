88d39be168632e0eadfda2affe7fd8f08b36ec81
Méril Reboud <merilreboud@gmail.com>
2025-03-18 14:09:36 +0100

    [c-decays] Simplify options in D -> P l nu decays

--- eos/c-decays/observables.cc
+++ eos/c-decays/observables.cc
@@ -83,31 +83,31 @@ namespace eos
                         Unit::InverseGeV2(),
                         &DToPseudoscalarLeptonNeutrino::differential_branching_ratio,
                         std::make_tuple("q2"),
-                        Options{ { "Q"_ok, "s" }, { "I"_ok, "1/2" } }),
+                        Options{ { "P"_ok, "K" } }),
 
                 make_observable("D->Klnu::BR", R"(\mathcal{B}(D\to K\ell^+ \nu))",
                         Unit::None(),
                         &DToPseudoscalarLeptonNeutrino::integrated_branching_ratio,
                         std::make_tuple("q2_min", "q2_max"),
-                        Options{ { "Q"_ok, "s" }, { "I"_ok, "1/2" } }),
+                        Options{ { "P"_ok, "K" } }),
 
                 make_observable("D->Klnu::width", R"(\Gamma(D\to K\ell^+ \nu))",
                         Unit::None(),
                         &DToPseudoscalarLeptonNeutrino::normalized_integrated_decay_width,
                         std::make_tuple("q2_min", "q2_max"),
-                        Options{ { "Q"_ok, "s" }, { "I"_ok, "1/2" } }),
+                        Options{ { "P"_ok, "K" } }),
 
                 make_observable("D->Klnu::P(q2_min,q2_max)", R"(P(D\to K\ell^+ \nu))",
                         Unit::None(),
                         &DToPseudoscalarLeptonNeutrino::integrated_pdf_q2,
                         std::make_tuple("q2_min", "q2_max"),
-                        Options{ { "Q"_ok, "s" }, { "I"_ok, "1/2" } }),
+                        Options{ { "P"_ok, "K" } }),
 
                 make_observable("D->Klnu::P(q2)", R"(dP(D\to K\ell^+ \nu)/dq^2)",
                         Unit::InverseGeV2(),
                         &DToPseudoscalarLeptonNeutrino::differential_pdf_q2,
                         std::make_tuple("q2"),
-                        Options{ { "Q"_ok, "s" }, { "I"_ok, "1/2" } }),
+                        Options{ { "P"_ok, "K" } }),
             }
         );
 
