290aa8a903d00627578a13e62c0ad6ab9737dd51
Matthew Kirk <matthew.j.kirk@durham.ac.uk>
2025-03-24 11:36:27 +0000

    [eos] Add a create-mask task
    Allows the creation of a mask on a set of ImportanceSamples, based upon
    the sign of one or more observable(s), and when using multiple observables
    the individual masks can be combined using either 'and' or 'or' logic.
    Masks can be used in the find-mode, corner-plot, and predict-observables
    tasks after creation.

--- python/eos/data/native.py
+++ python/eos/data/native.py
@@ -697,3 +697,52 @@ class NabuLikelihood:
             yaml.dump(description, description_file, default_flow_style=False)
 
         likelihood.save(os.path.join(path, 'likelihood.nabu'))
+
+
+class SampleMask:
+    def __init__(self, path):
+        """ Read a mask from a file.
+
+        :param path: Path to the storage location.
+        :type path: str
+        """
+        if not os.path.exists(path) or not os.path.isdir(path):
+            raise RuntimeError(f'Path {path} does not exist or is not a directory')
+
+        f = os.path.join(path, 'description.yaml')
+        if not os.path.exists(f) or not os.path.isfile(f):
+            raise RuntimeError(f'Description file {f} does not exist or is not a file')
+
+        with open(f) as df:
+            description = yaml.load(df, Loader=yaml.SafeLoader)
+
+        if not description['type'] == 'Mask':
+            raise RuntimeError(f'Path {path} not pointing to a Mode file')
+
+        self.type = 'Mask'
+        self.observables = description['observables']
+
+        f = os.path.join(path, 'mask.npy')
+        if not os.path.exists(f) or not os.path.isfile(f):
+            raise RuntimeError(f'Weights file {f} does not exist or is not a file')
+        self.mask = _np.load(f)
+
+    @staticmethod
+    def create(path, mask, observables):
+        """ Write a new Mask object to disk.
+
+        :param path: Path to the storage location, which will be created as a directory.
+        :type path: str
+        :param mask: Mask as a 1D boolean array
+        :type mask: list or iterable of bool
+        :param observables: Observables as a 1D array
+        """
+        description = {}
+        description['version'] = eos.__version__
+        description['type'] = 'Mask'
+        description['observables'] = observables
+
+        os.makedirs(path, exist_ok=True)
+        with open(os.path.join(path, 'description.yaml'), 'w') as description_file:
+            yaml.dump(description, description_file, default_flow_style=False)
+        _np.save(os.path.join(path, 'mask.npy'), mask)
