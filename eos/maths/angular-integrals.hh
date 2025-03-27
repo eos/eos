03c3d0bb9bcc4461459106ee875f783305e50493
Florian Herren <florian.s.herren@gmail.com>
2025-03-27 14:06:05 +0100

    [maths] Add integrals over one, two or three associated Legendre Polynomials

---
+++ eos/maths/angular-integrals.hh
@@ -0,0 +1,31 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2025 Florian Herren
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef EOS_GUARD_EOS_MATHS_ANGULAR_INTEGRALS_HH
+#define EOS_GUARD_EOS_MATHS_ANGULAR_INTEGRALS_HH 1
+
+namespace eos
+{
+    double wigner_3j(const int & j1, const int & m1, const int & j2, const int & m2, const int & j3, const int & m3);
+    double legendre_integral(const int & l, const int & m);
+    double two_legendre_integral(const int & l1, const int & m1, const int & l2, const int & m2);
+    double three_legendre_integral(const int & l1, const int & m1, const int & l2, const int & m2, const int & l3, const int & m3);
+}
+
+#endif
