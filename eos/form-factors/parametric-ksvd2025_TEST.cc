a1382ffe7205492cc1d6fe66dd2b2f7c13bcc601
Matthew Kirk <matthew.j.kirk@durham.ac.uk>
2025-10-29 16:23:42 +0000

    [form-factors] Add 0->Kpi form factor observables
    f_+(q2=0) is useful for comparison to lattice, and the lambda parameters are
    useful for comparison to experiment.

--- eos/form-factors/parametric-ksvd2025_TEST.cc
+++ eos/form-factors/parametric-ksvd2025_TEST.cc
@@ -129,6 +129,10 @@ class ParametricKSvD2025Test :
                     TEST_CHECK_NEARLY_EQUAL(imag(ff.f_p(0.1)),  0.0,         eps);
                     TEST_CHECK_NEARLY_EQUAL(real(ff.f_p(0.5)), -11.28826275, eps);
                     TEST_CHECK_NEARLY_EQUAL(imag(ff.f_p(0.5)),  0.10003175,  eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(ff.fp_at_0(), -3.33011297, eps);
+                    TEST_CHECK_NEARLY_EQUAL(ff.lambda_plus(), 0.03238868, eps);
+
                 }
 
                 /* f_0 at timelike q2 > 0.0 */
@@ -166,6 +170,8 @@ class ParametricKSvD2025Test :
                     TEST_CHECK_NEARLY_EQUAL(imag(ff.f_0(0.1)),   0.0,       eps);
                     TEST_CHECK_NEARLY_EQUAL(real(ff.f_0(0.5)), -8.9575287,  eps);
                     TEST_CHECK_NEARLY_EQUAL(imag(ff.f_0(0.5)),-13.7266102,  eps);
+
+                    TEST_CHECK_NEARLY_EQUAL(ff.lambda_zero(), 0.03024065, eps);
                 }
             }
         }
