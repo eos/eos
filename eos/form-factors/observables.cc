cb5ab998a1f5f87fa12230af52ab296aa453615b
Matthew Kirk <matthew.j.kirk@durham.ac.uk>
2025-11-26 10:48:56 +0000

    [form-factors] Fix latex of lambda''_0 observable
    Can't have two superscripts.
    The other changes are "internal", just making the latex look the same so I
    could easily compare single and doubleprime cases.
    
    Fixes the first point in issue #1116

--- eos/form-factors/observables.cc
+++ eos/form-factors/observables.cc
@@ -2889,10 +2889,10 @@ namespace eos
                 make_observable("0->Kpi::f_+(0)", R"(f_+^{0 \to K\pi}(0))", Unit::None(),
                         &KSvD2025FormFactors<VacuumToKPi>::fp_at_0),
 
-                make_observable("0->Kpi::lambda_prime_+", R"(\lambda_+^{('), K \to \pi})", Unit::None(),
+                make_observable("0->Kpi::lambda_prime_+", R"(\lambda_+^{(\prime), K \to \pi})", Unit::None(),
                         &KSvD2025FormFactors<VacuumToKPi>::lambda_prime_plus),
 
-                make_observable("0->Kpi::lambda_doubleprime_+", R"(\lambda^{\prime\prime, 0 \to K \pi}_+)", Unit::None(),
+                make_observable("0->Kpi::lambda_doubleprime_+", R"(\lambda_+^{\prime\prime, K \to \pi})", Unit::None(),
                         &KSvD2025FormFactors<VacuumToKPi>::lambda_doubleprime_plus),
 
                 make_observable("0->Kpi::b0_f+@KSvD2025", R"(b_0^{+, 0 \to K\pi})", Unit::None(),
@@ -2913,10 +2913,10 @@ namespace eos
                 make_form_factor_adapter("0->Kpi::Im{f_0}(Re{q2},Im{q2})", R"(\textrm{Im}(f_0^{0\to K\pi}(q^2)))",
                         &FormFactors<VacuumToPP>::im_f_0, std::make_tuple("Re{q2}", "Im{q2}")),
 
-                make_observable("0->Kpi::lambda_prime_0", R"(\lambda_0^{('), K \to \pi})", Unit::None(),
+                make_observable("0->Kpi::lambda_prime_0", R"(\lambda_0^{(\prime), K \to \pi})", Unit::None(),
                         &KSvD2025FormFactors<VacuumToKPi>::lambda_prime_zero),
 
-                make_observable("0->Kpi::lambda_doubleprime_0", R"(\lambda^{\prime\prime}_0^{K \to \pi})", Unit::None(),
+                make_observable("0->Kpi::lambda_doubleprime_0", R"(\lambda_0^{\prime\prime, K \to \pi})", Unit::None(),
                         &KSvD2025FormFactors<VacuumToKPi>::lambda_doubleprime_zero),
 
                 make_observable("0->Kpi::b0_f0@KSvD2025", R"(b_0^{0, 0 \to K\pi})", Unit::None(),
