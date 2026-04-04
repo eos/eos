c1075076e5ce60be7702f00d1bb3e8590e72252e
Dominik Suelmann <dominik.suelmann@tu-dortmund.de>
2026-04-04 16:10:43 +0200

    [rare-c-decays] Add calculation of Lambda_c->pll decays

---
+++ eos/rare-c-decays/lambda-c-to-proton-l-l.hh
@@ -0,0 +1,78 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2019 Ahmet Kokulu
+ * Copyright (c) 2019 Danny van Dyk
+ * Copyright (c) 2023 Méril Reboud
+ * Copyright (c) 2026 Dominik Suelmann
+ *
+ * This file is part of the EOS project. EOS is free software;
+ * you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2, as published by the Free Software Foundation.
+ *
+ * EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef EOS_GUARD_EOS_RARE_C_DECAYS_LAMBDA_C_TO_PROTON_LEPTON_LEPTON_HH
+#define EOS_GUARD_EOS_RARE_C_DECAYS_LAMBDA_C_TO_PROTON_LEPTON_LEPTON_HH 1
+
+#include <eos/utils/options.hh>
+#include <eos/utils/parameters.hh>
+#include <eos/utils/private_implementation_pattern.hh>
+#include <eos/utils/reference-name.hh>
+
+namespace eos
+{
+    /*
+     * Decay: Lambda_c -> p l l
+     */
+    class LambdaCToProtonLeptonLepton : public ParameterUser, public PrivateImplementationPattern<LambdaCToProtonLeptonLepton>
+    {
+        public:
+            LambdaCToProtonLeptonLepton(const Parameters &, const Options &);
+            ~LambdaCToProtonLeptonLepton();
+
+            double double_differential_decay_width(const double & q2, const double & c_lep) const;
+            double integrated_decay_width(const double & q2_min, const double & q2_max) const;
+
+            double differential_branching_ratio(const double & q2) const;
+            double differential_a_fb_leptonic(const double & q2) const;
+            double differential_f_l(const double & q2) const;
+
+            double integrated_branching_ratio(const double & q2_min, const double & q2_max) const;
+            double integrated_a_fb_leptonic(const double & q2_min, const double & q2_max) const;
+            double integrated_a_fb_leptonic_num(const double & q2_min, const double & q2_max) const;
+            double integrated_f_l(const double & q2_min, const double & q2_max) const;
+            double integrated_f_l_num(const double & q2_min, const double & q2_max) const;
+            double integrated_k1ss(const double & q2_min, const double & q2_max) const;
+            double integrated_k1cc(const double & q2_min, const double & q2_max) const;
+            double integrated_k1c(const double & q2_min, const double & q2_max) const;
+
+            /*!
+             * Descriptions of the process and its kinematics.
+             */
+            static const std::string description;
+            static const std::string kinematics_description_q2;
+            static const std::string kinematics_description_c_theta_l;
+
+            /*!
+             * References used in the computation of our observables.
+             */
+            static const std::set<ReferenceName> references;
+
+            /*!
+             * Options used in the computation of our observables.
+             */
+            static std::vector<OptionSpecification>::const_iterator begin_options();
+            static std::vector<OptionSpecification>::const_iterator end_options();
+    };
+} // namespace eos
+
+#endif
