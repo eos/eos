9197876fb41e411925ff5429949615296090f076
Danny van Dyk <danny.van.dyk@gmail.com>
2026-06-06 14:01:30 +0100

    [b-decays] Replace ``s`` with ``q2`` throughout the Lambda_b->Lambda_c(2595,2625)lnu codes

--- eos/b-decays/lambdab-to-lambdac2595-l-nu.cc
+++ eos/b-decays/lambdab-to-lambdac2595-l-nu.cc
@@ -2,7 +2,7 @@
 
 /*
  * Copyright (c) 2017      Elena Graverini
- * Copyright (c) 2017-2025 Danny van Dyk
+ * Copyright (c) 2017-2026 Danny van Dyk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -72,127 +72,127 @@ namespace eos
             u.uses(*model);
         }
 
-        double F12T(const double & s) const { return form_factors->f_time_v(s); };
-        double F120(const double & s) const { return form_factors->f_long_v(s); };
-        double F12P(const double & s) const { return form_factors->f_perp_v(s); };
-        double G12T(const double & s) const { return form_factors->f_time_a(s); };
-        double G120(const double & s) const { return form_factors->f_long_a(s); };
-        double G12P(const double & s) const { return form_factors->f_perp_a(s); };
-        double s_plus(const double & s) const { return power_of<2>(m_LambdaB + m_LambdaC2595) - s; };
-        double s_minus(const double & s) const { return power_of<2>(m_LambdaB - m_LambdaC2595) - s; };
+        double F12T(const double & q2) const { return form_factors->f_time_v(q2); };
+        double F120(const double & q2) const { return form_factors->f_long_v(q2); };
+        double F12P(const double & q2) const { return form_factors->f_perp_v(q2); };
+        double G12T(const double & q2) const { return form_factors->f_time_a(q2); };
+        double G120(const double & q2) const { return form_factors->f_long_a(q2); };
+        double G12P(const double & q2) const { return form_factors->f_perp_a(q2); };
+        double s_plus(const double & q2) const { return power_of<2>(m_LambdaB + m_LambdaC2595) - q2; };
+        double s_minus(const double & q2) const { return power_of<2>(m_LambdaB - m_LambdaC2595) - q2; };
 
         // [BBGIOvD] parametrization for the differential decay width
-        double a_l(const double & s) const
+        double a_l(const double & q2) const
         {
-            double val = power_of<2>(F12T(s)) * (power_of<2>(m_l) / s) * power_of<2>(m_LambdaB - m_LambdaC2595);
-            val += power_of<2>(F120(s)) * power_of<2>(m_LambdaB + m_LambdaC2595) + power_of<2>(F12P(s)) * (power_of<2>(m_l) + s);
-            val += power_of<2>(G12T(s)) * power_of<2>(m_l) / s * power_of<2>(m_LambdaB + m_LambdaC2595);
-            val += power_of<2>(G120(s)) * power_of<2>(m_LambdaB - m_LambdaC2595) + power_of<2>(G12P(s)) * (power_of<2>(m_l) + s);
+            double val = power_of<2>(F12T(q2)) * (power_of<2>(m_l) / q2) * power_of<2>(m_LambdaB - m_LambdaC2595);
+            val += power_of<2>(F120(q2)) * power_of<2>(m_LambdaB + m_LambdaC2595) + power_of<2>(F12P(q2)) * (power_of<2>(m_l) + q2);
+            val += power_of<2>(G12T(q2)) * power_of<2>(m_l) / q2 * power_of<2>(m_LambdaB + m_LambdaC2595);
+            val += power_of<2>(G120(q2)) * power_of<2>(m_LambdaB - m_LambdaC2595) + power_of<2>(G12P(q2)) * (power_of<2>(m_l) + q2);
             return val / 2.0;
         }
 
-        double b_l(const double & s) const
+        double b_l(const double & q2) const
         {
-            double val = 2. * (F12T(s) * F120(s) + G12T(s) * G120(s)) * power_of<2>(m_l) / s;
+            double val = 2. * (F12T(q2) * F120(q2) + G12T(q2) * G120(q2)) * power_of<2>(m_l) / q2;
             val *= (power_of<2>(m_LambdaB) - power_of<2>(m_LambdaC2595));
-            val += (-4.) * s * (F12P(s) * G12P(s));
+            val += (-4.) * q2 * (F12P(q2) * G12P(q2));
             return val / 2.0;
         }
 
-        double c_l(const double & s) const
+        double c_l(const double & q2) const
         {
-            double val = power_of<2>(F120(s)) * power_of<2>(m_LambdaB + m_LambdaC2595);
-            val += (-1.) * s * power_of<2>(F12P(s));
-            val += power_of<2>(G120(s)) * power_of<2>(m_LambdaB - m_LambdaC2595);
-            val += (-1.) * s * power_of<2>(G12P(s));
-            val *= (-1.) * (1. - power_of<2>(m_l) / s);
+            double val = power_of<2>(F120(q2)) * power_of<2>(m_LambdaB + m_LambdaC2595);
+            val += (-1.) * q2 * power_of<2>(F12P(q2));
+            val += power_of<2>(G120(q2)) * power_of<2>(m_LambdaB - m_LambdaC2595);
+            val += (-1.) * q2 * power_of<2>(G12P(q2));
+            val *= (-1.) * (1. - power_of<2>(m_l) / q2);
             return val / 2.0;
         }
 
-        double gamma_0(const double & s) const
+        double gamma_0(const double & q2) const
         {
-            double val = power_of<2>(g_fermi) * sqrt(s_plus(s) * s_minus(s));
+            double val = power_of<2>(g_fermi) * sqrt(s_plus(q2) * s_minus(q2));
             val *= m_LambdaB * m_LambdaC2595;
             val *= (1. / (96. * power_of<3>(M_PI * m_LambdaB)));
-            val *= power_of<2>(1. - power_of<2>(m_l) / s);
+            val *= power_of<2>(1. - power_of<2>(m_l) / q2);
             return val;
         }
 
-        inline double lambda(const double & s) const
+        inline double lambda(const double & q2) const
         {
-            return s_plus(s) * s_minus(s);
+            return s_plus(q2) * s_minus(q2);
         }
 
         // normalized to V_cb = 1
-        double normalized_differential_decay_width(const double & s) const
+        double normalized_differential_decay_width(const double & q2) const
         {
-            if ((s < m_l * m_l) || (lambda(s) < 0.0))
+            if ((q2 < m_l * m_l) || (lambda(q2) < 0.0))
             {
                 return 0.0;
             }
 
-            return 2. * gamma_0(s) * (a_l(s) + c_l(s) / 3.);
+            return 2. * gamma_0(q2) * (a_l(q2) + c_l(q2) / 3.);
         }
 
-        double normalized_differential_forward_backward_asymmetry(const double & s) const
+        double normalized_differential_forward_backward_asymmetry(const double & q2) const
         {
-            if ((s < m_l * m_l) || (lambda(s) < 0.0))
+            if ((q2 < m_l * m_l) || (lambda(q2) < 0.0))
             {
                 return 0.0;
             }
 
             // in order to obtain the q^2-integrated A_FB later on, we require
             // this to be normalized to Gamma_0.
-            return gamma_0(s) * b_l(s);
+            return gamma_0(q2) * b_l(q2);
         }
 
-        double normalized_double_differential_decay_width(const double & s, const double & cos_theta_l) const
+        double normalized_double_differential_decay_width(const double & q2, const double & cos_theta_l) const
         {
-            if ((s < m_l * m_l) || (lambda(s) < 0.0))
+            if ((q2 < m_l * m_l) || (lambda(q2) < 0.0))
             {
                 return 0.0;
             }
 
-            return gamma_0(s) * (a_l(s) + b_l(s) * cos_theta_l + c_l(s) * power_of<2>(cos_theta_l));
+            return gamma_0(q2) * (a_l(q2) + b_l(q2) * cos_theta_l + c_l(q2) * power_of<2>(cos_theta_l));
         }
 
-        double differential_decay_width(const double & s) const
+        double differential_decay_width(const double & q2) const
         {
-            return normalized_differential_decay_width(s) * std::norm(model->ckm_cb());
+            return normalized_differential_decay_width(q2) * std::norm(model->ckm_cb());
         }
 
-        double double_differential_decay_width(const double & s, const double & cos_theta_l) const
+        double double_differential_decay_width(const double & q2, const double & cos_theta_l) const
         {
-            return normalized_double_differential_decay_width(s, cos_theta_l) * std::norm(model->ckm_cb());
+            return normalized_double_differential_decay_width(q2, cos_theta_l) * std::norm(model->ckm_cb());
         }
 
-        double differential_branching_ratio(const double & s) const
+        double differential_branching_ratio(const double & q2) const
         {
-            return differential_decay_width(s) * tau_LambdaB / hbar;
+            return differential_decay_width(q2) * tau_LambdaB / hbar;
         }
 
-        double double_differential_branching_ratio(const double & s, const double & cos_theta_l) const
+        double double_differential_branching_ratio(const double & q2, const double & cos_theta_l) const
         {
-            return double_differential_decay_width(s, cos_theta_l) * tau_LambdaB / hbar;
+            return double_differential_decay_width(q2, cos_theta_l) * tau_LambdaB / hbar;
         }
 
-        double integrated_branching_ratio(const double & s_min, const double & s_max) const
+        double integrated_branching_ratio(const double & q2_min, const double & q2_max) const
         {
             std::function<double (const double &)> f = std::bind(&Implementation<LambdaBToLambdaC2595LeptonNeutrino>::differential_branching_ratio,
                     *this, std::placeholders::_1);
 
-            return integrate<GSL::QAGS>(f, s_min, s_max);
+            return integrate<GSL::QAGS>(f, q2_min, q2_max);
         }
 
-        double integrated_forward_backward_asymmetry(const double & s_min, const double & s_max) const
+        double integrated_forward_backward_asymmetry(const double & q2_min, const double & q2_max) const
         {
             std::function<double (const double &)> numerator   = std::bind(&Implementation<LambdaBToLambdaC2595LeptonNeutrino>::normalized_differential_forward_backward_asymmetry,
                     *this, std::placeholders::_1);
             std::function<double (const double &)> denominator = std::bind(&Implementation<LambdaBToLambdaC2595LeptonNeutrino>::normalized_differential_decay_width,
                     *this, std::placeholders::_1);
 
-            const double inum   = integrate<GSL::QAGS>(numerator,   s_min, s_max);
-            const double idenom = integrate<GSL::QAGS>(denominator, s_min, s_max);
+            const double inum   = integrate<GSL::QAGS>(numerator,   q2_min, q2_max);
+            const double idenom = integrate<GSL::QAGS>(denominator, q2_min, q2_max);
 
             return inum / idenom;
         }
@@ -216,54 +216,54 @@ namespace eos
     }
 
     double
-    LambdaBToLambdaC2595LeptonNeutrino::a_l(const double & s) const
+    LambdaBToLambdaC2595LeptonNeutrino::a_l(const double & q2) const
     {
-        return _imp->a_l(s);
+        return _imp->a_l(q2);
     }
 
     double
-    LambdaBToLambdaC2595LeptonNeutrino::b_l(const double & s) const
+    LambdaBToLambdaC2595LeptonNeutrino::b_l(const double & q2) const
     {
-        return _imp->b_l(s);
+        return _imp->b_l(q2);
     }
 
     double
-    LambdaBToLambdaC2595LeptonNeutrino::c_l(const double & s) const
+    LambdaBToLambdaC2595LeptonNeutrino::c_l(const double & q2) const
     {
-        return _imp->c_l(s);
+        return _imp->c_l(q2);
     }
 
     double
-    LambdaBToLambdaC2595LeptonNeutrino::differential_branching_ratio(const double & s) const
+    LambdaBToLambdaC2595LeptonNeutrino::differential_branching_ratio(const double & q2) const
     {
-        return _imp->differential_branching_ratio(s);
+        return _imp->differential_branching_ratio(q2);
     }
 
     double
-    LambdaBToLambdaC2595LeptonNeutrino::double_differential_branching_ratio(const double & s, const double & cos_theta_l) const
+    LambdaBToLambdaC2595LeptonNeutrino::double_differential_branching_ratio(const double & q2, const double & cos_theta_l) const
     {
-        return _imp->double_differential_branching_ratio(s, cos_theta_l);
+        return _imp->double_differential_branching_ratio(q2, cos_theta_l);
     }
 
     double
-    LambdaBToLambdaC2595LeptonNeutrino::integrated_branching_ratio(const double & s_min, const double & s_max) const
+    LambdaBToLambdaC2595LeptonNeutrino::integrated_branching_ratio(const double & q2_min, const double & q2_max) const
     {
-        return _imp->integrated_branching_ratio(s_min, s_max);
+        return _imp->integrated_branching_ratio(q2_min, q2_max);
     }
 
     double
-    LambdaBToLambdaC2595LeptonNeutrino::integrated_forward_backward_asymmetry(const double & s_min, const double & s_max) const
+    LambdaBToLambdaC2595LeptonNeutrino::integrated_forward_backward_asymmetry(const double & q2_min, const double & q2_max) const
     {
-        return _imp->integrated_forward_backward_asymmetry(s_min, s_max);
+        return _imp->integrated_forward_backward_asymmetry(q2_min, q2_max);
     }
 
     double
-    LambdaBToLambdaC2595LeptonNeutrino::normalized_integrated_branching_ratio(const double & s_min, const double & s_max) const
+    LambdaBToLambdaC2595LeptonNeutrino::normalized_integrated_branching_ratio(const double & q2_min, const double & q2_max) const
     {
-        const double abs_s_min = power_of<2>(_imp->m_l);
-        const double abs_s_max = power_of<2>(_imp->m_LambdaB - _imp->m_LambdaC2595);
+        const double abs_q2_min = power_of<2>(_imp->m_l);
+        const double abs_q2_max = power_of<2>(_imp->m_LambdaB - _imp->m_LambdaC2595);
 
-        return _imp->integrated_branching_ratio(s_min, s_max) / _imp->integrated_branching_ratio(abs_s_min, abs_s_max);
+        return _imp->integrated_branching_ratio(q2_min, q2_max) / _imp->integrated_branching_ratio(abs_q2_min, abs_q2_max);
     }
 
     const std::string
@@ -271,7 +271,7 @@ namespace eos
 The decay Lambda_b -> Lambda_c(2595) l nu, where l=e,mu,tau is a lepton.";
 
     const std::string
-    LambdaBToLambdaC2595LeptonNeutrino::kinematics_description_s = "\
+    LambdaBToLambdaC2595LeptonNeutrino::kinematics_description_q2 = "\
 The invariant mass of the l-nubar pair in GeV^2.";
 
     const std::string
