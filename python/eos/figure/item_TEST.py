2bdbe00ee4f0fd5b856a7462cc54cac446049190
Danny van Dyk <danny.van.dyk@gmail.com>
2026-06-14 09:01:02 +0100

    [python] Add test case for ``eos.figure.item.ComplexPlaneItem``

--- python/eos/figure/item_TEST.py
+++ python/eos/figure/item_TEST.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2025-2026 Danny van Dyk
+#
+# This file is part of the EOS project. EOS is free software;
+# you can redistribute it and/or modify it under the terms of the GNU General
+# Public License version 2, as published by the Free Software Foundation.
+#
+# EOS is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+
 import unittest
 
 import eos
@@ -237,7 +252,24 @@ class SignalPDFItemTests(unittest.TestCase):
         except Exception as e:
             self.fail(f"Error when testing item of type 'signal-pdf': {e}")
 
-# class ComplexPlaneItemTests(unittest.TestCase):
+class ComplexPlaneItemTests(unittest.TestCase):
+
+    def test_full(self):
+
+        try:
+            input = """
+            type: 'complex-plane'
+            observable: 'b->s::Re{F17}(Re{q2},Im{q2})'
+            variables: ['Re{q2}', 'Im{q2}']
+            ranges: [[-1.0, +1.0], [-1.0, +1.0]]
+            resolution: 10
+            """
+            item = eos.figure.ItemFactory.from_yaml(input)
+            item.prepare()
+            _, ax = plt.subplots()
+            item.draw(ax)
+        except Exception as e:
+            self.fail(f"Error when testing item of type 'complex-plane': {e}")
 
 class ErrorBarsItemTests(unittest.TestCase):
 
