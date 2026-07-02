c23fdecf58977a7c335343600483101a629e7edb
Danny van Dyk <danny.van.dyk@gmail.com>
2026-07-02 20:20:15 +0100

    [python] Convert ``eos.data.MarkovChain`` to ``eos.Serializable``/``eos.Deserializable``

--- python/eos/data/markov_chain.py
+++ python/eos/data/markov_chain.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2023 Danny van Dyk
+# Copyright (c) 2020-2026 Danny van Dyk
 # Copyright (c) 2023 Stephan Kürten
 #
 # This file is part of the EOS project. EOS is free software;
@@ -14,10 +14,73 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
 
+from dataclasses import dataclass, field, asdict
+from eos.data._common import ParameterDescription, load_array
+from eos.deserializable import Deserializable
+from eos.serializable import Serializable
+
+import copy as _copy
 import eos
 import os
 import numpy as _np
-import yaml
+
+
+@dataclass(kw_only=True)
+class MarkovChainDescription(Serializable, Deserializable):
+    r"""Schema of the ``description.yaml`` written for a :class:`MarkovChain`.
+
+    This is the single source of truth for the metadata stored alongside a Markov chain, and it backs
+    both the read path (:meth:`from_yaml_file`) and the write path (:meth:`to_yaml_file`). The
+    ``type`` discriminator is validated on deserialization and is not part of the constructor.
+
+    :param version: The version of EOS that wrote the data object.
+    :type version: str
+    :param parameters: The descriptions of the varied parameters.
+    :type parameters: list[ParameterDescription]
+    :param has_weights: Whether the chain stores importance weights (on-disk key ``has-weights``).
+    :type has_weights: bool
+    """
+    version:str
+    parameters:list[ParameterDescription]
+    has_weights:bool = field(default=False)
+    type:str = field(init=False, default='MarkovChain')
+
+    @classmethod
+    def from_dict(cls, **kwargs):
+        """Create a :class:`MarkovChainDescription` from its on-disk keyword description.
+
+        Validates the ``type`` discriminator, normalizes the on-disk ``has-weights`` key to the
+        ``has_weights`` field, and deserializes each entry of ``parameters`` into a
+        :class:`ParameterDescription`.
+
+        :raises ValueError: If the description does not identify a Markov chain.
+        """
+        _kwargs = _copy.deepcopy(kwargs)
+
+        _type = _kwargs.pop('type', None)
+        if _type != 'MarkovChain':
+            raise ValueError(f'Expected a description of type \'MarkovChain\', got \'{_type}\'')
+
+        if 'has-weights' in _kwargs:
+            _kwargs['has_weights'] = _kwargs.pop('has-weights')
+
+        if 'parameters' in _kwargs:
+            _kwargs['parameters'] = [ParameterDescription.from_dict(**p) for p in _kwargs['parameters']]
+
+        return Deserializable.make(cls, **_kwargs)
+
+    def to_dict(self):
+        """Serialize this description into the on-disk mapping written to ``description.yaml``.
+
+        Emits the on-disk ``has-weights`` key and the ``type`` discriminator, inverting
+        :meth:`from_dict`.
+        """
+        return {
+            'version':     self.version,
+            'type':        self.type,
+            'parameters':  [asdict(p) for p in self.parameters],
+            'has-weights': self.has_weights,
+        }
 
 
 class MarkovChain:
@@ -45,35 +108,18 @@ class MarkovChain:
         if not os.path.exists(path) or not os.path.isdir(path):
             raise RuntimeError(f'Path {path} does not exist or is not a directory')
 
-        f = os.path.join(path, 'description.yaml')
-        if not os.path.exists(f) or not os.path.isfile(f):
-            raise RuntimeError(f'Description file {f} does not exist or is not a file')
+        description = MarkovChainDescription.from_yaml_file(os.path.join(path, 'description.yaml'))
 
-        with open(f) as df:
-            description = yaml.load(df, Loader=yaml.SafeLoader)
+        self.type = description.type
+        self.varied_parameters = [asdict(p) for p in description.parameters]
+        self.lookup_table = { p.name: idx for idx, p in enumerate(description.parameters) }
 
-        if not description['type'] == 'MarkovChain':
-            raise RuntimeError(f'Path {path} not pointing to a MarkovChain')
+        ncols = len(description.parameters)
+        self.samples  = load_array(path, 'samples.npy',  ncols=ncols)
+        self.usamples = load_array(path, 'usamples.npy', ncols=ncols)
 
-        self.type = 'MarkovChain'
-        self.varied_parameters = description['parameters']
-        self.lookup_table = { item['name']: idx for idx, item in enumerate(self.varied_parameters) }
-
-        f = os.path.join(path, 'samples.npy')
-        if not os.path.exists(f) or not os.path.isfile(f):
-            raise RuntimeError(f'Samples file {f} does not exist or is not a file')
-        self.samples = _np.load(f)
-
-        f = os.path.join(path, 'usamples.npy')
-        if not os.path.exists(f) or not os.path.isfile(f):
-            raise RuntimeError(f'U-space samples file {f} does not exist or is not a file')
-        self.usamples = _np.load(f)
-
-        if description['has-weights']:
-            f = os.path.join(path, 'weights.npy')
-            if not os.path.exists(f) or not os.path.isfile(f):
-                raise RuntimeError(f'Weights file {f} does not exist or is not a file')
-            self.weights = _np.load(f)
+        if description.has_weights:
+            self.weights = load_array(path, 'weights.npy', nrows=self.samples.shape[0])
         else:
             self.weights = None
 
@@ -93,16 +139,6 @@ class MarkovChain:
         :param weights: Weights on a linear scale as a 1D array of shape (N, ).
         :type weights: 1D numpy array, optional
         """
-        description = {}
-        description['version'] = eos.__version__
-        description['type'] = 'MarkovChain'
-        description['parameters'] = [{
-            'name': p.name(),
-            'min': p.min(),
-            'max': p.max()
-        } for p in parameters]
-        description['has-weights'] = (not weights is None)
-
         if not samples.shape[1] == len(parameters):
             raise RuntimeError(f'Shape of samples {samples.shape} incompatible with number of parameters {len(parameters)}')
 
@@ -112,9 +148,14 @@ class MarkovChain:
         if not weights is None and not samples.shape[0] == weights.shape[0]:
             raise RuntimeError(f'Shape of weights {weights.shape} incompatible with shape of samples {samples.shape}')
 
+        description = MarkovChainDescription(
+            version     = eos.__version__,
+            parameters  = [ParameterDescription(name=p.name(), min=p.min(), max=p.max()) for p in parameters],
+            has_weights = weights is not None,
+        )
+
         os.makedirs(path, exist_ok=True)
-        with open(os.path.join(path, 'description.yaml'), 'w') as description_file:
-            yaml.dump(description, description_file, default_flow_style=False)
+        description.to_yaml_file(os.path.join(path, 'description.yaml'))
         _np.save(os.path.join(path, 'samples.npy'), samples)
         _np.save(os.path.join(path, 'usamples.npy'), usamples)
 
