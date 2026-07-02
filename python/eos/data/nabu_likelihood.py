6225bf922323a0e77db6e2fa63f132e339cc0f43
Danny van Dyk <danny.van.dyk@gmail.com>
2026-07-02 23:22:23 +0100

    [python] Convert ``eos.data.NabuLikelihood`` to ``eos.Serializable``/``eos.Deserializable``

--- python/eos/data/nabu_likelihood.py
+++ python/eos/data/nabu_likelihood.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2024 Danny van Dyk
+# Copyright (c) 2024-2026 Danny van Dyk
 #
 # This file is part of the EOS project. EOS is free software;
 # you can redistribute it and/or modify it under the terms of the GNU General
@@ -13,9 +13,63 @@
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
 
+from dataclasses import dataclass, field, asdict
+from eos.data._common import ParameterDescription
+from eos.deserializable import Deserializable
+from eos.serializable import Serializable
+
+import copy as _copy
 import eos
 import os
-import yaml
+
+
+@dataclass(kw_only=True)
+class NabuLikelihoodDescription(Serializable, Deserializable):
+    r"""Schema of the ``description.yaml`` written for a :class:`NabuLikelihood` object.
+
+    This is the single source of truth for the metadata stored alongside a nabu-serialized likelihood,
+    and it backs both the read path (:meth:`from_yaml_file`) and the write path (:meth:`to_yaml_file`).
+    The ``type`` discriminator is validated on deserialization and is not part of the constructor.
+
+    :param version: The version of EOS that wrote the data object.
+    :type version: str
+    :param parameters: The descriptions of the varied parameters.
+    :type parameters: list[ParameterDescription]
+    """
+    version:str
+    parameters:list[ParameterDescription]
+    type:str = field(init=False, default='NabuLikelihood')
+
+    @classmethod
+    def from_dict(cls, **kwargs):
+        """Create a :class:`NabuLikelihoodDescription` from its on-disk keyword description.
+
+        Validates the ``type`` discriminator and deserializes each entry of ``parameters`` into a
+        :class:`ParameterDescription`.
+
+        :raises ValueError: If the description does not identify a nabu-likelihood object.
+        """
+        _kwargs = _copy.deepcopy(kwargs)
+
+        _type = _kwargs.pop('type', None)
+        if _type != 'NabuLikelihood':
+            raise ValueError(f'Expected a description of type \'NabuLikelihood\', got \'{_type}\'')
+
+        if 'parameters' in _kwargs:
+            _kwargs['parameters'] = [ParameterDescription.from_dict(**p) for p in _kwargs['parameters']]
+
+        return Deserializable.make(cls, **_kwargs)
+
+    def to_dict(self):
+        """Serialize this description into the on-disk mapping written to ``description.yaml``.
+
+        Emits the ``type`` discriminator, inverting :meth:`from_dict`.
+        """
+        return {
+            'version':    self.version,
+            'type':       self.type,
+            'parameters': [asdict(p) for p in self.parameters],
+        }
 
 
 class NabuLikelihood:
@@ -38,54 +92,40 @@ class NabuLikelihood:
         :param path: Path to the storage location.
         :type path: str
         """
-        import nabu
-
         if not os.path.exists(path) or not os.path.isdir(path):
             raise RuntimeError(f'Path {path} does not exist or is not a directory')
 
-        f = os.path.join(path, 'description.yaml')
-        if not os.path.exists(f) or not os.path.isfile(f):
-            raise RuntimeError(f'Description file {f} does not exist or is not a file')
+        description = NabuLikelihoodDescription.from_yaml_file(os.path.join(path, 'description.yaml'))
 
-        with open(f) as df:
-            description = yaml.load(df, Loader=yaml.SafeLoader)
-
-        if not description['type'] == 'NabuLikelihood':
-            raise RuntimeError(f'Path {path} not pointing to a NabuLikelihood file')
-
-        self.type = 'NabuLikelihood'
-        self.varied_parameters = description['parameters']
-        self.lookup_table = { item['name']: idx for idx, item in enumerate(self.varied_parameters) }
+        self.type = description.type
+        self.varied_parameters = [asdict(p) for p in description.parameters]
+        self.lookup_table = { p.name: idx for idx, p in enumerate(description.parameters) }
 
         f = os.path.join(path, 'likelihood.nabu')
         if not os.path.exists(f) or not os.path.isfile(f):
             raise RuntimeError(f'Nabu likelihood file {f} does not exist or is not a file')
 
+        import nabu
         self.likelihood = nabu.Likelihood.load(f)
 
 
     @staticmethod
     def create(path, parameters, likelihood):
-        """ Write a new Results object (in the dynesty.results module) to disk.
+        """ Write a new NabuLikelihood object to disk.
 
         :param path: Path to the storage location, which will be created as a directory.
         :type path: str
         :param parameters: Parameter descriptions as a 1D array of shape (N, ).
         :type parameters: list or iterable of eos.Parameter
         :param likelihood: The likelihood object created by nabu.
-        :type results: nabu.Likelihood or descendant
+        :type likelihood: nabu.Likelihood or descendant
         """
-        description = {}
-        description['version'] = eos.__version__
-        description['type'] = 'NabuLikelihood'
-        description['parameters'] = [{
-            'name': p.name(),
-            'min': p.min(),
-            'max': p.max()
-        } for p in parameters]
+        description = NabuLikelihoodDescription(
+            version    = eos.__version__,
+            parameters = [ParameterDescription(name=p.name(), min=p.min(), max=p.max()) for p in parameters],
+        )
 
         os.makedirs(path, exist_ok=True)
-        with open(os.path.join(path, 'description.yaml'), 'w') as description_file:
-            yaml.dump(description, description_file, default_flow_style=False)
+        description.to_yaml_file(os.path.join(path, 'description.yaml'))
 
         likelihood.save(os.path.join(path, 'likelihood.nabu'))
