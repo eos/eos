e44a42820944b7af18e111959162b34fa3235a98
Danny van Dyk <danny.van.dyk@gmail.com>
2026-08-18 10:19:59 +0100

    [python] Convert ``eos-data`` into a shim and introduce ``eos.cli`` package

--- python/eos/cli/__init__.py
+++ python/eos/cli/__init__.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python3
-
-# Copyright (c) 2024-2026 Danny van Dyk
+# Copyright (c) 2026 Danny van Dyk
 #
 # This file is part of the EOS project. EOS is free software;
 # you can redistribute it and/or modify it under the terms of the GNU General
@@ -14,8 +12,3 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 # Place, Suite 330, Boston, MA  02111-1307  USA
-
-from eos.cli.data import main
-
-if __name__ == '__main__':
-    raise SystemExit(main())
