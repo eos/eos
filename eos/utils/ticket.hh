309d9c1c12dcb7b78e4b22231e18d029262e103a
Danny van Dyk <danny.van.dyk@gmail.com>
2026-08-27 15:49:02 +0100

    [utils] Add missing header files

--- eos/utils/ticket.hh
+++ eos/utils/ticket.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2008 Danny van Dyk <danny.dyk@uni-dortmund.de>
+ * Copyright (c) 2008-2026 Danny van Dyk
  *
  * This file is part of the EOS program. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -20,6 +20,7 @@
 #ifndef EOS_GUARD_EOS_UTILS_TICKET_HH
 #define EOS_GUARD_EOS_UTILS_TICKET_HH 1
 
+#include <eos/utils/instantiation_policy.hh>
 #include <eos/utils/private_implementation_pattern.hh>
 
 namespace eos
--- eos/utils/units.hh
+++ eos/utils/units.hh
@@ -1,7 +1,7 @@
 /* vim: set sw=4 sts=4 et foldmethod=syntax : */
 
 /*
- * Copyright (c) 2021-2025 Danny van Dyk
+ * Copyright (c) 2021-2026 Danny van Dyk
  *
  * This file is part of the EOS project. EOS is free software;
  * you can redistribute it and/or modify it under the terms of the GNU General
@@ -23,6 +23,7 @@
 #include <eos/utils/instantiation_policy.hh>
 
 #include <string>
+#include <vector>
 
 namespace eos
 {
