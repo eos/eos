f3379c2fc4fc48130397971b839c14ca09316207
Danny van Dyk <danny.van.dyk@gmail.com>
2026-08-30 17:13:20 +0100

    [python] Document ``ExpressionReferences``

--- python/_eos_TEST.py
+++ python/_eos_TEST.py
@@ -472,7 +472,6 @@ _TODO         = 'not yet documented'
 # since Python's own conventions describe them.
 BINDINGS_XFAILS_UNDOCUMENTED = {
     'BToSWilsonCoefficients':                     _TODO,
-    'ExpressionReferences':                       _TODO,
     'OptionSpecification':                        _TODO,
     'ReferenceName':                              _TODO,
     'test_statisticsChiSquare':                   _TODO,
