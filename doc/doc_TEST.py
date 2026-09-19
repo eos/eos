d036ccceb4cfb3850d3b3cc091815b8973bce4d1
Matthew Kirk <matthew.j.kirk@durham.ac.uk>
2026-09-19 17:40:58 +0100

    [doc] Make Parameter, Observable and Reference latex tests list all failures
    Removes the latex error output, but I guess it is usually obvious what the
    problem is.

--- doc/doc_TEST.py
+++ doc/doc_TEST.py
@@ -16,6 +16,7 @@ class DocTests(unittest.TestCase):
 
         texmanager = TexManager()
 
+        bad_parameters = []
         for section in eos.Parameters.Defaults().sections():
             for group in section:
                 for parameter in group:
@@ -24,7 +25,12 @@ class DocTests(unittest.TestCase):
                         try:
                             texmanager.get_text_width_height_descent('$' + latex_string + '$', fontsize=12)
                         except Exception as e:
-                            self.fail(f"Cannot compile latex representation of parameter {parameter.name()}, caught exception of type {type(e).__name__}: {e}")
+                                bad_parameters.append((parameter.name(), f'{type(e).__name__}: {e}'))
+
+        if bad_parameters:
+            print('Could not compile latex representation of the following parameters:\n'
+                  + '\n'.join(f'  {name} failed with error: {reason}' for name, reason in bad_parameters))
+            self.fail(f"Could not compile latex representation of the following parameters: {', '.join(name for name, _ in bad_parameters)}")
 
     def test_001_Observables(self):
         """Check the latex representation of all the observables"""
@@ -32,6 +38,7 @@ class DocTests(unittest.TestCase):
 
         texmanager = TexManager()
 
+        bad_observables = []
         for section in eos.Observables().sections():
             for group in section:
                 for qn, entry in group:
@@ -40,7 +47,11 @@ class DocTests(unittest.TestCase):
                         try:
                             texmanager.get_text_width_height_descent('$' + latex_string + '$', fontsize=12)
                         except Exception as e:
-                            self.fail(f"Cannot compile latex representation of observable {qn}, caught exception of type {type(e).__name__}: {e}")
+                            bad_observables.append((str(qn), f'{type(e).__name__}: {e}'))
+        if bad_observables:
+            print('Could not compile latex representation of the following observables:\n'
+                  + '\n'.join(f'  {name} failed with error: {reason}' for name, reason in bad_observables))
+            self.fail(f"Could not compile latex representation of the following observables: {', '.join(name for name, _ in bad_observables)}")
 
     def test_002_References(self):
         """Check the latex representation of all reference titles"""
@@ -48,13 +59,18 @@ class DocTests(unittest.TestCase):
 
         texmanager = TexManager()
 
+        bad_references = []
         for ref_id, ref in eos.References():
             latex_string = ref.title()
             if latex_string:
                 try:
                     texmanager.get_text_width_height_descent(latex_string, fontsize=12)
                 except Exception as e:
-                    self.fail(f"Cannot compile latex representation of reference {ref_id} title, caught exception of type {type(e).__name__}: {e}")
+                    bad_references.append((str(ref_id), f'{type(e).__name__}: {e}'))
+        if bad_references:
+            print('Could not compile latex representation of the following reference titles:\n'
+                  + '\n'.join(f'  {name} failed with error: {reason}' for name, reason in bad_references))
+            self.fail(f"Could not compile latex representation of the following reference titles: {', '.join(name for name, _ in bad_references)}")
 
     def test_003_math_conversion(self):
         """Check for any lingering ':math:' in the generated html documentation"""
